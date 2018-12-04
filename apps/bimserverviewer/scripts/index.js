afb583e3e543f13379d9a763e18543e341e5d683
xeolabs <lindsay.kay@xeolabs.com>
2018-12-04 23:56:22 +0100

    Added BIMServerModelsPlugin example

--- apps/bimserverviewer/scripts/index.js
+++ apps/bimserverviewer/scripts/index.js
@@ -1,44 +1,67 @@
-import BimServerClient from "http://localhost:8082/apps/bimserverjavascriptapi/bimserverclient.js"
+/**
+ * Info on this example: https://github.com/xeolabs/xeokit.io/wiki/Loading-IFC-Models-from-BIMServer
+ */
+
+import BimServerClient from "http://localhost:8082/apps/bimserverjavascriptapi/bimserverclient.js";
 import {Viewer} from "../../../src/viewer/Viewer.js";
 import {BIMServerModelsPlugin} from "../../../src/viewer/plugins/BIMServerModelsPlugin/BIMServerModelsPlugin.js";
-import {AxisGizmoPlugin} from "../../../src/viewer/plugins/AxisGizmoPlugin/AxisGizmoPlugin.js";
 
 const bimServerAddress = "http://localhost:8082";
 const username = "admin@bimserver.org";
 const password = "admin";
-const poid = 196609;
+const poid = 131073;
 
+// Create a Viewer
 const viewer = new Viewer({});
 
-// Create a BIMServer client API
+// Create a BIMServer client
 
-const bimServerClient = new BimServerClient(bimServerAddress);
+const bimServerAPI = new BimServerClient(bimServerAddress);
 
-// Fire up the client API
+// Add a BIMServerModelsPlugin that uses the client
 
-bimServerClient.init(() => {
+const bimServerModelsPlugin = new BIMServerModelsPlugin(viewer, {
+    bimServerAPI: bimServerAPI
+});
 
-    // Login to the client API
+// Initialize the BIMServer client
 
-    bimServerClient.login(username, password, () => {
+bimServerAPI.init(() => {
 
-        // Query BIMServer project
+    // Login to BIMServer
 
-        bimServerClient.call("ServiceInterface", "getProjectByPoid", { // TODO: Redundant?
+    bimServerAPI.login(username, password, () => {
+
+        // Query a project by ID
+
+        bimServerAPI.call("ServiceInterface", "getProjectByPoid", {
             poid: poid
         }, (project) => {
 
-            // Create a viewer plugin to load BIMServer models into the viewer via the client API
+            // Load the latest revision of the project
 
-            const bimServerModelsPlugin = new BIMServerModelsPlugin(viewer, bimServerClient);
-
-            // Load the latest revision of a project from BIMServer
+            const roid = project.lastRevisionId;
+            const schema = project.schema;
 
             const model = bimServerModelsPlugin.load({
                 id: "myModel",
-                poid: 196609,
-                roid: project.lastRevisionId,
-                schema: project.schema
+                poid: poid,
+                roid: roid,
+                schema: schema,
+                scale: [0.001, 0.001, 0.001],   // Shrink the model a bit
+                rotation: [-90, 0, 0],          // Model has Z+ axis as "up"
+                edges: true                     // Emphasize edges
+            });
+
+            const scene = viewer.scene;  // xeogl.Scene
+            const camera = scene.camera; // xeogl.Camera
+
+            model.on("loaded", () => {
+                camera.orbitPitch(20);
+                viewer.cameraFlight.flyTo(model);
+                scene.on("tick", () => {
+                    camera.orbitYaw(0.3);
+                })
             });
         });
     });
