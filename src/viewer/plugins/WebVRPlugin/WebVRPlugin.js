4c08973f5fbd97160bc4918af4627268099553a6
xeolabs <lindsay.kay@xeolabs.com>
2018-12-07 06:03:00 +0100

    Added WebVRPlugin stub

---
+++ src/viewer/plugins/WebVRPlugin/WebVRPlugin.js
@@ -0,0 +1,21 @@
+import {ModelsPlugin} from "./../../../viewer/Plugin.js";
+
+/**
+ * A viewer plugin that supports WebVR.
+ *
+ * @class WebVRPlugin
+ */
+class WebVRPlugin extends Plugin {
+
+    /**
+     * @constructor
+     * @param {Viewer} viewer The Viewer.
+     * @param {Object} cfg  Plugin configuration.
+     * @param {String} [cfg.id="WebVR"] Optional ID for this plugin, so that we can find it within {@link Viewer#plugins}.
+     */
+    constructor(viewer, cfg) {
+        super("WebVR", viewer, STLModel, cfg);
+    }
+}
+
+export {WebVRPlugin}
