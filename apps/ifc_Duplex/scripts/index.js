d3bc7c9b57f317519fdbb1bfac45dd6fb5b191e7
xeolabs <lindsay.kay@xeolabs.com>
2018-12-16 21:43:41 +0100

    More work on Duplex example

--- apps/ifc_Duplex/scripts/index.js
+++ apps/ifc_Duplex/scripts/index.js
@@ -27,7 +27,53 @@ const model = gltfModels.load({
     id: "myModel",
     src: "./../../../models/gltf/duplex/duplex.gltf",
     metadataSrc: "./../../../models/metadata/duplex/flatStructure.json",
-    edges: true
+    edges: true,
+    handleNode(modelId, nodeInfo, actions) {
+        const name = nodeInfo.name;
+        if (!name) {
+            return true; // Continue descending this node subtree
+        }
+        const objectId = modelId + "#" + name;
+        const objectMetadata = viewer.metadata.objects[objectId];
+
+        const ifcType = (objectMetadata ? objectMetadata.type : "DEFAULT") || "DEFAULT";
+        var colorize = ifcDefaultMaterials[ifcType];
+
+        actions.createObject = {
+            id: modelId + "#" + name,
+            visible: true,
+            colorize: colorize,
+            entityType: ifcType // Registers xeogl.Object in Scene#entities
+        };
+
+
+        switch (ifcType) { // Configure initial state of object depending on its type
+
+            case "IfcWindow":
+                actions.createObject.pickable = false;
+                actions.createObject.opacity = 0.2;
+                break;
+
+            case "IfcSpace":
+                actions.createObject.pickable = false;
+                actions.createObject.visible = false;
+                actions.createObject.colorize = [0.86, 0.16, 0.16];
+                actions.createObject.opacity = 0.2;
+                break;
+
+            case "IfcOpeningElement":
+                actions.createObject.pickable = false;
+                actions.createObject.visible = false;
+                break;
+
+            default: // Unrecognized type
+                actions.createObject.pickable = true;
+                actions.createObject.opacity = 0.2;
+              //  actions.createObject.visible = false;
+        }
+
+        return true; // Continue descending this glTF node subtree
+    }
 });
 
 const scene = viewer.scene;
@@ -37,12 +83,10 @@ camera.orbitPitch(20);
 
 model.on("loaded", () => {
     viewer.cameraFlight.flyTo(model);
-});
+    var modelMetadata = viewer.metadata.structures[model.id];
+    if (modelMetadata) {
 
-model.on("metadata-loaded", function () {
-    var objects = model.objects;
-
-    console.log(JSON.stringify(viewer.metadata.structures[model.id], null, "\t"));
+    }
 });
 
 
@@ -61,6 +105,8 @@ input.on("mouseclicked", function (coords) {
         var objectMetadata = viewer.metadata.objects[oid];
 
         console.log(hit.mesh.id);
+
+        hit.mesh.visible = false;
 
         if (objectMetadata) {
             console.log(JSON.stringify(objectMetadata, null, "\t"));
