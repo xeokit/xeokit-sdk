e2b7de90846f1f0b8d301252aa0793a719af0823
xeolabs <lindsay.kay@xeolabs.com>
2018-12-18 10:27:57 +0100

    Refactorings

---
+++ apps/gltf_Schependomlaan/scripts/index.js
@@ -0,0 +1,29 @@
+import {Viewer} from "../../../src/viewer/Viewer.js";
+import {GLTFLoaderPlugin} from "../../../src/viewer/plugins/GLTFLoaderPlugin/GLTFLoaderPlugin.js";
+import {AxisGizmoPlugin} from  "../../../src/viewer/plugins/AxisGizmoPlugin/AxisGizmoPlugin.js";
+
+const viewer = new Viewer({
+    canvasId: "myCanvas"
+});
+
+const gltfModels = new GLTFLoaderPlugin(viewer);
+
+new AxisGizmoPlugin(viewer, {size: [250, 250]});
+
+const model = gltfModels.load({
+    id: "myModel",
+    src: "./../../models/gltf/schependomlaan/schependomlaan.gltf",
+    edges: true
+});
+
+const scene = viewer.scene;
+const camera = scene.camera;
+
+camera.orbitPitch(20);
+
+model.on("loaded", () => {
+    viewer.cameraFlight.flyTo(model);
+    scene.on("tick", () => {
+        camera.orbitYaw(0.4);
+    })
+});
