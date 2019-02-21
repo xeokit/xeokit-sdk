b76e752c05b381404329de86d146a400ccab786c
xeolabs <lindsay.kay@xeolabs.com>
2019-02-21 15:28:20 +0100

    Fix: complete all scene tasks before Scene#render(force=true)

--- src/scene/scene/Scene.js
+++ src/scene/scene/Scene.js
@@ -60,7 +60,7 @@ function getEntityIDMap(scene, entityIds) {
  * @desc Contains the components that comprise a 3D scene.
  *
  * * A {@link Viewer} has a single Scene, which it provides in {@link Viewer#scene}.
- * * Plugins like {@link AxisGizmoPlugin} and {@link PlanViewPlugin} also have their own private Scenes.
+ * * Plugins like {@link AxisGizmoPlugin} and {@link StoreyViewsPlugin} also have their own private Scenes.
  *
  * ## Getting a Viewer's Scene
  *
@@ -979,6 +979,10 @@ class Scene extends Component {
      * since the last render.
      */
     render(forceRender) {
+
+        if (forceRender) {
+            core.runTasks();
+        }
 
         const renderEvent = {
             sceneId: null,
