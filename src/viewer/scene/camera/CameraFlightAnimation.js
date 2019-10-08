a141d267f252dcd2e287d6b132e92ee7e11a3c85
xeolabs <lindsay.kay@xeolabs.com>
2019-10-08 23:00:15 +0200

    Added projection option for CameraFlightAnimation.jumpTo()

--- src/viewer/scene/camera/CameraFlightAnimation.js
+++ src/viewer/scene/camera/CameraFlightAnimation.js
@@ -349,6 +349,7 @@ class CameraFlightAnimation extends Component {
      * @param {Number[]} [params.eye] Position to fly the eye position to.
      * @param {Number[]} [params.look]  Position to fly the look position to.
      * @param {Number[]} [params.up] Position to fly the up vector to.
+     * @param {String} [params.projection] Projection type to transition into. Can be any of the values of {@link Camera.projection}.
      * @param {Number} [params.fitFOV] How much of field-of-view, in degrees, that a target {@link Entity} or its AABB should fill the canvas on arrival. Overrides {@link CameraFlightAnimation#fitFOV}.
      * @param {Boolean} [params.fit] Whether to fit the target to the view volume. Overrides {@link CameraFlightAnimation#fit}.
      */
@@ -443,6 +444,10 @@ class CameraFlightAnimation extends Component {
             if (newUp) {
                 camera.up = newUp;
             }
+        }
+
+        if (params.projection) {
+            camera.projection = params.projection;
         }
     }
 
