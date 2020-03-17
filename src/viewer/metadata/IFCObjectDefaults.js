70171d5b700d8fac21231b1a0ef67d73b628954a
xeolabs <lindsay.kay@xeolabs.com>
2020-03-17 14:42:49 +0100

    Make IfcWindow and IfcPlate default colors less opaque

--- src/viewer/metadata/IFCObjectDefaults.js
+++ src/viewer/metadata/IFCObjectDefaults.js
@@ -75,7 +75,7 @@ const IFCObjectDefaults = {
     },
     IfcPlate: {
         colorize: [0.8470588235, 0.427450980392, 0, 0.5],
-        opacity: 0.5,
+        opacity: 0.3,
         priority: 3
     },
     IfcTransportElement: {
@@ -169,7 +169,7 @@ const IFCObjectDefaults = {
     IfcWindow: {
         colorize: [0.137255, 0.403922, 0.870588],
         pickable: true,
-        opacity: 0.4,
+        opacity: 0.1,
         priority: 6 // FIXME: transparent objects need to be last in order to avoid strange wireframe effect
     },
 
