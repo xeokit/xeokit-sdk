27ad5a19dff73b83d2767599fdda54b3c4836ab7
xeolabs <lindsay.kay@xeolabs.com>
2020-03-27 01:36:48 +0100

    GLTFPerformanceLoader - remove prioritized loading support

--- src/viewer/metadata/IFCObjectDefaults.js
+++ src/viewer/metadata/IFCObjectDefaults.js
@@ -13,164 +13,131 @@ const IFCObjectDefaults = {
     // Priority 0
 
     IfcRoof: {
-        colorize: [0.837255, 0.203922, 0.270588],
-        priority: 0
+        colorize: [0.837255, 0.203922, 0.270588]
     },
     IfcSlab: {
-        colorize: [0.637255, 0.603922, 0.670588],
-        priority: 0
+        colorize: [0.637255, 0.603922, 0.670588]
     },
     IfcWall: {
-        colorize: [0.537255, 0.337255, 0.237255],
-        priority: 0
+        colorize: [0.537255, 0.337255, 0.237255]
     },
     IfcWallStandardCase: {
-        colorize: [0.537255, 0.337255, 0.237255],
-        priority: 0
+        colorize: [0.537255, 0.337255, 0.237255]
     },
     IfcCovering: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 0
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
 
     // Priority 1
 
     IfcDoor: {
-        colorize: [0.637255, 0.603922, 0.670588],
-        priority: 1
+        colorize: [0.637255, 0.603922, 0.670588]
     },
 
     // Priority 2
 
     IfcStair: {
-        colorize: [0.637255, 0.603922, 0.670588],
-        priority: 2
+        colorize: [0.637255, 0.603922, 0.670588]
     },
     IfcStairFlight: {
-        colorize: [0.637255, 0.603922, 0.670588],
-        priority: 2
+        colorize: [0.637255, 0.603922, 0.670588]
     },
     IfcProxy: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 2
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcRamp: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 2
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
 
     // Priority 3
 
     IfcColumn: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 3
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcBeam: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 3
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcCurtainWall: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 3
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcPlate: {
         colorize: [0.8470588235, 0.427450980392, 0, 0.5],
-        opacity: 0.5,
-        priority: 3
+        opacity: 0.5
     },
     IfcTransportElement: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 3
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
     IfcFooting: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 3
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
 
     // Priority 4
 
     IfcRailing: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 4
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcFurnishingElement: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 4
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcFurniture: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 4
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
     IfcSystemFurnitureElement: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 4
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
 
     // Priority 5
 
     IfcFlowSegment: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 5
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcFlowitting: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 5
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcFlowTerminal: {
-        colorize: [0.137255, 0.403922, 0.870588],
-        priority: 5
+        colorize: [0.137255, 0.403922, 0.870588]
     },
     IfcFlowController: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 5
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
     IfcFlowFitting: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 5
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
     IfcDuctSegment: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 5
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
     IfcDistributionFlowElement: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 5
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
     IfcDuctFitting: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 5
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
     IfcLightFixture: {
-        colorize: [0.8470588235, 0.8470588235, 0.870588],
-        priority: 5
+        colorize: [0.8470588235, 0.8470588235, 0.870588]
     },
 
     // Priority 6
 
     IfcAirTerminal: {
-        colorize: [0.8470588235, 0.427450980392, 0],
-        priority: 6
+        colorize: [0.8470588235, 0.427450980392, 0]
     },
 
     IfcOpeningElement: {
         colorize: [0.137255, 0.403922, 0.870588],
         pickable: false,
-        visible: false,
-        priority: 6
+        visible: false
     },
     IfcSpace: {
         colorize: [0.137255, 0.403922, 0.870588],
         pickable: false,
         visible: false,
-        opacity: 0.5,
-        priority: 6
+        opacity: 0.5
     },
 
     IfcWindow: {
         colorize: [0.137255, 0.403922, 0.870588],
         pickable: true,
-        opacity: 0.4,
-        priority: 6 // FIXME: transparent objects need to be last in order to avoid strange wireframe effect
+        opacity: 0.4
     },
 
     //
@@ -188,8 +155,7 @@ const IFCObjectDefaults = {
     },
 
     DEFAULT: {
-        colorize: [0.5, 0.5, 0.5],
-        priority: 10
+        colorize: [0.5, 0.5, 0.5]
     }
 };
 
