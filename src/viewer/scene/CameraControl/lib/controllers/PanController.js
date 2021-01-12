73596fafd8effb157eaaef7a9b92a5c7356749b4
xeolabs <lindsay.kay@xeolabs.com>
2021-01-12 17:34:21 +0100

    Refactor PanController to use new Camera properties #530

--- src/viewer/scene/CameraControl/lib/controllers/PanController.js
+++ src/viewer/scene/CameraControl/lib/controllers/PanController.js
@@ -17,36 +17,7 @@ const tempVec4c = math.vec4();
 class PanController {
 
     constructor(scene) {
-
         this._scene = scene;
-
-        this._inverseProjectMat = math.mat4();
-        this._transposedProjectMat = math.mat4();
-
-        this._inverseOrthoProjectMat = math.mat4();
-        this._transposedOrthoProjectMat = math.mat4();
-
-        this._inverseViewMat = math.mat4();
-        this._projMatDirty = true;
-        this._viewMatDirty = true;
-        this._sceneDiagSizeDirty = true;
-        this._sceneDiagSize = 1;
-
-        this._onCameraOrthoProjMatrix = this._scene.camera.ortho.on("matrix", () => {
-            this._projMatDirty = true;
-        });
-
-        this._onCameraPerspectiveProjMatrix = this._scene.camera.perspective.on("matrix", () => {
-            this._projMatDirty = true;
-        });
-
-        this._onCameraViewMatrix = this._scene.camera.on("viewMatrix", () => {
-            this._viewMatDirty = true;
-        });
-
-        this._onSceneBoundary = this._scene.scene.on("boundary", () => {
-            this._sceneDiagSizeDirty = true;
-        });
     }
 
     /**
@@ -76,7 +47,7 @@ class PanController {
 
             camera.ortho.scale = camera.ortho.scale - dollyDelta;
 
-            const unprojectedWorldPos = this._unprojectPerspective(targetCanvasPos, tempVec4a);
+            const unprojectedWorldPos = this._unproject(targetCanvasPos, tempVec4a);
             const offset = math.subVec3(unprojectedWorldPos, camera.eye, tempVec4c);
             const moveVec = math.mulVec3Scalar(math.normalizeVec3(offset), -dollyDelta, []);
 
@@ -103,12 +74,12 @@ class PanController {
             // - get the vector in which we're dollying;
             // - add both vectors to camera eye and look.
 
-            const worldPos1 = this._unprojectOrtho(targetCanvasPos, tempVec4a);
+            const worldPos1 = this._unproject(targetCanvasPos, tempVec4a);
 
             camera.ortho.scale = camera.ortho.scale - dollyDelta;
             camera.ortho._update(); // HACK
 
-            const worldPos2 = this._unprojectOrtho(targetCanvasPos, tempVec4b);
+            const worldPos2 = this._unproject(targetCanvasPos, tempVec4b);
             const offset = math.subVec3(worldPos2, worldPos1, tempVec4c);
             const eyeLookMoveVec = math.mulVec3Scalar(math.normalizeVec3(math.subVec3(camera.look, camera.eye, tempVec3a)), -dollyDelta, tempVec3b);
             const moveVec = math.addVec3(offset, eyeLookMoveVec, tempVec3c);
@@ -120,113 +91,21 @@ class PanController {
         return dolliedThroughSurface;
     }
 
-    _getInverseProjectMat() {
-        this._updateProjMatrices();
-        return this._inverseProjectMat;
-    }
+    _unproject(canvasPos, worldPos) {
 
-    _getTransposedProjectMat() {
-        this._updateProjMatrices();
-        return this._transposedProjectMat;
-    }
-
-    _getInverseOrthoProjectMat() {
-        this._updateProjMatrices();
-        return this._inverseOrthoProjectMat;
-    }
-
-    _getTransposedOrthoProjectMat() {
-        this._updateProjMatrices();
-        return this._transposedOrthoProjectMat;
-    }
-
-    _updateProjMatrices() {
-        if (this._projMatDirty) {
-            math.inverseMat4(this._scene.camera.perspective.matrix, this._inverseProjectMat);
-            math.inverseMat4(this._scene.camera.ortho.matrix, this._inverseOrthoProjectMat);
-            math.transposeMat4(this._scene.camera.perspective.matrix, this._transposedProjectMat);
-            math.transposeMat4(this._scene.camera.ortho.matrix, this._transposedOrthoProjectMat);
-            this._projMatDirty = false;
-        }
-    }
-
-    _getInverseViewMat() {
-        if (this._viewMatDirty) {
-            math.inverseMat4(this._scene.camera.viewMatrix, this._inverseViewMat);
-        }
-        return this._inverseViewMat;
-    }
-
-    _getSceneDiagSize() {
-        if (this._sceneDiagSizeDirty) {
-            this._sceneDiagSize = math.getAABB3Diag(this._scene.aabb);
-        }
-        return this._sceneDiagSize;
-    }
-
-    _unprojectPerspective(canvasPos, worldPos) {
-
-        const canvas = this._scene.canvas.canvas;
-        const transposedProjectMat = this._getTransposedProjectMat();
+        const camera = this._scene.camera;
+        const transposedProjectMat = camera.project.transposedMatrix;
         const Pt3 = transposedProjectMat.subarray(8, 12);
         const Pt4 = transposedProjectMat.subarray(12);
-        const D = [0, 0, -this._getSceneDiagSize(), 1];
+        const D = [0, 0, -1.0, 1];
         const screenZ = math.dotVec4(D, Pt3) / math.dotVec4(D, Pt4);
-        const inverseProjMat = this._getInverseProjectMat();
-        const inverseViewMat = this._getInverseViewMat();
-        const halfCanvasWidth = canvas.offsetWidth / 2.0;
-        const halfCanvasHeight = canvas.offsetHeight / 2.0;
 
-        screenPos[0] = (canvasPos[0] - halfCanvasWidth) / halfCanvasWidth;
-        screenPos[1] = (canvasPos[1] - halfCanvasHeight) / halfCanvasHeight;
-        screenPos[2] = screenZ;
-        screenPos[3] = 1.0;
-
-        math.mulMat4v4(inverseProjMat, screenPos, viewPos);
-        math.mulVec3Scalar(viewPos, 1.0 / viewPos[3]); // Normalize homogeneous coord
-
-        viewPos[3] = 1.0;
-        viewPos[1] *= -1; // TODO: Why is this reversed?
-
-        math.mulMat4v4(inverseViewMat, viewPos, worldPos);
-
-        return worldPos;
-    }
-
-    _unprojectOrtho(canvasPos, worldPos) {
-
-        const canvas = this._scene.canvas.canvas;
-        const transposedProjectMat = this._getTransposedOrthoProjectMat();
-        const Pt3 = transposedProjectMat.subarray(8, 12);
-        const Pt4 = transposedProjectMat.subarray(12);
-        const D = [0, 0, -this._getSceneDiagSize(), 1];
-        const screenZ = math.dotVec4(D, Pt3) / math.dotVec4(D, Pt4);
-        const inverseProjMat = this._getInverseOrthoProjectMat();
-        const inverseViewMat = this._getInverseViewMat();
-        const halfCanvasWidth = canvas.offsetWidth / 2.0;
-        const halfCanvasHeight = canvas.offsetHeight / 2.0;
-
-        screenPos[0] = (canvasPos[0] - halfCanvasWidth) / halfCanvasWidth;
-        screenPos[1] = (canvasPos[1] - halfCanvasHeight) / halfCanvasHeight;
-        screenPos[2] = screenZ;
-        screenPos[3] = 1.0;
-
-        math.mulMat4v4(inverseProjMat, screenPos, viewPos);
-        math.mulVec3Scalar(viewPos, 1.0 / viewPos[3]); // Normalize homogeneous coord
-
-        viewPos[3] = 1.0;
-        viewPos[1] *= -1; // TODO: Why is this reversed?
-
-        math.mulMat4v4(inverseViewMat, viewPos, worldPos);
+        camera.project.unproject(canvasPos, screenZ, screenPos, viewPos, worldPos);
 
         return worldPos;
     }
 
     destroy() {
-        this._scene.camera.perspective.off(this._onCameraPerspectiveProjMatrix);
-        this._scene.camera.ortho.off(this._onCameraOrthoProjMatrix);
-        this._scene.camera.off(this._onCameraViewMatrix);
-        this._scene.scene.off(this._onSceneBoundary);
     }
 }
 
