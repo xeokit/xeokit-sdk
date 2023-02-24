53a4eeff672e04319cd44adafca62b9405b98546
Hugo Duroux <hugo@bimdata.io>
2023-02-24 14:14:02 +0100

    Skip inactive section planes in BCF getViewpoint

--- src/plugins/BCFViewpointsPlugin/BCFViewpointsPlugin.js
+++ src/plugins/BCFViewpointsPlugin/BCFViewpointsPlugin.js
@@ -396,7 +396,9 @@ class BCFViewpointsPlugin extends Plugin {
         for (let id in sectionPlanes) {
             if (sectionPlanes.hasOwnProperty(id)) {
                 let sectionPlane = sectionPlanes[id];
-
+                if (!sectionPlane.active) {
+                    continue;
+                }
                 let location = sectionPlane.pos;
 
                 let direction;
