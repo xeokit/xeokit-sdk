d2e5b31c20b141d25126138d6c98a85e96a53227
lindsay <lindsay.kay@xeolabs.com>
2023-03-11 22:24:43 +0100

    Support federated metamodels #991

--- src/viewer/metadata/MetaModel.js
+++ src/viewer/metadata/MetaModel.js
@@ -110,8 +110,17 @@ class MetaModel {
          *
          * @property rootMetaObject
          * @type {MetaObject}
+         * @deprecated
          */
         this.rootMetaObject = rootMetaObject;
+
+        /**
+         * The root {@link MetaObject}s in this MetaModel's composition structure hierarchy.
+         *
+         * @property rootMetaObject
+         * @type {MetaObject[]}
+         */
+        this.rootMetaObjects = [rootMetaObject];
     }
 
     getJSON() {
