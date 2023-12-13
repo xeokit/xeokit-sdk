56b5b37db74c9c0e64ed8a4612a4598b18468b11
lindsay <lindsay.kay@xeolabs.com>
2023-12-13 03:35:06 +0100

    Fix Mesh shader compilation errors

--- src/viewer/scene/mesh/emphasis/EmphasisEdgesShaderSource.js
+++ src/viewer/scene/mesh/emphasis/EmphasisEdgesShaderSource.js
@@ -12,7 +12,8 @@ class EmphasisEdgesShaderSource {
 
 function buildVertex(mesh) {
     const scene = mesh.scene;
-    const clipping = scene._sectionPlanesState.sectionPlanes.length > 0;
+    const sectionPlanesState = scene._sectionPlanesState;
+    const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
     const quantizedGeometry = !!mesh._geometry._state.compressGeometry;
     const billboard = mesh._state.billboard;
     const stationary = mesh._state.stationary;
--- src/viewer/scene/mesh/emphasis/EmphasisFillShaderSource.js
+++ src/viewer/scene/mesh/emphasis/EmphasisFillShaderSource.js
@@ -15,7 +15,8 @@ function buildVertex(mesh) {
     const scene = mesh.scene;
     const lightsState = scene._lightsState;
     const normals = hasNormals(mesh);
-    const clipping = scene._sectionPlanesState.sectionPlanes.length > 0;
+    const sectionPlanesState = scene._sectionPlanesState;
+    const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
     const quantizedGeometry = !!mesh._geometry._state.compressGeometry;
     const billboard = mesh._state.billboard;
     const stationary = mesh._state.stationary;
