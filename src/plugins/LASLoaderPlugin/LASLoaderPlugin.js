6cbcfb7a2921ca82841534807b45a34a14a80734
lindsay <lindsay.kay@xeolabs.com>
2024-01-16 01:59:05 +0100

    Make LASLoaderPlugin handle double-precision coordinates, fix chunking

--- src/plugins/LASLoaderPlugin/LASLoaderPlugin.js
+++ src/plugins/LASLoaderPlugin/LASLoaderPlugin.js
@@ -4,7 +4,7 @@ import {Plugin} from "../../viewer/Plugin.js";
 import {LASDefaultDataSource} from "./LASDefaultDataSource.js";
 import {math} from "../../viewer/index.js";
 import {parse} from '@loaders.gl/core';
-import {LASLoader} from '@loaders.gl/las/dist/esm/las-loader.js';
+import {LASLoader} from '@loaders.gl/las';
 import {loadLASHeader} from "./loadLASHeader";
 
 const MAX_VERTICES = 500000; // TODO: Rough estimate
@@ -339,48 +339,6 @@ class LASLoaderPlugin extends Plugin {
 
     _parseModel(arrayBuffer, params, options, sceneModel) {
 
-        function readPositions(attributesPosition) {
-            const positionsValue = attributesPosition.value;
-            if (params.rotateX) {
-                if (positionsValue) {
-                    for (let i = 0, len = positionsValue.length; i < len; i += 3) {
-                        const temp = positionsValue[i + 1];
-                        positionsValue[i + 1] = positionsValue[i + 2];
-                        positionsValue[i + 2] = temp;
-                    }
-                }
-            }
-            return positionsValue;
-        }
-
-        function readColorsAndIntensities(attributesColor, attributesIntensity) {
-            const colors = attributesColor.value;
-            const colorSize = attributesColor.size;
-            const intensities = attributesIntensity.value;
-            const colorsCompressedSize = intensities.length * 4;
-            const colorsCompressed = new Uint8Array(colorsCompressedSize);
-            for (let i = 0, j = 0, k = 0, len = intensities.length; i < len; i++, k += colorSize, j += 4) {
-                colorsCompressed[j + 0] = colors[k + 0];
-                colorsCompressed[j + 1] = colors[k + 1];
-                colorsCompressed[j + 2] = colors[k + 2];
-                colorsCompressed[j + 3] = Math.round((intensities[i] / 65536) * 255);
-            }
-            return colorsCompressed;
-        }
-
-        function readIntensities(attributesIntensity) {
-            const intensities = attributesIntensity.intensity;
-            const colorsCompressedSize = intensities.length * 4;
-            const colorsCompressed = new Uint8Array(colorsCompressedSize);
-            for (let i = 0, j = 0, k = 0, len = intensities.length; i < len; i++, k += 3, j += 4) {
-                colorsCompressed[j + 0] = 0;
-                colorsCompressed[j + 1] = 0;
-                colorsCompressed[j + 2] = 0;
-                colorsCompressed[j + 3] = Math.round((intensities[i] / 65536) * 255);
-            }
-            return colorsCompressed;
-        }
-
         return new Promise((resolve, reject) => {
 
             if (sceneModel.destroyed) {
@@ -404,6 +362,190 @@ class LASLoaderPlugin extends Plugin {
             try {
 
                 const lasHeader = loadLASHeader(arrayBuffer);
+
+                // Version Number: The version number consists of a major and minor field. The major and minor
+                // fields combine to form the number that indicates the format number of the current specification
+                // itself. For example, specification number 1.2 would contain 1 in the major field and
+                // 2 in the minor field.
+
+                const versionMajor = lasHeader.VersionMajor;
+                const versionMinor = lasHeader.VersionMinor;
+
+                if (versionMajor !== 1 || (versionMinor !== 1 && versionMinor !== 2 && versionMinor !== 3)) {
+                    sceneModel.finalize();
+                    reject("Unsupported LAS version - supported versions are v1.2 and v1.3.");
+                    return;
+                }
+
+                // console.info(`LAS Version: ${versionMajor}.${versionMinor}`);
+
+                // Point Data Format ID: The point data format ID corresponds to the point data record format
+                // type. LAS 1.2 defines types 0, 1, 2 and 3.
+
+                const pointDataFormatId = lasHeader.PointDataFormatID;
+
+                if (pointDataFormatId !== 0 &&
+                    pointDataFormatId !== 1 &&
+                    pointDataFormatId !== 2 &&
+                    pointDataFormatId !== 3) {
+                    sceneModel.finalize();
+                    reject("Unsupported LAS Point Data Format ID - supported formats are 0, 1, 2 and 3.");
+                    return;
+                }
+
+                // console.info(`LAS Point Data Format: ${pointDataFormatId}`);
+
+                // X, Y, and Z scale factors: The scale factor fields contain a double floating point value that is used
+                // to scale the corresponding X, Y, and Z long values within the point records. The corresponding
+                // X, Y, and Z scale factor must be multiplied by the X, Y, or Z point record value to get the actual
+                // X, Y, or Z coordinate. For example, if the X, Y, and Z coordinates are intended to have two
+                // decimal point values, then each scale factor will contain the number 0.01.
+
+                const scaleFactorX = lasHeader.ScaleFactorX || 1.0;
+                const scaleFactorY = lasHeader.ScaleFactorY || 1.0;
+                const scaleFactorZ = lasHeader.ScaleFactorZ || 1.0;
+
+                // console.info(`LAS ScaleFactors: [${scaleFactorX},${scaleFactorY},${scaleFactorZ}]`);
+
+                // X, Y, and Z offset: The offset fields should be used to set the overall offset for the point records.
+                // In general these numbers will be zero, but for certain cases the resolution of the point data may
+                // not be large enough for a given projection system. However, it should always be assumed that
+                // these numbers are used. So to scale a given X from the point record, take the point record X
+                // multiplied by the X scale factor, and then add the X offset.
+
+                const offsetX = lasHeader.OffsetX || 0.0;
+                const offsetY = lasHeader.OffsetY || 0.0;
+                const offsetZ = lasHeader.OffsetZ || 0.0;
+
+                // console.info(`LAS Offsets: [${offsetX},${offsetY},${offsetZ}]`);
+
+                // Max and Min X, Y, Z: The max and min data fields are the actual unscaled extents of the LAS
+                // point file data, specified in the coordinate system of the LAS data.
+
+                const minX = lasHeader.MinX;
+                const minY = lasHeader.MinY;
+                const minZ = lasHeader.MinZ;
+                const maxX = lasHeader.MaxX;
+                const maxY = lasHeader.MaxY;
+                const maxZ = lasHeader.MaxZ;
+
+                // console.info(`LAS unscaled Min = [${minX},${minY},${minZ}], Max = [${maxX},${maxY},${maxZ}]`);
+
+                // The projection information for the point data is required for all data. The projection information
+                // will be placed in the Variable Length Records. Placing the projection information within the
+                // Variable Length Records allows for any projection to be defined including custom projections.
+                // The GeoTIff specification http://www.remotesensing.org/geotiff/geotiff.html is the model for
+                // representing the projection information, and the format is explicitly defined by this specification.
+
+                const numberOfVariablelengthRecords = lasHeader.NumberOfVaraibleLengthRecords;
+
+                // TODO: Parse projection info from Variable Length Records?
+
+                function readColorsAndIntensities(attributesColor, attributesIntensity) {
+                    const colors = attributesColor.value;
+                    const colorSize = attributesColor.size;
+                    const intensities = attributesIntensity.value;
+                    const colorsCompressedSize = intensities.length * 4;
+                    const colorsCompressed = new Uint8Array(colorsCompressedSize);
+                    for (let i = 0, j = 0, k = 0, len = intensities.length; i < len; i++, k += colorSize, j += 4) {
+                        colorsCompressed[j + 0] = colors[k + 0];
+                        colorsCompressed[j + 1] = colors[k + 1];
+                        colorsCompressed[j + 2] = colors[k + 2];
+                        colorsCompressed[j + 3] = Math.round((intensities[i] / 65536) * 255);
+                    }
+                    return colorsCompressed;
+                }
+
+                function readIntensities(attributesIntensity) {
+                    const intensities = attributesIntensity.intensity;
+                    const colorsCompressedSize = intensities.length * 4;
+                    const colorsCompressed = new Uint8Array(colorsCompressedSize);
+                    for (let i = 0, j = 0, k = 0, len = intensities.length; i < len; i++, k += 3, j += 4) {
+                        colorsCompressed[j + 0] = 0;
+                        colorsCompressed[j + 1] = 0;
+                        colorsCompressed[j + 2] = 0;
+                        colorsCompressed[j + 3] = Math.round((intensities[i] / 65536) * 255);
+                    }
+                    return colorsCompressed;
+                }
+
+                function readPositions(attributesPosition) {
+                    const positionsValue = attributesPosition.value;
+
+                    // Positions are already scaled and offset by loaders.gl
+                    // Disable scaling and offset, but keep here for reference for now
+
+                    // for (let i = 0, len = positionsValue.length; i < len; i += 3) {
+                    //     positionsValue[i + 0] = (positionsValue[i + 0] * scaleFactorX) + offsetX;
+                    //     positionsValue[i + 1] = (positionsValue[i + 1] * scaleFactorY) + offsetY;
+                    //     positionsValue[i + 2] = (positionsValue[i + 2] * scaleFactorZ) + offsetZ;
+                    // }
+
+                    if (params.rotateX) {
+                        if (positionsValue) {
+                            for (let i = 0, len = positionsValue.length; i < len; i += 3) {
+                                const temp = positionsValue[i + 1];
+                                positionsValue[i + 1] = positionsValue[i + 2];
+                                positionsValue[i + 2] = temp;
+                            }
+                        }
+                    }
+                    return positionsValue;
+                }
+
+                function worldToRTCPos(worldPos, origin, rtcPos, cellSize = 1000) { // TODO: cellSize guessed
+                    origin[0] = Math.round(worldPos[0] / cellSize) * cellSize;
+                    origin[1] = Math.round(worldPos[1] / cellSize) * cellSize;
+                    origin[2] = Math.round(worldPos[2] / cellSize) * cellSize;
+                    rtcPos[0] = worldPos[0] - origin[0];
+                    rtcPos[1] = worldPos[1] - origin[1];
+                    rtcPos[2] = worldPos[2] - origin[2];
+                }
+
+                function getRTCPositions(positions, colors) {
+
+                    if ((positions.length / 3) !== (colors.length / 4)) {
+                        throw "Mismatch in lengths of positions and colors arrays";
+                    }
+
+                    const rtcTiles = {};
+
+                    const worldPos = math.vec3();
+                    const origin = math.vec3();
+                    const rtcPos = math.vec3();
+
+                    for (let i = 0, j = 0, len = positions.length; i < len; i += 3, j += 4) {
+
+                        worldPos[0] = positions[i];
+                        worldPos[1] = positions[i + 1];
+                        worldPos[2] = positions[i + 2];
+
+                        worldToRTCPos(worldPos, origin, rtcPos);
+
+                        const tileId = `${origin[0]}-${origin[1]}-${origin[2]}`;
+
+                        let rtcTile = rtcTiles[tileId];
+                        if (!rtcTile) {
+                            rtcTile = {
+                                origin: origin.slice(),
+                                positions: [],
+                                colors: []
+                            };
+                            rtcTiles[tileId] = rtcTile;
+                        }
+
+                        rtcTile.positions.push(rtcPos[0]);
+                        rtcTile.positions.push(rtcPos[1]);
+                        rtcTile.positions.push(rtcPos[2]);
+
+                        rtcTile.colors.push(colors[j + 0]);
+                        rtcTile.colors.push(colors[j + 1]);
+                        rtcTile.colors.push(colors[j + 2]);
+                        rtcTile.colors.push(colors[j + 3]);
+                    }
+
+                    return Object.values(rtcTiles);
+                }
 
                 parse(arrayBuffer, LASLoader, options).then((parsedData) => {
 
@@ -454,44 +596,31 @@ class LASLoaderPlugin extends Plugin {
                             break;
                     }
 
-                    const pointsChunks = chunkArray(positionsValue, MAX_VERTICES * 3);
-                    const colorsChunks = chunkArray(colorsCompressed, MAX_VERTICES * 4);
                     const meshIds = [];
 
-                    for (let i = 0, len = pointsChunks.length; i < len; i++) {
-                        const meshId = `pointsMesh${i}`;
-                        meshIds.push(meshId);
-                        sceneModel.createMesh({
-                            id: meshId,
-                            primitive: "points",
-                            positions: pointsChunks[i],
-                            colorsCompressed: (i < colorsChunks.length) ? colorsChunks[i] : null
-                        });
+                    const rtcTiles = getRTCPositions(positionsValue, colorsCompressed);
+
+                    for (let i = 0, leni = rtcTiles.length; i < leni; i++) {
+
+                        const rtcTile = rtcTiles[i];
+                        const pointsChunks = chunkArray(rtcTile.positions, MAX_VERTICES * 3);
+                        const colorsChunks = chunkArray(rtcTile.colors, MAX_VERTICES * 4);
+                        const origin = rtcTile.origin;
+
+                        for (let j = 0, lenj = pointsChunks.length; j < lenj; j++) {
+
+                            const meshId = `pointsMesh-${i}-${j}`;
+                            meshIds.push(meshId);
+
+                            sceneModel.createMesh({
+                                id: meshId,
+                                primitive: "points",
+                                origin,
+                                positions: pointsChunks[j],
+                                colorsCompressed: (j < colorsChunks.length) ? colorsChunks[j] : null
+                            });
+                        }
                     }
-                    /*
-                                const pointsChunks = chunkArray(positionsValue, MAX_VERTICES * 3);
-                    const colorsChunks = chunkArray(colorsCompressed, MAX_VERTICES * 4);
-                    const meshIds = [];
-
-                    for (let i = 0, len = pointsChunks.length; i < len; i++) {
-
-                        const geometryId = `geometryMesh${i}`;
-                        const meshId = `pointsMesh${i}`;
-                        meshIds.push(meshId);
-
-                        sceneModel.createGeometry({
-                            id: geometryId,
-                            primitive: "points",
-                            positions: pointsChunks[i],
-                            colorsCompressed: (i < colorsChunks.length) ? colorsChunks[i] : null
-                        });
-
-                        sceneModel.createMesh({
-                            id: meshId,
-                            geometryId
-                        });
-                    }
-                     */
 
                     const pointsObjectId = math.createUUID();
 
@@ -519,7 +648,7 @@ class LASLoaderPlugin extends Plugin {
                                 },
                                 {
                                     id: pointsObjectId,
-                                    name: "PointCloud (LAS)",
+                                    name: `PointCloud (LAS v${versionMajor}.${versionMinor})`,
                                     type: "PointCloud",
                                     parent: rootMetaObjectId,
                                     attributes: lasHeader || {}
@@ -551,7 +680,7 @@ class LASLoaderPlugin extends Plugin {
 
 function chunkArray(array, chunkSize) {
     if (chunkSize >= array.length) {
-        return array;
+        return [array]; // One chunk
     }
     let result = [];
     for (let i = 0; i < array.length; i += chunkSize) {
