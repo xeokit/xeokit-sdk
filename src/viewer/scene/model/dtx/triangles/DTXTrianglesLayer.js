54ff6570f1498c3dffb9604f46ce66fded7aba1f
xeolabs <lindsay.kay@xeolabs.com>
2024-03-28 00:11:08 +0100

    Add selection glowThrough support to DTX triangles renderer layer

--- src/viewer/scene/model/dtx/triangles/DTXTrianglesLayer.js
+++ src/viewer/scene/model/dtx/triangles/DTXTrianglesLayer.js
@@ -860,7 +860,9 @@ export class DTXTrianglesLayer {
         // Color
 
         let f0;
-        if (!visible || culled || xrayed) { // Highlight & select are layered on top of color - not mutually exclusive
+        if (!visible || culled || xrayed
+            || (highlighted && !this.model.scene.highlightMaterial.glowThrough)
+            || (selected && !this.model.scene.selectedMaterial.glowThrough)) {
             f0 = RENDER_PASSES.NOT_RENDERED;
         } else {
             if (transparent) {
