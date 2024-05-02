fe2bf8385341deaa8580c76f1e887407cf34b662
lindsay <lindsay.kay@xeolabs.com>
2024-05-02 19:13:59 +0200

    Rebuild

--- dist/xeokit-sdk.es5.js
+++ dist/xeokit-sdk.es5.js
@@ -8448,7 +8448,129 @@ _this52.enabled=cfg.enabled;_this52.kernelRadius=cfg.kernelRadius;_this52.intens
      * @type {Number}
      */function get(){return this._blendFactor;}/**
      * Destroys this component.
-     */,set:function set(value){if(value===undefined||value===null){value=1.0;}if(this._blendFactor===value){return;}this._blendFactor=value;this.glRedraw();}},{key:"destroy",value:function destroy(){_get(_getPrototypeOf(SAO.prototype),"destroy",this).call(this);}}]);return SAO;}(Component);var CrossSections=/*#__PURE__*/function(_Component20){_inherits(CrossSections,_Component20);var _super32=_createSuper(CrossSections);/** @private */function CrossSections(owner){var _this53;var cfg=arguments.length>1&&arguments[1]!==undefined?arguments[1]:{};_classCallCheck(this,CrossSections);_this53=_super32.call(this,owner,cfg);_this53.sliceColor=cfg.sliceColor;_this53.sliceThickness=cfg.sliceThickness;return _this53;}/**
+     */,set:function set(value){if(value===undefined||value===null){value=1.0;}if(this._blendFactor===value){return;}this._blendFactor=value;this.glRedraw();}},{key:"destroy",value:function destroy(){_get(_getPrototypeOf(SAO.prototype),"destroy",this).call(this);}}]);return SAO;}(Component);/**
+ * @desc Configures cross-section slices for a {@link Scene}.
+ *
+ * ## Overview
+ *
+ * Cross-sections allow to create an additional colored slice for used clipping planes. It is only a visual effect
+ * calculated by shaders. It makes it easier to see the intersection between the model and the clipping plane this way.
+ *
+ * ## Usage
+ *
+ * In the example below, we'll configure CrossSections to manipulate the slice representation.
+ *
+ * ````javascript
+ * //------------------------------------------------------------------------------------------------------------------
+ * // Import the modules we need for this example
+ * //------------------------------------------------------------------------------------------------------------------
+ *
+ * import {PhongMaterial, Viewer, math, SectionPlanesPlugin, XKTLoaderPlugin, Mesh, ReadableGeometry, buildPolylineGeometryFromCurve, SplineCurve} from "../../dist/xeokit-sdk.es.js";
+ *
+ * //------------------------------------------------------------------------------------------------------------------
+ * // Create a Viewer and arrange the camera
+ * //------------------------------------------------------------------------------------------------------------------
+ *
+ * const viewer = new Viewer({
+ *   canvasId: "myCanvas",
+ *   transparent: true
+ * });
+ *
+ * viewer.camera.eye = [-2.341298674548419, 22.43987089731119, 7.236688436028655];
+ * viewer.camera.look = [4.399999999999963, 3.7240000000000606, 8.899000000000006];
+ * viewer.camera.up = [0.9102954845584759, 0.34781746407929504, 0.22446635042673466];
+ *
+ * const cameraControl = viewer.cameraControl;
+ * cameraControl.navMode = "orbit";
+ * cameraControl.followPointer = true;
+ *
+ * //----------------------------------------------------------------------------------------------------------------------
+ * // Create a xeokit loader plugin, load a model, fit to view
+ * //----------------------------------------------------------------------------------------------------------------------
+ *
+ * const xktLoader = new XKTLoaderPlugin(viewer);
+ *
+ * var t0 = performance.now();
+ *
+ * document.getElementById("time").innerHTML = "Loading model...";
+ *
+ * const sceneModel = xktLoader.load({
+ *   id: "myModel",
+ *   src: "../../assets/models/xkt/v10/glTF-Embedded/Duplex_A_20110505.glTFEmbedded.xkt",
+ *   edges: true
+ * });
+ *
+ * sceneModel.on("loaded", () => {
+ *   var t1 = performance.now();
+ *   document.getElementById("time").innerHTML = "Model loaded in " + Math.floor(t1 - t0) / 1000.0 + " seconds<br>Objects: " + sceneModel.numEntities;
+ *
+ *   let path = new SplineCurve(viewer.scene, {
+ *     points: [
+ *       [0, 0, -10],
+ *       [0, 0, -3],
+ *       [10, 0, 10],
+ *       [10, 0, 30],
+ *     ],
+ *   });
+ *
+ *   new Mesh(viewer.scene, {
+ *     geometry: new ReadableGeometry(viewer.scene, buildPolylineGeometryFromCurve({
+ *       id: "SplineCurve",
+ *       curve: path,
+ *       divisions: 50,
+ *     })),
+ *     material: new PhongMaterial(viewer.scene, {
+ *       emissive: [1, 0, 0]
+ *     })
+ *   });
+ *
+ *   //------------------------------------------------------------------------------------------------------------------
+ *   // Create a moving SectionPlane, that moves through the table models
+ *   //------------------------------------------------------------------------------------------------------------------
+ *
+ *   const sectionPlanes = new SectionPlanesPlugin(viewer, {
+ *     overviewCanvasId: "mySectionPlanesOverviewCanvas",
+ *     overviewVisible: true
+ *   });
+ *
+ *   let currentPoint = path.getPoint(0);
+ *   let currentDirection = path.getTangent(0);
+ *
+ *   const sectionPlane = sectionPlanes.createSectionPlane({
+ *     id: "mySectionPlane",
+ *     pos: currentPoint,
+ *     dir: currentDirection
+ *   });
+ *
+ *   sectionPlanes.showControl(sectionPlane.id);
+ *
+ *   //------------------------------------------------------------------------------------------------------------------
+ *   // Controlling SectionPlane position and direction
+ *   //------------------------------------------------------------------------------------------------------------------
+ *
+ *   let currentT = 0.0;
+ *   document.getElementById("section_path").oninput = function() {
+ *     currentT = Number(document.getElementById("section_path").value);
+ *     currentPoint = path.getPoint(currentT);
+ *     currentDirection = path.getTangent(currentT);
+ *     sectionPlane.pos = currentPoint;
+ *     sectionPlane.dir = currentDirection;
+ *   };
+ *
+ *   window.viewer = viewer;
+ *
+ *   //------------------------------------------------------------------------------------------------------------------
+ *   // Controlling CrossSections settings
+ *   //------------------------------------------------------------------------------------------------------------------
+ *
+ *   viewer.scene.crossSections.sliceThickness = 0.05;
+ *   viewer.scene.crossSections.sliceColor = [0.0, 0.0, 0.0, 1.0];
+ * });
+ * ````
+ *
+ * [[Run this example](https://xeokit.github.io/xeokit-sdk/examples/slicing/#SectionPlanesPlugin_Duplex_SectionPath_CrossSections)]
+ *
+ */var CrossSections=/*#__PURE__*/function(_Component20){_inherits(CrossSections,_Component20);var _super32=_createSuper(CrossSections);/** @private */function CrossSections(owner){var _this53;var cfg=arguments.length>1&&arguments[1]!==undefined?arguments[1]:{};_classCallCheck(this,CrossSections);_this53=_super32.call(this,owner,cfg);_this53.sliceColor=cfg.sliceColor;_this53.sliceThickness=cfg.sliceThickness;return _this53;}/**
      * Sets the thickness of a slice created by a section.
      *
      * Default value is ````0.0````.
@@ -8467,7 +8589,7 @@ _this52.enabled=cfg.enabled;_this52.kernelRadius=cfg.kernelRadius;_this52.intens
      *
      * @type {Number}
      */,set:function set(value){if(value===undefined||value===null){value=0.0;}if(this._sliceThickness===value){return;}this._sliceThickness=value;this.glRedraw();}},{key:"sliceColor",get:/**
-     * Gets the thickness of a slice created by a section.
+     * Gets the color of a slice created by a section.
      *
      * Default value is ````[0.0, 0.0, 0.0, 1.0]````.
      *
@@ -16820,7 +16942,7 @@ var edgeIndex=void 0;if((edgeIndex=edgeSearch(ii0,ii1))>=0){if(alreadyOutputEdge
          * Unique ID of this SceneModelTransform.
          *
          * The SceneModelTransform is registered against this ID in {@link SceneModel#transforms}.
-         */this.id=cfg.id;this._parentTransform=cfg.parent;this._childTransforms=[];this._meshes=[];this._scale=new Float32Array([1,1,1]);this._quaternion=math.identityQuaternion(new Float32Array(4));this._rotation=new Float32Array(3);this._position=new Float32Array(3);this._localMatrix=math.identityMat4(new Float32Array(16));this._worldMatrix=math.identityMat4(new Float32Array(16));this._localMatrixDirty=true;this._worldMatrixDirty=true;if(cfg.matrix){this.matrix=cfg.matrix;}else{this.scale=cfg.scale;this.position=cfg.position;if(cfg.quaternion);else{this.rotation=cfg.rotation;}}if(cfg.parent){cfg.parent._addChildTransform(this);}}_createClass(SceneModelTransform,[{key:"_addChildTransform",value:function _addChildTransform(childTransform){this._childTransforms.push(childTransform);childTransform._parentTransform=this;childTransform._transformDirty();childTransform._setSubtreeAABBsDirty(this);}},{key:"_addMesh",value:function _addMesh(mesh){this._meshes.push(mesh);mesh.transform=this;// childTransform._setWorldMatrixDirty();
+         */this.id=cfg.id;this._parentTransform=cfg.parent;this._childTransforms=[];this._meshes=[];this._scale=new Float32Array([1,1,1]);this._quaternion=math.identityQuaternion(new Float32Array(4));this._rotation=new Float32Array(3);this._position=new Float32Array(3);this._localMatrix=math.identityMat4(new Float32Array(16));this._worldMatrix=math.identityMat4(new Float32Array(16));this._localMatrixDirty=true;this._worldMatrixDirty=true;if(cfg.matrix){this.matrix=cfg.matrix;}else{this.scale=cfg.scale;this.position=cfg.position;if(cfg.quaternion);else{this.rotation=cfg.rotation;}}if(cfg.parent){cfg.parent._addChildTransform(this);}}_createClass(SceneModelTransform,[{key:"_addChildTransform",value:function _addChildTransform(childTransform){this._childTransforms.push(childTransform);childTransform._parentTransform=this;childTransform._transformDirty();childTransform._setAABBDirty();}},{key:"_addMesh",value:function _addMesh(mesh){this._meshes.push(mesh);mesh.transform=this;// childTransform._setWorldMatrixDirty();
 // childTransform._setAABBDirty();
 }/**
      * The optional parent SceneModelTransform.
