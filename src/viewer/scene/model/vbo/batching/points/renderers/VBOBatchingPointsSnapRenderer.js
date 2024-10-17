40871e0f19c61689df03c0e60c912906d5407fd3
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-10-17 23:54:49 +0200

    Remove VBOBatchingPointsSnapRenderer::_allocate overload

--- src/viewer/scene/model/vbo/batching/points/renderers/VBOBatchingPointsSnapRenderer.js
+++ src/viewer/scene/model/vbo/batching/points/renderers/VBOBatchingPointsSnapRenderer.js
@@ -132,21 +132,6 @@ export class VBOBatchingPointsSnapRenderer extends VBORenderer {
         gl.drawArrays(gl.POINTS, 0, state.positionsBuf.numItems);
     }
 
-    _allocate() {
-        super._allocate();
-
-        const program = this._program;
-
-        if (SNAPPING_LOG_DEPTH_BUF_ENABLED) {
-            this._uLogDepthBufFC = program.getLocation("logDepthBufFC");
-        }
-        this.uVectorA = program.getLocation("snapVectorA");
-        this.uInverseVectorAB = program.getLocation("snapInvVectorAB");
-        this._uLayerNumber = program.getLocation("layerNumber");
-        this._uCoordinateScaler = program.getLocation("coordinateScaler");
-        this._uPointSize = program.getLocation("pointSize");
-    }
-
     _buildVertexShader() {
         const isSnapInit = this._isSnapInit;
         const scene = this._scene;
