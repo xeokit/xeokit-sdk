a7052eddee9a3283a4659ba95019f2cd8b674f12
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-10-18 23:39:58 +0200

    Combine instancing/triangles EdgesColorRenderer and EdgesEmphasisRenderer

--- src/viewer/scene/model/vbo/instancing/triangles/renderers/TrianglesInstancingRenderer.js
+++ src/viewer/scene/model/vbo/instancing/triangles/renderers/TrianglesInstancingRenderer.js
@@ -4,8 +4,8 @@ import {VBORenderer} from "./../../../VBORenderer.js";
  * @private
  */
 export class TrianglesInstancingRenderer extends VBORenderer {
-    constructor(scene, withSAO, {edges = false, useAlphaCutoff = false} = {}) {
-        super(scene, withSAO, {instancing: true, edges, useAlphaCutoff});
+    constructor(scene, withSAO, {edges = false, useAlphaCutoff = false, colorUniform = false} = {}) {
+        super(scene, withSAO, {instancing: true, edges, useAlphaCutoff, colorUniform});
     }
 
     _draw(drawCfg) {
