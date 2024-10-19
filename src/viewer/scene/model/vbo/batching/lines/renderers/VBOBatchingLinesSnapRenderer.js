ff8f93fec50c58e7aec924a908878089769f2777
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-10-19 12:07:06 +0200

    Replace VBOSceneModelLineBatchingRenderer and VBOInstancingLinesRenderer by VBORenderer logic

--- src/viewer/scene/model/vbo/batching/lines/renderers/VBOBatchingLinesSnapRenderer.js
+++ src/viewer/scene/model/vbo/batching/lines/renderers/VBOBatchingLinesSnapRenderer.js
@@ -9,7 +9,7 @@ const SNAPPING_LOG_DEPTH_BUF_ENABLED = true; // Improves occlusion accuracy at d
 export class VBOBatchingLinesSnapRenderer extends VBORenderer {
 
     constructor(scene, isSnapInit) {
-        super(scene, false, { primType: "lineType", progMode: isSnapInit ? "snapInitMode" : "snapMode" });
+        super(scene, false, { instancing: false, primType: "lineType", progMode: isSnapInit ? "snapInitMode" : "snapMode" });
     }
 
     _buildVertexShader() {
