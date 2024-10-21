7460c2b4213148ae29e1e3754b98845c21f23987
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-10-21 20:12:56 +0200

    FIX Remove VBOBatchingPointsSnapRenderer::_getHash override since it relies on unused pointsMaterial

--- src/viewer/scene/model/vbo/batching/points/renderers/VBOBatchingPointsSnapRenderer.js
+++ src/viewer/scene/model/vbo/batching/points/renderers/VBOBatchingPointsSnapRenderer.js
@@ -15,10 +15,6 @@ const SNAPPING_LOG_DEPTH_BUF_ENABLED = true; // Improves occlusion accuracy at d
  * @private
  */
 export class VBOBatchingPointsSnapRenderer extends VBORenderer{
-    _getHash() {
-        return this._scene._sectionPlanesState.getHash() + (this._scene.pointsMaterial.hash);
-    }
-
     drawLayer(frameCtx, batchingLayer, renderPass) {
 
         if (!this._program) {
