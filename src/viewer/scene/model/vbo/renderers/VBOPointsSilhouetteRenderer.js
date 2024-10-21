63d85cbb9ddf4e1ee12a715a1a8eb00ef5be1227
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-10-21 20:08:54 +0200

    Replace VBOBatchingPointsSilhouetteRenderer and VBOInstancingPointsSilhouetteRenderer by VBOPointsSilhouetteRenderer

--- src/viewer/scene/model/vbo/renderers/VBOPointsSilhouetteRenderer.js
+++ src/viewer/scene/model/vbo/renderers/VBOPointsSilhouetteRenderer.js
@@ -1,34 +1,41 @@
-import {VBORenderer} from "../../../VBORenderer.js";
+import {VBORenderer} from "../VBORenderer.js";
 
 /**
  * @private
  */
-export class VBOBatchingPointsSilhouetteRenderer extends VBORenderer {
+export class VBOPointsSilhouetteRenderer extends VBORenderer {
 
-    constructor(scene) {
-        super(scene, false, { instancing: false, primType: "pointType", progMode: "silhouetteMode", hashPointsMaterial: true, colorUniform: true });
+    constructor(scene, instancing) {
+        super(scene, false, { instancing: instancing, primType: "pointType", progMode: "silhouetteMode", hashPointsMaterial: true, colorUniform: true });
     }
 
     _buildVertexShader() {
         const scene = this._scene;
-        const sectionPlanesState = scene._sectionPlanesState;
-        const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
-        const pointsMaterial = scene.pointsMaterial._state;
+        const clipping = scene._sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
         const src = [];
-        src.push ('#version 300 es');
-        src.push("// Points batching silhouette vertex shader");
+        src.push('#version 300 es');
+        src.push("// " + this._primType + " " + this._instancing + " " + this._progMode + " vertex shader");
         src.push("uniform int renderPass;");
-
         src.push("in vec3 position;");
+        if (this._instancing) { // this "if" and "else" are likely something wrong, as the mode seems to rely on silhouetteColor instead
+            src.push("in vec4 color;");
+        } else {
+            src.push("uniform vec4 color;");
+        }
+        src.push("in float flags;");
         if (scene.entityOffsetsEnabled) {
             src.push("in vec3 offset;");
         }
-        src.push("in float flags;");
+
+        if (this._instancing) {
+            src.push("in vec4 modelMatrixCol0;"); // Modeling matrix
+            src.push("in vec4 modelMatrixCol1;");
+            src.push("in vec4 modelMatrixCol2;");
+        }
 
         this._addMatricesUniformBlockLines(src);
 
-        src.push("uniform vec4 color;");
-
+        const pointsMaterial = scene.pointsMaterial._state;
         src.push("uniform float pointSize;");
         if (pointsMaterial.perspectivePoints) {
             src.push("uniform float nearPlaneHeight;");
@@ -38,34 +45,42 @@ export class VBOBatchingPointsSilhouetteRenderer extends VBORenderer {
             src.push("uniform float logDepthBufFC;");
             src.push("out float vFragDepth;");
         }
-
         if (clipping) {
             src.push("out vec4 vWorldPosition;");
             src.push("out float vFlags;");
         }
+        if (this._instancing) {
+            src.push("uniform vec4 silhouetteColor;");
+            src.push("out vec4 vColor;");
+        }
 
         src.push("void main(void) {");
-
         // silhouetteFlag = NOT_RENDERED | SILHOUETTE_HIGHLIGHTED | SILHOUETTE_SELECTED | SILHOUETTE_XRAYED
-        // renderPass = SILHOUETTE_HIGHLIGHTED | SILHOUETTE_SELECTED | | SILHOUETTE_XRAYED
-
+        // renderPass = SILHOUETTE_HIGHLIGHTED | SILHOUETTE_SELECTED | SILHOUETTE_XRAYED
         src.push(`int silhouetteFlag = int(flags) >> 4 & 0xF;`);
         src.push(`if (silhouetteFlag != renderPass) {`);
         src.push("   gl_Position = vec4(0.0, 0.0, 0.0, 0.0);"); // Cull vertex
         src.push("} else {");
-
-        src.push("      vec4 worldPosition = worldMatrix * (positionsDecodeMatrix * vec4(position, 1.0)); ");
-        if (scene.entityOffsetsEnabled) {
-            src.push("      worldPosition.xyz = worldPosition.xyz + offset;");
+        if (this._instancing) {
+            src.push("vec4 worldPosition = positionsDecodeMatrix * vec4(position, 1.0);");
+            src.push("worldPosition = worldMatrix * vec4(dot(worldPosition, modelMatrixCol0), dot(worldPosition, modelMatrixCol1), dot(worldPosition, modelMatrixCol2), 1.0);");
+        } else {
+            src.push("vec4 worldPosition = worldMatrix * (positionsDecodeMatrix * vec4(position, 1.0));");
         }
-        src.push("vec4 viewPosition  = viewMatrix * worldPosition; ");
+        if (scene.entityOffsetsEnabled) {
+            src.push("worldPosition.xyz = worldPosition.xyz + offset;");
+        }
+        src.push("vec4 viewPosition  = viewMatrix * worldPosition;");
+        if (this._instancing) {
+            src.push("vColor = silhouetteColor;");
+        }
         if (clipping) {
             src.push("vWorldPosition = worldPosition;");
             src.push("vFlags = flags;");
         }
         src.push("vec4 clipPos = projMatrix * viewPosition;");
         if (scene.logarithmicDepthBufferEnabled) {
-           src.push("vFragDepth = 1.0 + clipPos.w;");
+            src.push("vFragDepth = 1.0 + clipPos.w;");
         }
         src.push("gl_Position = clipPos;");
         if (pointsMaterial.perspectivePoints) {
@@ -83,12 +98,10 @@ export class VBOBatchingPointsSilhouetteRenderer extends VBORenderer {
     _buildFragmentShader() {
         const scene = this._scene;
         const sectionPlanesState = scene._sectionPlanesState;
-        let i;
-        let len;
         const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
         const src = [];
-        src.push ('#version 300 es');
-        src.push("// Points batching silhouette vertex shader");
+        src.push('#version 300 es');
+        src.push("// " + this._primType + " " + this._instancing + " " + this._progMode + " fragment shader");
         src.push("#ifdef GL_FRAGMENT_PRECISION_HIGH");
         src.push("precision highp float;");
         src.push("precision highp int;");
@@ -103,13 +116,17 @@ export class VBOBatchingPointsSilhouetteRenderer extends VBORenderer {
         if (clipping) {
             src.push("in vec4 vWorldPosition;");
             src.push("in float vFlags;");
-            for (i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
+            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
                 src.push("uniform bool sectionPlaneActive" + i + ";");
                 src.push("uniform vec3 sectionPlanePos" + i + ";");
                 src.push("uniform vec3 sectionPlaneDir" + i + ";");
             }
         }
-        src.push("uniform vec4 color;");
+        if (this._instancing) {
+            src.push("in vec4 vColor;");
+        } else {
+            src.push("uniform vec4 color;");
+        }
         src.push("out vec4 outColor;");
         src.push("void main(void) {");
         if (scene.pointsMaterial.roundPoints) {
@@ -123,7 +140,7 @@ export class VBOBatchingPointsSilhouetteRenderer extends VBORenderer {
             src.push("  bool clippable = (int(vFlags) >> 16 & 0xF) == 1;");
             src.push("  if (clippable) {");
             src.push("  float dist = 0.0;");
-            for (i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
+            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
                 src.push("if (sectionPlaneActive" + i + ") {");
                 src.push("   dist += clamp(dot(-sectionPlaneDir" + i + ".xyz, vWorldPosition.xyz - sectionPlanePos" + i + ".xyz), 0.0, 1000.0);");
                 src.push("}");
@@ -134,9 +151,8 @@ export class VBOBatchingPointsSilhouetteRenderer extends VBORenderer {
         if (scene.logarithmicDepthBufferEnabled) {
             src.push("gl_FragDepth = log2( vFragDepth ) * logDepthBufFC * 0.5;");
         }
-        src.push("outColor = color;");
+        src.push("outColor = " + (this._instancing ? "vColor" : "color") + ";");
         src.push("}");
         return src;
     }
 }
-
