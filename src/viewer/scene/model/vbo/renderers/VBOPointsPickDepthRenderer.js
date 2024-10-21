b2c0771859265ee2dfc728ab60efd12908c6f273
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-10-21 21:57:13 +0200

    Replace VBOBatchingPointsPickDepthRenderer and VBOInstancingPointsPickDepthRenderer by VBOPointsPickDepthRenderer

--- src/viewer/scene/model/vbo/renderers/VBOPointsPickDepthRenderer.js
+++ src/viewer/scene/model/vbo/renderers/VBOPointsPickDepthRenderer.js
@@ -1,41 +1,38 @@
-import {VBORenderer} from "../../../VBORenderer.js";
+import {VBORenderer} from "../VBORenderer.js";
 
 /**
  * @private
  */
-export class VBOInstancingPointsPickDepthRenderer extends VBORenderer {
+export class VBOPointsPickDepthRenderer extends VBORenderer {
 
-    constructor(scene) {
-        super(scene, false, { instancing: true, primType: "pointType", progMode: "pickDepthMode", hashPointsMaterial: true });
+    constructor(scene, instancing) {
+        super(scene, false, { instancing: instancing, primType: "pointType", progMode: "pickDepthMode", hashPointsMaterial: true });
     }
 
     _buildVertexShader() {
         const scene = this._scene;
-        const sectionPlanesState = scene._sectionPlanesState;
-        const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
-        const pointsMaterial = scene.pointsMaterial._state;
+        const clipping = scene._sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
         const src = [];
-
         src.push('#version 300 es');
-        src.push("// Points instancing pick depth vertex shader");
-
+        src.push("// " + this._primType + " " + this._instancing + " " + this._progMode + " vertex shader");
         src.push("uniform int renderPass;");
-
         src.push("in vec3 position;");
-
+        src.push("in float flags;");
         if (scene.entityOffsetsEnabled) {
             src.push("in vec3 offset;");
         }
 
-        src.push("in float flags;");
-        src.push("in vec4 modelMatrixCol0;"); // Modeling matrix
-        src.push("in vec4 modelMatrixCol1;");
-        src.push("in vec4 modelMatrixCol2;");
+        if (this._instancing) {
+            src.push("in vec4 modelMatrixCol0;"); // Modeling matrix
+            src.push("in vec4 modelMatrixCol1;");
+            src.push("in vec4 modelMatrixCol2;");
+        }
 
         this._addMatricesUniformBlockLines(src);
 
         this._addRemapClipPosLines(src);
 
+        const pointsMaterial = scene.pointsMaterial._state;
         src.push("uniform float pointSize;");
         if (pointsMaterial.perspectivePoints) {
             src.push("uniform float nearPlaneHeight;");
@@ -45,37 +42,35 @@ export class VBOInstancingPointsPickDepthRenderer extends VBORenderer {
             src.push("uniform float logDepthBufFC;");
             src.push("out float vFragDepth;");
         }
-
         if (clipping) {
             src.push("out vec4 vWorldPosition;");
             src.push("out float vFlags;");
         }
-
         src.push("out vec4 vViewPosition;");
-        src.push("void main(void) {");
 
+        src.push("void main(void) {");
         // pickFlag = NOT_RENDERED | PICK
         // renderPass = PICK
-
         src.push(`int pickFlag = int(flags) >> 12 & 0xF;`);
         src.push(`if (pickFlag != renderPass) {`);
-        src.push("   gl_Position = vec4(0.0, 0.0, 0.0, 0.0);"); // Cull vertex
-
+        src.push("gl_Position = vec4(0.0, 0.0, 0.0, 0.0);"); // Cull vertex
         src.push("} else {");
-        src.push("  vec4 worldPosition = positionsDecodeMatrix * vec4(position, 1.0); ");
-        src.push("  worldPosition = worldMatrix * vec4(dot(worldPosition, modelMatrixCol0), dot(worldPosition, modelMatrixCol1), dot(worldPosition, modelMatrixCol2), 1.0);");
-
+        if (this._instancing) {
+            src.push("vec4 worldPosition = positionsDecodeMatrix * vec4(position, 1.0);");
+            src.push("worldPosition = worldMatrix * vec4(dot(worldPosition, modelMatrixCol0), dot(worldPosition, modelMatrixCol1), dot(worldPosition, modelMatrixCol2), 1.0);");
+        } else {
+            src.push("vec4 worldPosition = worldMatrix * (positionsDecodeMatrix * vec4(position, 1.0));");
+        }
         if (scene.entityOffsetsEnabled) {
-            src.push("      worldPosition.xyz = worldPosition.xyz + offset;");
+            src.push("worldPosition.xyz = worldPosition.xyz + offset;");
         }
-        src.push("  vec4 viewPosition  = viewMatrix * worldPosition; ");
+        src.push("vec4 viewPosition  = viewMatrix * worldPosition;");
+        src.push("vViewPosition = viewPosition;");
         if (clipping) {
-            src.push("  vWorldPosition = worldPosition;");
-            src.push("  vFlags = flags;");
+            src.push("vWorldPosition = worldPosition;");
+            src.push("vFlags = flags;");
         }
-        src.push("  vViewPosition = viewPosition;");
         src.push("vec4 clipPos = projMatrix * viewPosition;");
-        src.push("gl_Position = remapClipPos(clipPos);");
         if (scene.logarithmicDepthBufferEnabled) {
             src.push("vFragDepth = 1.0 + clipPos.w;");
         }
@@ -99,8 +94,7 @@ export class VBOInstancingPointsPickDepthRenderer extends VBORenderer {
         const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
         const src = [];
         src.push('#version 300 es');
-        src.push("// Points instancing pick depth fragment shader");
-
+        src.push("// " + this._primType + " " + this._instancing + " " + this._progMode + " fragment shader");
         src.push("#ifdef GL_FRAGMENT_PRECISION_HIGH");
         src.push("precision highp float;");
         src.push("precision highp int;");
@@ -108,7 +102,6 @@ export class VBOInstancingPointsPickDepthRenderer extends VBORenderer {
         src.push("precision mediump float;");
         src.push("precision mediump int;");
         src.push("#endif");
-
         if (scene.logarithmicDepthBufferEnabled) {
             src.push("uniform float logDepthBufFC;");
             src.push("in float vFragDepth;");
@@ -120,7 +113,7 @@ export class VBOInstancingPointsPickDepthRenderer extends VBORenderer {
         if (clipping) {
             src.push("in vec4 vWorldPosition;");
             src.push("in float vFlags;");
-            for (let i = 0; i < sectionPlanesState.getNumAllocatedSectionPlanes(); i++) {
+            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
                 src.push("uniform bool sectionPlaneActive" + i + ";");
                 src.push("uniform vec3 sectionPlanePos" + i + ";");
                 src.push("uniform vec3 sectionPlaneDir" + i + ";");
@@ -136,7 +129,6 @@ export class VBOInstancingPointsPickDepthRenderer extends VBORenderer {
         src.push("}");
         src.push("out vec4 outColor;");
         src.push("void main(void) {");
-
         if (scene.pointsMaterial.roundPoints) {
             src.push("  vec2 cxy = 2.0 * gl_PointCoord - 1.0;");
             src.push("  float r = dot(cxy, cxy);");
@@ -144,24 +136,23 @@ export class VBOInstancingPointsPickDepthRenderer extends VBORenderer {
             src.push("       discard;");
             src.push("  }");
         }
-
         if (clipping) {
             src.push("  bool clippable = (int(vFlags) >> 16 & 0xF) == 1;");
             src.push("  if (clippable) {");
             src.push("  float dist = 0.0;");
-            for (let i = 0; i < sectionPlanesState.getNumAllocatedSectionPlanes(); i++) {
+            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
                 src.push("if (sectionPlaneActive" + i + ") {");
                 src.push("   dist += clamp(dot(-sectionPlaneDir" + i + ".xyz, vWorldPosition.xyz - sectionPlanePos" + i + ".xyz), 0.0, 1000.0);");
                 src.push("}");
             }
-            src.push("if (dist > 0.0) { discard; }");
+            src.push("  if (dist > 0.0) { discard; }");
             src.push("}");
         }
         if (scene.logarithmicDepthBufferEnabled) {
             src.push("gl_FragDepth = log2( vFragDepth ) * logDepthBufFC * 0.5;");
         }
-        src.push("    float zNormalizedDepth = abs((pickZNear + vViewPosition.z) / (pickZFar - pickZNear));");
-        src.push("    outColor = packDepth(zNormalizedDepth); "); // Must be linear depth
+        src.push("float zNormalizedDepth = abs((pickZNear + vViewPosition.z) / (pickZFar - pickZNear));");
+        src.push("outColor = packDepth(zNormalizedDepth);"); // Must be linear depth
         src.push("}");
         return src;
     }
