f19a7597789bc154adfd313e45c8b291cfa95af1
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-10-22 15:13:58 +0200

    Replace batching and instancing TrianglesPickNormalsRenderers by VBOTrianglesPickNormalsRenderer

--- src/viewer/scene/model/vbo/renderers/VBOTrianglesPickNormalsRenderer.js
+++ src/viewer/scene/model/vbo/renderers/VBOTrianglesPickNormalsRenderer.js
@@ -1,28 +1,37 @@
-import {VBORenderer} from "./../../../VBORenderer.js";
-import { math } from "../../../../../math/math.js";
+import {VBORenderer} from "../VBORenderer.js";
+import { math } from "../../../math/math.js";
 
 /**
  * @private
  */
-export class TrianglesPickNormalsRenderer extends VBORenderer {
-    constructor(scene) {
-        super(scene, false, { instancing: false, primType: "triangleType", progMode: "pickNormalsMode" });
+export class VBOTrianglesPickNormalsRenderer extends VBORenderer {
+
+    constructor(scene, instancing) {
+        super(scene, false, { instancing: instancing, primType: "triangleType", progMode: "pickNormalsMode" });
     }
 
     _buildVertexShader() {
         const scene = this._scene;
         const clipping = scene._sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
         const src = [];
-        src.push("#version 300 es");
-        src.push("// Triangles batching pick normals vertex shader");
-        
+        src.push('#version 300 es');
+        src.push("// " + this._primType + " " + this._instancing + " " + this._progMode + " vertex shader");
         src.push("uniform int renderPass;");
         src.push("in vec3 position;");
+        src.push("in vec3 normal;");
+        src.push("in float flags;");
         if (scene.entityOffsetsEnabled) {
             src.push("in vec3 offset;");
         }
-        src.push("in vec3 normal;");
-        src.push("in float flags;");
+
+        if (this._instancing) {
+            src.push("in vec4 modelMatrixCol0;"); // Modeling matrix
+            src.push("in vec4 modelMatrixCol1;");
+            src.push("in vec4 modelMatrixCol2;");
+            src.push("in vec4 modelNormalMatrixCol0;");
+            src.push("in vec4 modelNormalMatrixCol1;");
+            src.push("in vec4 modelNormalMatrixCol2;");
+        }
 
         this._addMatricesUniformBlockLines(src);
 
@@ -48,31 +57,42 @@ export class TrianglesPickNormalsRenderer extends VBORenderer {
             src.push("out float vFlags;");
         }
         src.push("out vec3 vWorldNormal;");
+
         src.push("void main(void) {");
         // pickFlag = NOT_RENDERED | PICK
         // renderPass = PICK
         src.push(`int pickFlag = int(flags) >> 12 & 0xF;`);
         src.push(`if (pickFlag != renderPass) {`);
-        src.push("      gl_Position = vec4(0.0, 0.0, 0.0, 0.0);"); // Cull vertex
-        src.push("  } else {");
-        src.push("      vec4 worldPosition = worldMatrix * (positionsDecodeMatrix * vec4(position, 1.0)); ");
-        if (scene.entityOffsetsEnabled) {
-            src.push("      worldPosition.xyz = worldPosition.xyz + offset;");
+        src.push("gl_Position = vec4(0.0, 0.0, 0.0, 0.0);"); // Cull vertex
+        src.push("} else {");
+        if (this._instancing) {
+            src.push("vec4 worldPosition = positionsDecodeMatrix * vec4(position, 1.0);");
+            src.push("worldPosition = worldMatrix * vec4(dot(worldPosition, modelMatrixCol0), dot(worldPosition, modelMatrixCol1), dot(worldPosition, modelMatrixCol2), 1.0);");
+        } else {
+            src.push("vec4 worldPosition = worldMatrix * (positionsDecodeMatrix * vec4(position, 1.0));");
         }
-        src.push("      vec4 viewPosition  = viewMatrix * worldPosition; ");
-        src.push("      vec3 worldNormal =  octDecode(normal.xy); ");
-        src.push("      vWorldNormal = worldNormal;");
+        if (scene.entityOffsetsEnabled) {
+            src.push("worldPosition.xyz = worldPosition.xyz + offset;");
+        }
+        src.push("vec4 viewPosition  = viewMatrix * worldPosition;");
+        if (this._instancing) {
+            src.push("vec4 modelNormal = vec4(octDecode(normal.xy), 0.0);");
+            src.push("vec3 worldNormal = vec3(dot(modelNormal, modelNormalMatrixCol0), dot(modelNormal, modelNormalMatrixCol1), dot(modelNormal, modelNormalMatrixCol2));");
+        } else {
+            src.push("vec3 worldNormal =  octDecode(normal.xy);");
+        }
+        src.push("vWorldNormal = worldNormal;");
         if (clipping) {
-            src.push("      vWorldPosition = worldPosition;");
-            src.push("      vFlags = flags;");
+            src.push("vWorldPosition = worldPosition;");
+            src.push("vFlags = flags;");
         }
         src.push("vec4 clipPos = projMatrix * viewPosition;");
         if (scene.logarithmicDepthBufferEnabled) {
-           src.push("vFragDepth = 1.0 + clipPos.w;");
+            src.push("vFragDepth = 1.0 + clipPos.w;");
             src.push("isPerspective = float (isPerspectiveMatrix(projMatrix));");
         }
         src.push("gl_Position = remapClipPos(clipPos);");
-        src.push("  }");
+        src.push("}");
         src.push("}");
         return src;
     }
@@ -82,9 +102,8 @@ export class TrianglesPickNormalsRenderer extends VBORenderer {
         const sectionPlanesState = scene._sectionPlanesState;
         const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
         const src = [];
-        src.push("#version 300 es");
-        src.push("// Triangles batching pick normals fragment shader");
-        
+        src.push('#version 300 es');
+        src.push("// " + this._primType + " " + this._instancing + " " + this._progMode + " fragment shader");
         src.push("#ifdef GL_FRAGMENT_PRECISION_HIGH");
         src.push("precision highp float;");
         src.push("precision highp int;");
@@ -100,7 +119,7 @@ export class TrianglesPickNormalsRenderer extends VBORenderer {
         if (clipping) {
             src.push("in vec4 vWorldPosition;");
             src.push("in float vFlags;");
-            for (var i = 0; i < sectionPlanesState.getNumAllocatedSectionPlanes(); i++) {
+            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
                 src.push("uniform bool sectionPlaneActive" + i + ";");
                 src.push("uniform vec3 sectionPlanePos" + i + ";");
                 src.push("uniform vec3 sectionPlaneDir" + i + ";");
@@ -112,19 +131,19 @@ export class TrianglesPickNormalsRenderer extends VBORenderer {
         if (clipping) {
             src.push("  bool clippable = (int(vFlags) >> 16 & 0xF) == 1;");
             src.push("  if (clippable) {");
-            src.push("      float dist = 0.0;");
-            for (let i = 0; i < sectionPlanesState.getNumAllocatedSectionPlanes(); i++) {
-                src.push("      if (sectionPlaneActive" + i + ") {");
-                src.push("          dist += clamp(dot(-sectionPlaneDir" + i + ".xyz, vWorldPosition.xyz - sectionPlanePos" + i + ".xyz), 0.0, 1000.0);");
-                src.push("      }");
+            src.push("  float dist = 0.0;");
+            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
+                src.push("if (sectionPlaneActive" + i + ") {");
+                src.push("   dist += clamp(dot(-sectionPlaneDir" + i + ".xyz, vWorldPosition.xyz - sectionPlanePos" + i + ".xyz), 0.0, 1000.0);");
+                src.push("}");
             }
-            src.push("      if (dist > 0.0) { discard; }");
-            src.push("  }");
+            src.push("  if (dist > 0.0) { discard; }");
+            src.push("}");
         }
         if (scene.logarithmicDepthBufferEnabled) {
-            src.push("    gl_FragDepth = isPerspective == 0.0 ? gl_FragCoord.z : log2( vFragDepth ) * logDepthBufFC * 0.5;");
+            src.push("gl_FragDepth = isPerspective == 0.0 ? gl_FragCoord.z : log2( vFragDepth ) * logDepthBufFC * 0.5;");
         }
-        src.push(`    outNormal = ivec4(vWorldNormal * float(${math.MAX_INT}), 1.0);`);
+        src.push(`outNormal = ivec4(vWorldNormal * float(${math.MAX_INT}), 1.0);`);
         src.push("}");
         return src;
     }
