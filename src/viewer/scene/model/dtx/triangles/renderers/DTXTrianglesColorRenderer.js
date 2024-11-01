f3c2142b34329d433296e62782a887429220ca12
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-01 22:54:07 +0100

    Remove unused uTexturePerPolygonIdNormals references

--- src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesColorRenderer.js
+++ src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesColorRenderer.js
@@ -242,7 +242,6 @@ export class DTXTrianglesColorRenderer {
         this.uTexturePerObjectPositionsDecodeMatrix = "uObjectPerObjectPositionsDecodeMatrix";
         this.uTexturePerObjectColorsAndFlags = "uObjectPerObjectColorsAndFlags";
         this._uTexturePerVertexIdCoordinates = "uTexturePerVertexIdCoordinates";
-        this._uTexturePerPolygonIdNormals = "uTexturePerPolygonIdNormals";
         this._uTexturePerPolygonIdIndices = "uTexturePerPolygonIdIndices";
         this._uTexturePerPolygonIdPortionIds = "uTexturePerPolygonIdPortionIds";
         this._uTexturePerObjectMatrix= "uTexturePerObjectMatrix";
