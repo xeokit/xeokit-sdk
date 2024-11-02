45401c1bd0807742ee906c731a76d8654057cc76
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-02 15:00:21 +0100

    Remove unused "in vec3 offset;" declarations, DTXTrianglesTextureFactory::createTextureForObjectOffsets, uObjectPerObjectOffsets sampler, and ineffective DTXTrianglesLayer::_subPortionSetOffset

--- src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapRenderer.js
+++ src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapRenderer.js
@@ -257,10 +257,6 @@ export class DTXTrianglesSnapRenderer {
 
         src.push("uniform int renderPass;");
 
-        if (scene.entityOffsetsEnabled) {
-            src.push("in vec3 offset;");
-        }
-
         src.push("uniform mat4 sceneModelMatrix;");
         src.push("uniform mat4 viewMatrix;");
         src.push("uniform mat4 projMatrix;");
