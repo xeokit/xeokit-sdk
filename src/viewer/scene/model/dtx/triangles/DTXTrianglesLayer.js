65537366737c80e98c0d1fc10e2ecad954c78815
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-02 15:00:21 +0100

    Remove unused "in vec3 offset;" declarations, DTXTrianglesTextureFactory::createTextureForObjectOffsets, uObjectPerObjectOffsets sampler, and ineffective DTXTrianglesLayer::_subPortionSetOffset

--- src/viewer/scene/model/dtx/triangles/DTXTrianglesLayer.js
+++ src/viewer/scene/model/dtx/triangles/DTXTrianglesLayer.js
@@ -1002,47 +1002,7 @@ export class DTXTrianglesLayer {
     }
 
     setOffset(portionId, offset) {
-        const subPortionIds = this._portionToSubPortionsMap[portionId];
-        for (let i = 0, len = subPortionIds.length; i < len; i++) {
-            this._subPortionSetOffset(subPortionIds[i], offset);
-        }
-    }
-
-    _subPortionSetOffset(subPortionId, offset) {
-        if (!this._finalized) {
-            throw "Not finalized";
-        }
-        // if (!this.model.scene.entityOffsetsEnabled) {
-        //     this.model.error("Entity#offset not enabled for this Viewer"); // See Viewer entityOffsetsEnabled
-        //     return;
-        // }
-        const textureState = this._dtxState;
-        const gl = this.model.scene.canvas.gl;
-        tempFloat32Array3 [0] = offset[0];
-        tempFloat32Array3 [1] = offset[1];
-        tempFloat32Array3 [2] = offset[2];
-        // object offset
-        textureState.texturePerObjectOffsets._textureData.set(tempFloat32Array3, subPortionId * 3);
-        if (this._deferredSetFlagsActive) {
-            this._deferredSetFlagsDirty = true;
-            return;
-        }
-        if (++this._numUpdatesInFrame >= MAX_OBJECT_UPDATES_IN_FRAME_WITHOUT_BATCHED_UPDATE) {
-            this._beginDeferredFlags(); // Subsequent flags updates now deferred
-        }
-        gl.bindTexture(gl.TEXTURE_2D, textureState.texturePerObjectOffsets._texture);
-        gl.texSubImage2D(
-            gl.TEXTURE_2D,
-            0, // level
-            0, // x offset
-            subPortionId, // yoffset
-            1, // width
-            1, // height
-            gl.RGB,
-            gl.FLOAT,
-            tempFloat32Array3
-        );
-        // gl.bindTexture (gl.TEXTURE_2D, null);
+        // NOT COMPLETE
     }
 
     setMatrix(portionId, matrix) {
