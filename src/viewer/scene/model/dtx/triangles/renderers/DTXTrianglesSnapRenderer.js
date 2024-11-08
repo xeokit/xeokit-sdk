3fe27d0d8f3ddccf2bde10c181995ca3107780a3
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-08 18:26:55 +0100

    Revert "Remove reallocation from drawLayer"
    This reverts commit fa8e33c96cec5a173ef78e8a6e24bc0bbb331f68.

--- src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapRenderer.js
+++ src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapRenderer.js
@@ -88,6 +88,13 @@ export class DTXTrianglesSnapRenderer {
     }
 
     drawLayer(frameCtx, dataTextureLayer, renderPass) {
+        if (!this._program) {
+            this._allocate();
+            if (!this._program) {
+                return;
+            }
+        }
+
         const program = this._program;
 
         if (frameCtx.lastProgramId !== program.id) {
