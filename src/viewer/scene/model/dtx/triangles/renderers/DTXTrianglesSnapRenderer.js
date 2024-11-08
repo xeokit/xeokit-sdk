ad7b75b9b6414daad3612fe62010e9313eaf9e76
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-08 18:28:51 +0100

    Remove reallocation from drawLayer

--- src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapRenderer.js
+++ src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapRenderer.js
@@ -89,10 +89,7 @@ export class DTXTrianglesSnapRenderer {
 
     drawLayer(frameCtx, dataTextureLayer, renderPass) {
         if (!this._program) {
-            this._allocate();
-            if (!this._program) {
-                return;
-            }
+            return;
         }
 
         const program = this._program;
