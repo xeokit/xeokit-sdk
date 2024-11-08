819d3e6ff422ab62dec50151e545570c18ebcb0d
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-08 15:41:41 +0100

    Migrate DTXTriangles{Color,Depth,EdgesColor,Edges,Occlusion,Silhouette,SnapInit,Snap}Renderer.js to _getViewParams

--- src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapInitRenderer.js
+++ src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesSnapInitRenderer.js
@@ -21,9 +21,13 @@ export class DTXTrianglesSnapInitRenderer {
 
         this._programName = "DTXTrianglesSnapInitRenderer";
         this._useLogDepthBuffer = true; // Improves occlusion accuracy at distance
-        this._getLogDepthFar = (frameCtx, cameraProjectFar) => frameCtx.pickProjMatrix ? frameCtx.pickZFar : cameraProjectFar;
         this._fragDepthDiff = (vFragDepth) => `length(vec2(dFdx(${vFragDepth}), dFdy(${vFragDepth})))`;
-        this._usePickMatrix = true;
+        this._getViewParams = (frameCtx, camera) => ({
+            viewMatrix: frameCtx.pickViewMatrix || camera.viewMatrix,
+            projMatrix: frameCtx.pickProjMatrix || camera.projMatrix,
+            eye: frameCtx.pickOrigin || camera.eye,
+            far: frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far
+        });
         // flags.w = NOT_RENDERED | PICK
         // renderPass = PICK
         this._renderPassFlag = "w";
@@ -111,16 +115,12 @@ export class DTXTrianglesSnapInitRenderer {
 
         const scene = this._scene;
         const gl = scene.canvas.gl;
-        const camera = scene.camera;
         const model = dataTextureLayer.model;
         const state = dataTextureLayer._state;
         const textureState = state.textureState;
         const origin = dataTextureLayer._state.origin;
         const {position, rotationMatrix} = model;
-        const viewMatrix = (this._usePickMatrix && frameCtx.pickViewMatrix) || camera.viewMatrix;
-        const projMatrix = frameCtx.pickProjMatrix || camera.projMatrix;
-        const eye = frameCtx.pickOrigin || camera.eye;
-        const far = frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far;
+        const viewParams = this._getViewParams(frameCtx, scene.camera);
 
         textureState.bindCommonTextures(
             program,
@@ -141,21 +141,21 @@ export class DTXTrianglesSnapInitRenderer {
                 math.transformPoint3(rotationMatrix, origin, rtcOrigin);
             }
             math.addVec3(rtcOrigin, position, rtcOrigin);
-            rtcViewMatrix = createRTCViewMat(viewMatrix, rtcOrigin, tempMat4a);
+            rtcViewMatrix = createRTCViewMat(viewParams.viewMatrix, rtcOrigin, tempMat4a);
         } else {
-            rtcViewMatrix = viewMatrix;
+            rtcViewMatrix = viewParams.viewMatrix;
         }
 
         gl.uniformMatrix4fv(this._uSceneModelMatrix, false, rotationMatrix);
         gl.uniformMatrix4fv(this._uViewMatrix, false, rtcViewMatrix);
-        gl.uniformMatrix4fv(this._uProjMatrix, false, projMatrix);
-        gl.uniform3fv(this._uCameraEyeRtc, math.subVec3(eye, rtcOrigin, tempVec3b));
+        gl.uniformMatrix4fv(this._uProjMatrix, false, viewParams.projMatrix);
+        gl.uniform3fv(this._uCameraEyeRtc, math.subVec3(viewParams.eye, rtcOrigin, tempVec3b));
         gl.uniform1i(this._uRenderPass, renderPass);
 
         this._setRenderState(frameCtx, dataTextureLayer, renderPass, rtcOrigin);
 
         if (this._useLogDepthBuffer) {
-            const logDepthBufFC = 2.0 / (Math.log(this._getLogDepthFar(frameCtx, camera.project.far) + 1.0) / Math.LN2);
+            const logDepthBufFC = 2.0 / (Math.log(viewParams.far + 1.0) / Math.LN2);
             gl.uniform1f(this._uLogDepthBufFC, logDepthBufFC);
         }
 
