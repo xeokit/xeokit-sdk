73c5a4cb847de66501f06d0e9a7ac63bbde3ef6f
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-14 18:55:19 +0100

    FIX Incorrect viewPosition reference in batching and instancing TrianglesFlatColorRenderer

--- src/viewer/scene/model/vbo/instancing/triangles/renderers/TrianglesFlatColorRenderer.js
+++ src/viewer/scene/model/vbo/instancing/triangles/renderers/TrianglesFlatColorRenderer.js
@@ -207,7 +207,7 @@ export class TrianglesFlatColorRenderer extends TrianglesInstancingRenderer {
                 }
             } else if (light.type === "point") {
                 if (light.space === "view") {
-                    src.push("viewLightDir = -normalize(lightPos" + i + " - viewPosition.xyz);");
+                    src.push("viewLightDir = -normalize(lightPos" + i + " - vViewPosition.xyz);");
                 } else {
                     src.push("viewLightDir = -normalize((viewMatrix * vec4(lightPos" + i + ", 0.0)).xyz);");
                 }
