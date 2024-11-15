8f0a9924ee936b1eddb495d815874196b9c21146
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-15 16:56:29 +0100

    Migrate VBOTrianglesPick{Depth,Mesh,NormalsFlat,Normals}Renderer to VBORenderer

--- src/viewer/scene/model/vbo/renderers/VBOTrianglesPickDepthRenderer.js
+++ src/viewer/scene/model/vbo/renderers/VBOTrianglesPickDepthRenderer.js
@@ -6,135 +6,52 @@ import {VBORenderer} from "../VBORenderer.js";
 export class VBOTrianglesPickDepthRenderer extends VBORenderer {
 
     constructor(scene, instancing, primitive) {
-        super(scene, instancing, primitive, false, { progMode: "pickDepthMode" });
-    }
+        super(scene, instancing, primitive, false, {
+            progMode: "pickDepthMode",
 
-    _buildVertexShader() {
-        const scene = this._scene;
-        const clipping = scene._sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
-        const src = [];
-        src.push("uniform int renderPass;");
-        src.push("in vec3 position;");
-        src.push("in float flags;");
-        if (scene.entityOffsetsEnabled) {
-            src.push("in vec3 offset;");
-        }
-
-        if (this._instancing) {
-            src.push("in vec4 modelMatrixCol0;"); // Modeling matrix
-            src.push("in vec4 modelMatrixCol1;");
-            src.push("in vec4 modelMatrixCol2;");
-        }
-
-        this._addMatricesUniformBlockLines(src);
-
-        this._addRemapClipPosLines(src);
-
-        if (scene.logarithmicDepthBufferEnabled) {
-            src.push("uniform float logDepthBufFC;");
-            src.push("out float vFragDepth;");
-            src.push("bool isPerspectiveMatrix(mat4 m) {");
-            src.push("    return (m[2][3] == - 1.0);");
-            src.push("}");
-            src.push("out float isPerspective;");
-        }
-        if (clipping) {
-            src.push("out vec4 vWorldPosition;");
-            src.push("out float vFlags;");
-        }
-        src.push("out vec4 vViewPosition;");
-
-        src.push("void main(void) {");
-        // pickFlag = NOT_RENDERED | PICK
-        // renderPass = PICK
-        src.push(`int pickFlag = int(flags) >> 12 & 0xF;`);
-        src.push(`if (pickFlag != renderPass) {`);
-        src.push("gl_Position = vec4(0.0, 0.0, 0.0, 0.0);"); // Cull vertex
-        src.push("} else {");
-        if (this._instancing) {
-            src.push("vec4 worldPosition = positionsDecodeMatrix * vec4(position, 1.0);");
-            src.push("worldPosition = worldMatrix * vec4(dot(worldPosition, modelMatrixCol0), dot(worldPosition, modelMatrixCol1), dot(worldPosition, modelMatrixCol2), 1.0);");
-        } else {
-            src.push("vec4 worldPosition = worldMatrix * (positionsDecodeMatrix * vec4(position, 1.0));");
-        }
-        if (scene.entityOffsetsEnabled) {
-            src.push("worldPosition.xyz = worldPosition.xyz + offset;");
-        }
-        src.push("vec4 viewPosition  = viewMatrix * worldPosition;");
-        src.push("vViewPosition = viewPosition;");
-        if (clipping) {
-            src.push("vWorldPosition = worldPosition;");
-            src.push("vFlags = flags;");
-        }
-        src.push("vec4 clipPos = projMatrix * viewPosition;");
-        if (scene.logarithmicDepthBufferEnabled) {
-            src.push("vFragDepth = 1.0 + clipPos.w;");
-            src.push("isPerspective = float (isPerspectiveMatrix(projMatrix));");
-        }
-        src.push("gl_Position = remapClipPos(clipPos);");
-        src.push("}");
-        src.push("}");
-        return src;
-    }
-
-    _buildFragmentShader() {
-        const scene = this._scene;
-        const sectionPlanesState = scene._sectionPlanesState;
-        const clipping = sectionPlanesState.getNumAllocatedSectionPlanes() > 0;
-        const src = [];
-        src.push("#ifdef GL_FRAGMENT_PRECISION_HIGH");
-        src.push("precision highp float;");
-        src.push("precision highp int;");
-        src.push("#else");
-        src.push("precision mediump float;");
-        src.push("precision mediump int;");
-        src.push("#endif");
-        if (scene.logarithmicDepthBufferEnabled) {
-            src.push("in float isPerspective;");
-            src.push("uniform float logDepthBufFC;");
-            src.push("in float vFragDepth;");
-        }
-
-        src.push("uniform float pickZNear;");
-        src.push("uniform float pickZFar;");
-
-        if (clipping) {
-            src.push("in vec4 vWorldPosition;");
-            src.push("in float vFlags;");
-            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
-                src.push("uniform bool sectionPlaneActive" + i + ";");
-                src.push("uniform vec3 sectionPlanePos" + i + ";");
-                src.push("uniform vec3 sectionPlaneDir" + i + ";");
-            }
-        }
-        src.push("in vec4 vViewPosition;");
-        src.push("vec4 packDepth(const in float depth) {");
-        src.push("  const vec4 bitShift = vec4(256.0*256.0*256.0, 256.0*256.0, 256.0, 1.0);");
-        src.push("  const vec4 bitMask  = vec4(0.0, 1.0/256.0, 1.0/256.0, 1.0/256.0);");
-        src.push("  vec4 res = fract(depth * bitShift);");
-        src.push("  res -= res.xxyz * bitMask;");
-        src.push("  return res;");
-        src.push("}");
-        src.push("out vec4 outColor;");
-        src.push("void main(void) {");
-        if (clipping) {
-            src.push("  bool clippable = (int(vFlags) >> 16 & 0xF) == 1;");
-            src.push("  if (clippable) {");
-            src.push("  float dist = 0.0;");
-            for (let i = 0, len = sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
-                src.push("if (sectionPlaneActive" + i + ") {");
-                src.push("   dist += clamp(dot(-sectionPlaneDir" + i + ".xyz, vWorldPosition.xyz - sectionPlanePos" + i + ".xyz), 0.0, 1000.0);");
+            getHash: () => [ ],
+            getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+            clippingCaps: false,
+            // pickFlag = NOT_RENDERED | PICK
+            // renderPass = PICK
+            renderPassFlag: 3,
+            appendVertexDefinitions: (src) => {
+                src.push("uniform vec2 pickClipPos;");
+                src.push("uniform vec2 drawingBufferSize;");
+                src.push("out vec4 vViewPosition;");
+            },
+            transformClipPos: clipPos => `vec4((${clipPos}.xy / ${clipPos}.w - pickClipPos) * drawingBufferSize * ${clipPos}.w, ${clipPos}.zw)`,
+            shadowParameters: null,
+            needVertexColor: false,
+            needPickColor: false,
+            needGl_Position: false,
+            needViewPosition: true,
+            needViewMatrixNormal: false,
+            needWorldNormal: false,
+            appendVertexOutputs: (src, color, pickColor, gl_Position, view, worldNormal) => src.push(`vViewPosition = ${view.viewPosition};`),
+            appendFragmentDefinitions: (src) => {
+                src.push("uniform float pickZNear;");
+                src.push("uniform float pickZFar;");
+                src.push("in vec4 vViewPosition;");
+                src.push("vec4 packDepth(const in float depth) {");
+                src.push("  const vec4 bitShift = vec4(256.0*256.0*256.0, 256.0*256.0, 256.0, 1.0);");
+                src.push("  const vec4 bitMask  = vec4(0.0, 1.0/256.0, 1.0/256.0, 1.0/256.0);");
+                src.push("  vec4 res = fract(depth * bitShift);");
+                src.push("  res -= res.xxyz * bitMask;");
+                src.push("  return res;");
                 src.push("}");
+                src.push("out vec4 outColor;");
+            },
+            sectionDiscardThreshold: "0.0",
+            needSliced: false,
+            needvWorldPosition: false,
+            needGl_FragCoord: false,
+            needViewMatrixInFragment: false,
+            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliced, viewMatrix) => {
+                src.push("float zNormalizedDepth = abs((pickZNear + vViewPosition.z) / (pickZFar - pickZNear));");
+                src.push("outColor = packDepth(zNormalizedDepth);"); // Must be linear depth
             }
-            src.push("  if (dist > 0.0) { discard; }");
-            src.push("}");
-        }
-        if (scene.logarithmicDepthBufferEnabled) {
-            src.push("gl_FragDepth = isPerspective == 0.0 ? gl_FragCoord.z : log2( vFragDepth ) * logDepthBufFC * 0.5;");
-        }
-        src.push("float zNormalizedDepth = abs((pickZNear + vViewPosition.z) / (pickZFar - pickZNear));");
-        src.push("outColor = packDepth(zNormalizedDepth);"); // Must be linear depth
-        src.push("}");
-        return src;
+        });
     }
+
 }
