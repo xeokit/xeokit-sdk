99fd1f4ab721358afae0cc659cf76676149a436f
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-17 14:55:49 +0100

    Combine VBO{Points,Triangles}PickDepthRenderer into VBOPickDepthRenderer

--- src/viewer/scene/model/vbo/renderers/VBOPickDepthRenderer.js
+++ src/viewer/scene/model/vbo/renderers/VBOPickDepthRenderer.js
@@ -3,15 +3,16 @@ import {VBORenderer} from "../VBORenderer.js";
 /**
  * @private
  */
-export class VBOPointsPickDepthRenderer extends VBORenderer {
+export class VBOPickDepthRenderer extends VBORenderer {
 
     constructor(scene, instancing, primitive) {
+        const isPoints = primitive === "points";
         const pointsMaterial = scene.pointsMaterial;
 
         super(scene, instancing, primitive, {
             progMode: "pickDepthMode",
 
-            getHash: () => [ pointsMaterial.hash ],
+            getHash: isPoints ? (() => [ pointsMaterial.hash ]) : (() => [ ]),
             getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
             clippingCaps: false,
             // pickFlag = NOT_RENDERED | PICK
@@ -21,9 +22,11 @@ export class VBOPointsPickDepthRenderer extends VBORenderer {
                 src.push("uniform vec2 pickClipPos;");
                 src.push("uniform vec2 drawingBufferSize;");
                 src.push("out vec4 vViewPosition;");
-                src.push("uniform float pointSize;");
-                if (pointsMaterial.perspectivePoints) {
-                    src.push("uniform float nearPlaneHeight;");
+                if (isPoints) {
+                    src.push("uniform float pointSize;");
+                    if (pointsMaterial.perspectivePoints) {
+                        src.push("uniform float nearPlaneHeight;");
+                    }
                 }
             },
             filterIntensityRange: false,
@@ -33,21 +36,23 @@ export class VBOPointsPickDepthRenderer extends VBORenderer {
             needPickColor: false,
             needUV: false,
             needMetallicRoughness: false,
-            needGl_Position: pointsMaterial.perspectivePoints,
+            needGl_Position: isPoints && pointsMaterial.perspectivePoints,
             needViewPosition: true,
             needViewMatrixNormal: false,
             needWorldNormal: false,
             needWorldPosition: false,
             appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
                 src.push(`vViewPosition = ${view.viewPosition};`);
-                if (pointsMaterial.perspectivePoints) {
-                    src.push(`gl_PointSize = (nearPlaneHeight * pointSize) / ${gl_Position}.w;`);
-                    src.push("gl_PointSize = max(gl_PointSize, " + Math.floor(pointsMaterial.minPerspectivePointSize) + ".0);");
-                    src.push("gl_PointSize = min(gl_PointSize, " + Math.floor(pointsMaterial.maxPerspectivePointSize) + ".0);");
-                } else {
-                    src.push("gl_PointSize = pointSize;");
+                if (isPoints) {
+                    if (pointsMaterial.perspectivePoints) {
+                        src.push(`gl_PointSize = (nearPlaneHeight * pointSize) / ${gl_Position}.w;`);
+                        src.push("gl_PointSize = max(gl_PointSize, " + Math.floor(pointsMaterial.minPerspectivePointSize) + ".0);");
+                        src.push("gl_PointSize = min(gl_PointSize, " + Math.floor(pointsMaterial.maxPerspectivePointSize) + ".0);");
+                    } else {
+                        src.push("gl_PointSize = pointSize;");
+                    }
+                    src.push("gl_PointSize += 10.0;");
                 }
-                src.push("gl_PointSize += 10.0;");
             },
             appendFragmentDefinitions: (src) => {
                 src.push("uniform float pickZNear;");
@@ -66,9 +71,9 @@ export class VBOPointsPickDepthRenderer extends VBORenderer {
             needvWorldPosition: false,
             needGl_FragCoord: false,
             needViewMatrixInFragment: false,
-            needGl_PointCoord: pointsMaterial.roundPoints,
+            needGl_PointCoord: isPoints && pointsMaterial.roundPoints,
             appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix, gl_PointCoord) => {
-                if (pointsMaterial.roundPoints) {
+                if (isPoints && pointsMaterial.roundPoints) {
                     src.push(`  vec2 cxy = 2.0 * ${gl_PointCoord} - 1.0;`);
                     src.push("  float r = dot(cxy, cxy);");
                     src.push("  if (r > 1.0) {");
