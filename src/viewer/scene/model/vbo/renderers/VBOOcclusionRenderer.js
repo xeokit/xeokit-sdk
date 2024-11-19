d39e96c6bce444781c42f3a34612b23427bd8572
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-19 15:43:04 +0100

    Replace needvWorldPosition with lazyShaderVariable vWorldPosition

--- src/viewer/scene/model/vbo/renderers/VBOOcclusionRenderer.js
+++ src/viewer/scene/model/vbo/renderers/VBOOcclusionRenderer.js
@@ -38,7 +38,6 @@ export class VBOOcclusionRenderer extends VBORenderer {
                 src.push("out vec4 outColor;");
             },
             slicedColorIfClipping: false,
-            needvWorldPosition: false,
             needGl_FragCoord: false,
             needViewMatrixInFragment: false,
             appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
