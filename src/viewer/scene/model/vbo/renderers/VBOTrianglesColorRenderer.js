2b1d13d3d021d6c479ec714813d055c5960bc56d
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-19 18:20:21 +0100

    Replace slicedColorIfClipping with sliceColorOr logic

--- src/viewer/scene/model/vbo/renderers/VBOTrianglesColorRenderer.js
+++ src/viewer/scene/model/vbo/renderers/VBOTrianglesColorRenderer.js
@@ -48,7 +48,6 @@ export class VBOTrianglesColorRenderer extends VBORenderer {
                 src.push("in vec4 vColor;");
                 src.push("out vec4 outColor;");
             },
-            slicedColorIfClipping: true,
             needGl_FragCoord: sao,
             appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
                 src.push(`vec4 fragColor = ${sliceColorOr("vColor")};`);
