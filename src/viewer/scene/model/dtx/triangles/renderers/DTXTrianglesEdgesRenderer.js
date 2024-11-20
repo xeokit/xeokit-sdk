81ab9c12a6165c451517387e3868be174c2f9e16
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-20 19:01:11 +0100

    Combine DTXTrianglesEdgesColorRenderer into DTXTrianglesEdgesRenderer

--- src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesEdgesRenderer.js
+++ src/viewer/scene/model/dtx/triangles/renderers/DTXTrianglesEdgesRenderer.js
@@ -2,14 +2,14 @@ import {DTXTrianglesDrawable} from "../DTXTrianglesDrawable.js";
 import {math} from "../../../../math/math.js";
 import {RENDER_PASSES} from "../../../RENDER_PASSES.js";
 
-const defaultColor = new Float32Array([0, 0, 0, 1]);
+const edgesDefaultColor = new Float32Array([0, 0, 0, 1]);
 
 /**
  * @private
  */
 export class DTXTrianglesEdgesRenderer {
 
-    constructor(scene) {
+    constructor(scene, colorUniform) {
         this.getValid  = () => drawable.getValid();
         this.drawLayer = (frameCtx, layer, renderPass) => drawable.drawLayer(frameCtx, layer, renderPass);
         this.destroy   = () => drawable.destroy();
@@ -17,7 +17,7 @@ export class DTXTrianglesEdgesRenderer {
         const inputs = { };
         const gl = scene.canvas.gl;
 
-        const drawable = new DTXTrianglesDrawable("DTXTrianglesEdgesRenderer", scene, false, {
+        const drawable = new DTXTrianglesDrawable(colorUniform ? "DTXTrianglesEdgesRenderer" : "DTXTrianglesEdgesColorRenderer", scene, false, {
             getHash: () => [ ],
             getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
             getViewParams: (frameCtx, camera) => ({
@@ -30,39 +30,53 @@ export class DTXTrianglesEdgesRenderer {
             // renderPass = EDGES_COLOR_OPAQUE | EDGES_COLOR_TRANSPARENT | EDGES_HIGHLIGHTED | EDGES_XRAYED | EDGES_SELECTED
             renderPassFlag: 2,
             cullOnAlphaZero: true,
-            appendVertexDefinitions: (src) => { },
+            appendVertexDefinitions: (src) => {
+                if (! colorUniform) {
+                    src.push("out vec4 vColor;");
+                }
+            },
             transformClipPos: clipPos => clipPos,
-            needVertexColor: false,
+            needVertexColor: ! colorUniform,
             needPickColor: false,
             needGl_Position: false,
             needViewMatrixPositionNormal: false,
-            appendVertexOutputs: (src, color, pickColor, gl_Position, view) => { },
+            appendVertexOutputs: (src, color, pickColor, gl_Position, view) => {
+                if (! colorUniform) {
+                    src.push(`vColor = vec4(vec3(${color}.rgb) * 0.5, float(${color}.a)) / 255.0;`);
+                }
+            },
             appendFragmentDefinitions: (src) => {
-                src.push("uniform vec4 color;");
+                if (colorUniform) {
+                    src.push("uniform vec4 edgeColor;");
+                } else {
+                    src.push("in vec4 vColor;");
+                }
                 src.push("out vec4 outColor;");
             },
             needvWorldPosition: false,
             needGl_FragCoord: false,
-            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord) => src.push("   outColor = color;"),
-            setupInputs: (program) => { this._uColor = program.getLocation("color"); },
+            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord) => src.push("outColor = " + (colorUniform ? "edgeColor" : "vColor") + ";"),
+            setupInputs: (program) => {
+                this._uEdgeColor = colorUniform && program.getLocation("edgeColor");
+            },
             setRenderState: (frameCtx, layer, renderPass, rtcOrigin) => {
-                if (renderPass === RENDER_PASSES.EDGES_XRAYED) {
-                    const material = scene.xrayMaterial._state;
-                    const edgeColor = material.edgeColor;
-                    const edgeAlpha = material.edgeAlpha;
-                    gl.uniform4f(this._uColor, edgeColor[0], edgeColor[1], edgeColor[2], edgeAlpha);
-                } else if (renderPass === RENDER_PASSES.EDGES_HIGHLIGHTED) {
-                    const material = scene.highlightMaterial._state;
-                    const edgeColor = material.edgeColor;
-                    const edgeAlpha = material.edgeAlpha;
-                    gl.uniform4f(this._uColor, edgeColor[0], edgeColor[1], edgeColor[2], edgeAlpha);
-                } else if (renderPass === RENDER_PASSES.EDGES_SELECTED) {
-                    const material = scene.selectedMaterial._state;
-                    const edgeColor = material.edgeColor;
-                    const edgeAlpha = material.edgeAlpha;
-                    gl.uniform4f(this._uColor, edgeColor[0], edgeColor[1], edgeColor[2], edgeAlpha);
-                } else {
-                    gl.uniform4fv(this._uColor, defaultColor);
+                if (colorUniform) {
+                    const edgeColor = this._uEdgeColor;
+                    const setSceneMaterial = material => {
+                        const color = material._state.edgeColor;
+                        const alpha = material._state.edgeAlpha;
+                        gl.uniform4f(edgeColor, color[0], color[1], color[2], alpha);
+                    };
+
+                    if (renderPass === RENDER_PASSES.EDGES_XRAYED) {
+                        setSceneMaterial(scene.xrayMaterial);
+                    } else if (renderPass === RENDER_PASSES.EDGES_HIGHLIGHTED) {
+                        setSceneMaterial(scene.highlightMaterial);
+                    } else if (renderPass === RENDER_PASSES.EDGES_SELECTED) {
+                        setSceneMaterial(scene.selectedMaterial);
+                    } else {
+                        gl.uniform4fv(edgeColor, edgesDefaultColor);
+                    }
                 }
             },
             getGlMode: (frameCtx) => gl.LINES
