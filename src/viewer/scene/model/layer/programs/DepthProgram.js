d218217ebcd155774a27a166c3e0b86cca483701
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-25 20:14:12 +0100

    Only reindent layer/programs

--- src/viewer/scene/model/layer/programs/DepthProgram.js
+++ src/viewer/scene/model/layer/programs/DepthProgram.js
@@ -1,22 +1,22 @@
 export const DepthProgram = function(logarithmicDepthBufferEnabled) {
-        return {
-            programName: "Depth",
-            getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
-            renderPassFlag: 0,  // COLOR_OPAQUE | COLOR_TRANSPARENT
-            appendVertexDefinitions: (src) => src.push("out highp vec2 vHighPrecisionZW;"),
-            appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => src.push(`vHighPrecisionZW = ${gl_Position}.zw;`),
-            appendFragmentDefinitions: (src) => {
-                src.push("in highp vec2 vHighPrecisionZW;");
-                src.push("out vec4 outColor;");
-            },
-            // isn't it the same as vec4(vec3(1.0 - gl_FragCoord.z), 1.0) ?
-            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => src.push("outColor = vec4(vec3((1.0 - vHighPrecisionZW[0] / vHighPrecisionZW[1]) / 2.0), 1.0);"),
+    return {
+        programName: "Depth",
+        getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        renderPassFlag: 0,  // COLOR_OPAQUE | COLOR_TRANSPARENT
+        appendVertexDefinitions: (src) => src.push("out highp vec2 vHighPrecisionZW;"),
+        appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => src.push(`vHighPrecisionZW = ${gl_Position}.zw;`),
+        appendFragmentDefinitions: (src) => {
+            src.push("in highp vec2 vHighPrecisionZW;");
+            src.push("out vec4 outColor;");
+        },
+        // isn't it the same as vec4(vec3(1.0 - gl_FragCoord.z), 1.0) ?
+        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => src.push("outColor = vec4(vec3((1.0 - vHighPrecisionZW[0] / vHighPrecisionZW[1]) / 2.0), 1.0);"),
 
-            getViewParams: (frameCtx, camera) => ({
-                viewMatrix: camera.viewMatrix,
-                projMatrix: camera.projMatrix,
-                eye: camera.eye,
-                far: camera.project.far
-            })
-        };
+        getViewParams: (frameCtx, camera) => ({
+            viewMatrix: camera.viewMatrix,
+            projMatrix: camera.projMatrix,
+            eye: camera.eye,
+            far: camera.project.far
+        })
+    };
 };
--- src/viewer/scene/model/layer/programs/PickMeshProgram.js
+++ src/viewer/scene/model/layer/programs/PickMeshProgram.js
@@ -1,38 +1,38 @@
 export const PickMeshProgram = function(scene, clipTransformSetup, isPoints) {
-        const gl = scene.canvas.gl;
-        return {
-            programName: "PickMesh",
-            getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
-            renderPassFlag: 3,  // PICK
-            appendVertexDefinitions: (src) => {
-                src.push("out vec4 vPickColor;");
-                clipTransformSetup.appendDefinitions(src);
-            },
-            transformClipPos: clipTransformSetup.transformClipPos,
-            appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
-                src.push(`vPickColor = ${pickColor} / 255.0;`);
-                if (isPoints) {
-                    src.push("gl_PointSize += 10.0;");
-                }
-            },
-            appendFragmentDefinitions: (src) => {
-                src.push("in vec4 vPickColor;");
-                src.push("out vec4 outPickColor;");
-            },
-            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
-                src.push("outPickColor = vPickColor;");
-            },
-            setupInputs: (program) => {
-                const setClipTransformState = clipTransformSetup.setupInputs(program);
-                return (frameCtx, layer, renderPass, rtcOrigin) => setClipTransformState(frameCtx);
-            },
+    const gl = scene.canvas.gl;
+    return {
+        programName: "PickMesh",
+        getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        renderPassFlag: 3,  // PICK
+        appendVertexDefinitions: (src) => {
+            src.push("out vec4 vPickColor;");
+            clipTransformSetup.appendDefinitions(src);
+        },
+        transformClipPos: clipTransformSetup.transformClipPos,
+        appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
+            src.push(`vPickColor = ${pickColor} / 255.0;`);
+            if (isPoints) {
+                src.push("gl_PointSize += 10.0;");
+            }
+        },
+        appendFragmentDefinitions: (src) => {
+            src.push("in vec4 vPickColor;");
+            src.push("out vec4 outPickColor;");
+        },
+        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
+            src.push("outPickColor = vPickColor;");
+        },
+        setupInputs: (program) => {
+            const setClipTransformState = clipTransformSetup.setupInputs(program);
+            return (frameCtx, layer, renderPass, rtcOrigin) => setClipTransformState(frameCtx);
+        },
 
-            dontCullOnAlphaZero: true, // should be false?
-            getViewParams: (frameCtx, camera) => ({
-                viewMatrix: frameCtx.pickViewMatrix || camera.viewMatrix,
-                projMatrix: frameCtx.pickProjMatrix || camera.projMatrix,
-                eye: frameCtx.pickOrigin || camera.eye,
-                far: frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far
-            })
-        };
+        dontCullOnAlphaZero: true, // should be false?
+        getViewParams: (frameCtx, camera) => ({
+            viewMatrix: frameCtx.pickViewMatrix || camera.viewMatrix,
+            projMatrix: frameCtx.pickProjMatrix || camera.projMatrix,
+            eye: frameCtx.pickOrigin || camera.eye,
+            far: frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far
+        })
+    };
 };
