ffadf154ad0555d45b3760d661358db39aaea852
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-25 20:14:12 +0100

    Only reindent layer/programs

--- src/viewer/scene/model/layer/programs/ColorTextureProgram.js
+++ src/viewer/scene/model/layer/programs/ColorTextureProgram.js
@@ -1,94 +1,94 @@
 import {WEBGL_INFO} from "../../../webglInfo.js";
 
 export const ColorTextureProgram = function(scene, lightSetup, sao, useAlphaCutoff, gammaOutput) {
-        const gl = scene.canvas.gl;
-        const maxTextureUnits = WEBGL_INFO.MAX_TEXTURE_IMAGE_UNITS;
-        return {
-            programName: "ColorTexture",
-            getHash: () => [lightSetup.getHash(), sao ? "sao" : "nosao", gammaOutput, useAlphaCutoff ? "alphaCutoffYes" : "alphaCutoffNo"],
-            getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
-            renderPassFlag: 0,      // COLOR_OPAQUE | COLOR_TRANSPARENT
-            appendVertexDefinitions: (src) => {
-                src.push("uniform mat3 uvDecodeMatrix;");
-                src.push("out vec4 vViewPosition;");
-                src.push("out vec2 vUV;");
-                src.push("out vec4 vColor;");
-            },
-            appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
-                src.push(`vViewPosition = ${view.viewPosition};`);
-                src.push(`vUV = (uvDecodeMatrix * vec3(${uv}, 1.0)).xy;`);
-                src.push(`vColor = vec4(${color}) / 255.0;`);
-            },
-            appendFragmentDefinitions: (src) => {
-                src.push("uniform sampler2D uColorMap;");
+    const gl = scene.canvas.gl;
+    const maxTextureUnits = WEBGL_INFO.MAX_TEXTURE_IMAGE_UNITS;
+    return {
+        programName: "ColorTexture",
+        getHash: () => [lightSetup.getHash(), sao ? "sao" : "nosao", gammaOutput, useAlphaCutoff ? "alphaCutoffYes" : "alphaCutoffNo"],
+        getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        renderPassFlag: 0,      // COLOR_OPAQUE | COLOR_TRANSPARENT
+        appendVertexDefinitions: (src) => {
+            src.push("uniform mat3 uvDecodeMatrix;");
+            src.push("out vec4 vViewPosition;");
+            src.push("out vec2 vUV;");
+            src.push("out vec4 vColor;");
+        },
+        appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
+            src.push(`vViewPosition = ${view.viewPosition};`);
+            src.push(`vUV = (uvDecodeMatrix * vec3(${uv}, 1.0)).xy;`);
+            src.push(`vColor = vec4(${color}) / 255.0;`);
+        },
+        appendFragmentDefinitions: (src) => {
+            src.push("uniform sampler2D uColorMap;");
 
-                src.push("vec4 sRGBToLinear( in vec4 value ) {");
-                src.push("  return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.w );");
+            src.push("vec4 sRGBToLinear( in vec4 value ) {");
+            src.push("  return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.w );");
+            src.push("}");
+            if (gammaOutput) {
+                src.push("uniform float gammaFactor;");
+                src.push("vec4 linearToGamma( in vec4 value, in float gammaFactor ) {");
+                src.push("  return vec4( pow( value.xyz, vec3( 1.0 / gammaFactor ) ), value.w );");
                 src.push("}");
-                if (gammaOutput) {
-                    src.push("uniform float gammaFactor;");
-                    src.push("vec4 linearToGamma( in vec4 value, in float gammaFactor ) {");
-                    src.push("  return vec4( pow( value.xyz, vec3( 1.0 / gammaFactor ) ), value.w );");
-                    src.push("}");
+            }
+
+            lightSetup.appendDefinitions(src);
+            sao && sao.appendDefinitions(src);
+
+            if (useAlphaCutoff) {
+                src.push("uniform float materialAlphaCutoff;");
+            }
+
+            src.push("in vec4 vViewPosition;");
+            src.push("in vec2 vUV;");
+            src.push("in vec4 vColor;");
+            src.push("out vec4 outColor;");
+        },
+        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
+            src.push("vec3 viewNormal = normalize(cross(dFdx(vViewPosition.xyz), dFdy(vViewPosition.xyz)));");
+            src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
+            lightSetup.getDirectionalLights(viewMatrix, "vViewPosition").forEach(light => {
+                src.push(`reflectedColor += max(dot(-viewNormal, ${light.direction}), 0.0) * ${light.color};`);
+            });
+
+            src.push(`vec4 color = vec4(${lightSetup.getAmbientColor()} + reflectedColor, 1) * ${sliceColorOr("vColor")};`);
+
+            src.push("vec4 sampleColor = sRGBToLinear(texture(uColorMap, vUV));");
+
+            if (useAlphaCutoff) {
+                src.push("if (sampleColor.a < materialAlphaCutoff) { discard; }");
+            }
+
+            src.push("vec4 colorTexel = color * sampleColor;");
+            src.push("outColor = vec4(colorTexel.rgb" + (sao ? (" * " + sao.getAmbient(gl_FragCoord)) : "") + ", color.a);");
+
+            if (gammaOutput) {
+                src.push("outColor = linearToGamma(outColor, gammaFactor);");
+            }
+        },
+        setupInputs: (program) => {
+            const uUVDecodeMatrix      = program.getLocation("uvDecodeMatrix");
+            const uColorMap            = program.getSampler("uColorMap");
+            const uGammaFactor         = gammaOutput && program.getLocation("gammaFactor");
+            const setLightsRenderState = lightSetup.setupInputs(program);
+            const setSAOState          = sao && sao.setupInputs(program);
+            const materialAlphaCutoff  = useAlphaCutoff && program.getLocation("materialAlphaCutoff");
+
+            return (frameCtx, layer, renderPass, rtcOrigin) => {
+                const state = layer._state;
+                gl.uniformMatrix3fv(uUVDecodeMatrix, false, state.uvDecodeMatrix);
+                const colorTexture = state.textureSet.colorTexture;
+                if (colorTexture) {
+                    uColorMap.bindTexture(colorTexture.texture, frameCtx.textureUnit);
+                    frameCtx.textureUnit = (frameCtx.textureUnit + 1) % maxTextureUnits;
                 }
+                uGammaFactor && gl.uniform1f(uGammaFactor, scene.gammaFactor);
+                setLightsRenderState(frameCtx);
+                setSAOState && setSAOState(frameCtx);
+                materialAlphaCutoff && gl.uniform1f(materialAlphaCutoff, state.textureSet.alphaCutoff);
+            };
+        },
 
-                lightSetup.appendDefinitions(src);
-                sao && sao.appendDefinitions(src);
-
-                if (useAlphaCutoff) {
-                    src.push("uniform float materialAlphaCutoff;");
-                }
-
-                src.push("in vec4 vViewPosition;");
-                src.push("in vec2 vUV;");
-                src.push("in vec4 vColor;");
-                src.push("out vec4 outColor;");
-            },
-            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
-                src.push("vec3 viewNormal = normalize(cross(dFdx(vViewPosition.xyz), dFdy(vViewPosition.xyz)));");
-                src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
-                lightSetup.getDirectionalLights(viewMatrix, "vViewPosition").forEach(light => {
-                    src.push(`reflectedColor += max(dot(-viewNormal, ${light.direction}), 0.0) * ${light.color};`);
-                });
-
-                src.push(`vec4 color = vec4(${lightSetup.getAmbientColor()} + reflectedColor, 1) * ${sliceColorOr("vColor")};`);
-
-                src.push("vec4 sampleColor = sRGBToLinear(texture(uColorMap, vUV));");
-
-                if (useAlphaCutoff) {
-                    src.push("if (sampleColor.a < materialAlphaCutoff) { discard; }");
-                }
-
-                src.push("vec4 colorTexel = color * sampleColor;");
-                src.push("outColor = vec4(colorTexel.rgb" + (sao ? (" * " + sao.getAmbient(gl_FragCoord)) : "") + ", color.a);");
-
-                if (gammaOutput) {
-                    src.push("outColor = linearToGamma(outColor, gammaFactor);");
-                }
-            },
-            setupInputs: (program) => {
-                const uUVDecodeMatrix      = program.getLocation("uvDecodeMatrix");
-                const uColorMap            = program.getSampler("uColorMap");
-                const uGammaFactor         = gammaOutput && program.getLocation("gammaFactor");
-                const setLightsRenderState = lightSetup.setupInputs(program);
-                const setSAOState          = sao && sao.setupInputs(program);
-                const materialAlphaCutoff  = useAlphaCutoff && program.getLocation("materialAlphaCutoff");
-
-                return (frameCtx, layer, renderPass, rtcOrigin) => {
-                    const state = layer._state;
-                    gl.uniformMatrix3fv(uUVDecodeMatrix, false, state.uvDecodeMatrix);
-                    const colorTexture = state.textureSet.colorTexture;
-                    if (colorTexture) {
-                        uColorMap.bindTexture(colorTexture.texture, frameCtx.textureUnit);
-                        frameCtx.textureUnit = (frameCtx.textureUnit + 1) % maxTextureUnits;
-                    }
-                    uGammaFactor && gl.uniform1f(uGammaFactor, scene.gammaFactor);
-                    setLightsRenderState(frameCtx);
-                    setSAOState && setSAOState(frameCtx);
-                    materialAlphaCutoff && gl.uniform1f(materialAlphaCutoff, state.textureSet.alphaCutoff);
-                };
-            },
-
-            incrementDrawState: true
-        };
+        incrementDrawState: true
+    };
 };
--- src/viewer/scene/model/layer/programs/PickDepthProgram.js
+++ src/viewer/scene/model/layer/programs/PickDepthProgram.js
@@ -1,54 +1,54 @@
 export const PickDepthProgram = function(scene, clipTransformSetup, isPoints) {
-        const gl = scene.canvas.gl;
-        return {
-            programName: "PickDepth",
-            getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
-            renderPassFlag: 3,  // PICK
-            appendVertexDefinitions: (src) => {
-                src.push("out vec4 vViewPosition;");
-                clipTransformSetup.appendDefinitions(src);
-            },
-            transformClipPos: clipTransformSetup.transformClipPos,
-            appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
-                src.push(`vViewPosition = ${view.viewPosition};`);
-                if (isPoints) {
-                    src.push("gl_PointSize += 10.0;");
-                }
-            },
-            appendFragmentDefinitions: (src) => {
-                src.push("uniform float pickZNear;");
-                src.push("uniform float pickZFar;");
-                src.push("in vec4 vViewPosition;");
-                src.push("vec4 packDepth(const in float depth) {");
-                src.push("  const vec4 bitShift = vec4(256.0*256.0*256.0, 256.0*256.0, 256.0, 1.0);");
-                src.push("  const vec4 bitMask  = vec4(0.0, 1.0/256.0, 1.0/256.0, 1.0/256.0);");
-                src.push("  vec4 res = fract(depth * bitShift);");
-                src.push("  res -= res.xxyz * bitMask;");
-                src.push("  return res;");
-                src.push("}");
-                src.push("out vec4 outPackedDepth;");
-            },
-            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
-                src.push("float zNormalizedDepth = abs((pickZNear + vViewPosition.z) / (pickZFar - pickZNear));");
-                src.push("outPackedDepth = packDepth(zNormalizedDepth);"); // Must be linear depth
-                // try: src.push("    outPackedDepth = vec4(zNormalizedDepth, fract(zNormalizedDepth * vec3(256.0, 256.0*256.0, 256.0*256.0*256.0)));");
-            },
-            setupInputs: (program) => {
-                const uPickZNear = program.getLocation("pickZNear");
-                const uPickZFar  = program.getLocation("pickZFar");
-                const setClipTransformState = clipTransformSetup.setupInputs(program);
-                return (frameCtx, layer, renderPass, rtcOrigin) => {
-                    gl.uniform1f(uPickZNear, frameCtx.pickZNear);
-                    gl.uniform1f(uPickZFar,  frameCtx.pickZFar);
-                    setClipTransformState(frameCtx);
-                };
-            },
+    const gl = scene.canvas.gl;
+    return {
+        programName: "PickDepth",
+        getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        renderPassFlag: 3,  // PICK
+        appendVertexDefinitions: (src) => {
+            src.push("out vec4 vViewPosition;");
+            clipTransformSetup.appendDefinitions(src);
+        },
+        transformClipPos: clipTransformSetup.transformClipPos,
+        appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
+            src.push(`vViewPosition = ${view.viewPosition};`);
+            if (isPoints) {
+                src.push("gl_PointSize += 10.0;");
+            }
+        },
+        appendFragmentDefinitions: (src) => {
+            src.push("uniform float pickZNear;");
+            src.push("uniform float pickZFar;");
+            src.push("in vec4 vViewPosition;");
+            src.push("vec4 packDepth(const in float depth) {");
+            src.push("  const vec4 bitShift = vec4(256.0*256.0*256.0, 256.0*256.0, 256.0, 1.0);");
+            src.push("  const vec4 bitMask  = vec4(0.0, 1.0/256.0, 1.0/256.0, 1.0/256.0);");
+            src.push("  vec4 res = fract(depth * bitShift);");
+            src.push("  res -= res.xxyz * bitMask;");
+            src.push("  return res;");
+            src.push("}");
+            src.push("out vec4 outPackedDepth;");
+        },
+        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
+            src.push("float zNormalizedDepth = abs((pickZNear + vViewPosition.z) / (pickZFar - pickZNear));");
+            src.push("outPackedDepth = packDepth(zNormalizedDepth);"); // Must be linear depth
+            // try: src.push("    outPackedDepth = vec4(zNormalizedDepth, fract(zNormalizedDepth * vec3(256.0, 256.0*256.0, 256.0*256.0*256.0)));");
+        },
+        setupInputs: (program) => {
+            const uPickZNear = program.getLocation("pickZNear");
+            const uPickZFar  = program.getLocation("pickZFar");
+            const setClipTransformState = clipTransformSetup.setupInputs(program);
+            return (frameCtx, layer, renderPass, rtcOrigin) => {
+                gl.uniform1f(uPickZNear, frameCtx.pickZNear);
+                gl.uniform1f(uPickZFar,  frameCtx.pickZFar);
+                setClipTransformState(frameCtx);
+            };
+        },
 
-            getViewParams: (frameCtx, camera) => ({
-                viewMatrix: frameCtx.pickViewMatrix || camera.viewMatrix,
-                projMatrix: frameCtx.pickProjMatrix || camera.projMatrix,
-                eye: frameCtx.pickOrigin || camera.eye,
-                far: frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far
-            })
-        };
+        getViewParams: (frameCtx, camera) => ({
+            viewMatrix: frameCtx.pickViewMatrix || camera.viewMatrix,
+            projMatrix: frameCtx.pickProjMatrix || camera.projMatrix,
+            eye: frameCtx.pickOrigin || camera.eye,
+            far: frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far
+        })
+    };
 };
