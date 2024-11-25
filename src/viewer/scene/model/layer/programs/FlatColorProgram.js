2f93a973c960a378385c82567da95eba30640793
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-25 20:14:12 +0100

    Only reindent layer/programs

--- src/viewer/scene/model/layer/programs/FlatColorProgram.js
+++ src/viewer/scene/model/layer/programs/FlatColorProgram.js
@@ -1,41 +1,41 @@
 export const FlatColorProgram = function(logarithmicDepthBufferEnabled, lightSetup, sao) {
-        return {
-            programName: "FlatColor",
-            getHash: () => [lightSetup.getHash(), sao ? "sao" : "nosao"],
-            getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
-            renderPassFlag: 0,      // COLOR_OPAQUE | COLOR_TRANSPARENT
-            appendVertexDefinitions: (src) => {
-                src.push("out vec4 vViewPosition;");
-                src.push("out vec4 vColor;");
-            },
-            appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
-                src.push(`vViewPosition = ${view.viewPosition};`);
-                src.push(`vColor = ${color} / 255.0;`);
-            },
-            appendFragmentDefinitions: (src) => {
-                lightSetup.appendDefinitions(src);
-                sao && sao.appendDefinitions(src);
-                src.push("in vec4 vViewPosition;");
-                src.push("in vec4 vColor;");
-                src.push("out vec4 outColor;");
-            },
-            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
-                src.push("vec3 viewNormal = normalize(cross(dFdx(vViewPosition.xyz), dFdy(vViewPosition.xyz)));");
-                src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
-                lightSetup.getDirectionalLights(viewMatrix, "vViewPosition").forEach(light => {
-                    src.push(`reflectedColor += max(dot(-viewNormal, ${light.direction}), 0.0) * ${light.color};`);
-                });
+    return {
+        programName: "FlatColor",
+        getHash: () => [lightSetup.getHash(), sao ? "sao" : "nosao"],
+        getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        renderPassFlag: 0,      // COLOR_OPAQUE | COLOR_TRANSPARENT
+        appendVertexDefinitions: (src) => {
+            src.push("out vec4 vViewPosition;");
+            src.push("out vec4 vColor;");
+        },
+        appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
+            src.push(`vViewPosition = ${view.viewPosition};`);
+            src.push(`vColor = ${color} / 255.0;`);
+        },
+        appendFragmentDefinitions: (src) => {
+            lightSetup.appendDefinitions(src);
+            sao && sao.appendDefinitions(src);
+            src.push("in vec4 vViewPosition;");
+            src.push("in vec4 vColor;");
+            src.push("out vec4 outColor;");
+        },
+        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
+            src.push("vec3 viewNormal = normalize(cross(dFdx(vViewPosition.xyz), dFdy(vViewPosition.xyz)));");
+            src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
+            lightSetup.getDirectionalLights(viewMatrix, "vViewPosition").forEach(light => {
+                src.push(`reflectedColor += max(dot(-viewNormal, ${light.direction}), 0.0) * ${light.color};`);
+            });
 
-                src.push(`vec4 fragColor = vec4(${lightSetup.getAmbientColor()} + reflectedColor, 1) * ${sliceColorOr("vColor")};`);
-                src.push("outColor = " + (sao ? ("vec4(fragColor.rgb * " + sao.getAmbient(gl_FragCoord) + ", fragColor.a)") : "fragColor") + ";");
-            },
-            setupInputs: (program) => {
-                const setLightsRenderState = lightSetup.setupInputs(program);
-                const setSAOState = sao && sao.setupInputs(program);
-                return (frameCtx, layer, renderPass, rtcOrigin) => {
-                    setLightsRenderState(frameCtx);
-                    setSAOState && setSAOState(frameCtx);
-                };
-            }
-        };
+            src.push(`vec4 fragColor = vec4(${lightSetup.getAmbientColor()} + reflectedColor, 1) * ${sliceColorOr("vColor")};`);
+            src.push("outColor = " + (sao ? ("vec4(fragColor.rgb * " + sao.getAmbient(gl_FragCoord) + ", fragColor.a)") : "fragColor") + ";");
+        },
+        setupInputs: (program) => {
+            const setLightsRenderState = lightSetup.setupInputs(program);
+            const setSAOState = sao && sao.setupInputs(program);
+            return (frameCtx, layer, renderPass, rtcOrigin) => {
+                setLightsRenderState(frameCtx);
+                setSAOState && setSAOState(frameCtx);
+            };
+        }
+    };
 };
--- src/viewer/scene/model/layer/programs/PickNormalsProgram.js
+++ src/viewer/scene/model/layer/programs/PickNormalsProgram.js
@@ -1,44 +1,44 @@
 import {math} from "../../../math/math.js";
 
 export const PickNormalsProgram = function(logarithmicDepthBufferEnabled, clipTransformSetup, isFlat) {
-        return {
-            programName: isFlat ? "PickNormalsFlat" : "PickNormals",
-            getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
-            renderPassFlag: 3,  // PICK
-            appendVertexDefinitions: (src) => {
-                if (! isFlat) {
-                    src.push("out vec3 vWorldNormal;");
-                }
-                clipTransformSetup.appendDefinitions(src);
-            },
-            transformClipPos: clipTransformSetup.transformClipPos,
-            appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
-                if (! isFlat) {
-                    src.push(`vWorldNormal = ${worldNormal};`);
-                }
-            },
-            appendFragmentDefinitions: (src) => {
-                if (! isFlat) {
-                    src.push("in vec3 vWorldNormal;");
-                }
-                src.push("out highp ivec4 outNormal;");
-            },
-            appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
-                const worldNormal = (isFlat
-                                     ? `normalize(cross(dFdx(${vWorldPosition}), dFdy(${vWorldPosition})))`
-                                     : "vWorldNormal");
-                src.push(`outNormal = ivec4(${worldNormal} * float(${math.MAX_INT}), 1.0);`);
-            },
-            setupInputs: (program) => {
-                const setClipTransformState = clipTransformSetup.setupInputs(program);
-                return (frameCtx, layer, renderPass, rtcOrigin) => setClipTransformState(frameCtx);
-            },
+    return {
+        programName: isFlat ? "PickNormalsFlat" : "PickNormals",
+        getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        renderPassFlag: 3,  // PICK
+        appendVertexDefinitions: (src) => {
+            if (! isFlat) {
+                src.push("out vec3 vWorldNormal;");
+            }
+            clipTransformSetup.appendDefinitions(src);
+        },
+        transformClipPos: clipTransformSetup.transformClipPos,
+        appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => {
+            if (! isFlat) {
+                src.push(`vWorldNormal = ${worldNormal};`);
+            }
+        },
+        appendFragmentDefinitions: (src) => {
+            if (! isFlat) {
+                src.push("in vec3 vWorldNormal;");
+            }
+            src.push("out highp ivec4 outNormal;");
+        },
+        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
+            const worldNormal = (isFlat
+                                 ? `normalize(cross(dFdx(${vWorldPosition}), dFdy(${vWorldPosition})))`
+                                 : "vWorldNormal");
+            src.push(`outNormal = ivec4(${worldNormal} * float(${math.MAX_INT}), 1.0);`);
+        },
+        setupInputs: (program) => {
+            const setClipTransformState = clipTransformSetup.setupInputs(program);
+            return (frameCtx, layer, renderPass, rtcOrigin) => setClipTransformState(frameCtx);
+        },
 
-            getViewParams: (frameCtx, camera) => ({
-                viewMatrix: frameCtx.pickViewMatrix || camera.viewMatrix,
-                projMatrix: frameCtx.pickProjMatrix || camera.projMatrix,
-                eye: frameCtx.pickOrigin || camera.eye,
-                far: frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far
-            })
-        };
+        getViewParams: (frameCtx, camera) => ({
+            viewMatrix: frameCtx.pickViewMatrix || camera.viewMatrix,
+            projMatrix: frameCtx.pickProjMatrix || camera.projMatrix,
+            eye: frameCtx.pickOrigin || camera.eye,
+            far: frameCtx.pickProjMatrix ? frameCtx.pickZFar : camera.project.far
+        })
+    };
 };
