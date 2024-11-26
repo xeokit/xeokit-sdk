ecdd6fa6278ef00a2c23d77a11fa68ae081e0e15
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-26 17:53:31 +0100

    Replace shadowParameters with isShadowProgram

--- src/viewer/scene/model/layer/programs/ShadowProgram.js
+++ src/viewer/scene/model/layer/programs/ShadowProgram.js
@@ -1,16 +1,10 @@
-export const ShadowProgram = function(scene) {
+export const ShadowProgram = function(logarithmicDepthBufferEnabled) {
     // VBOBatchingPointsShadowRenderer has been implemented by 14e973df6268369b00baef60e468939e062ac320,
     // but never used (and probably not maintained), as opposed to VBOInstancingPointsShadowRenderer in the same commit
-    const gl = scene.canvas.gl;
     return {
         programName: "Shadow",
-        getLogDepth: scene.logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
         renderPassFlag: 0,
-        appendVertexDefinitions: (src) => {
-            src.push("uniform mat4 shadowProjMatrix;");
-            src.push("uniform mat4 shadowViewMatrix;");
-        },
-        appendVertexOutputs: (src, color, pickColor, uv, metallicRoughness, gl_Position, view, worldNormal, worldPosition) => { },
         appendFragmentDefinitions: (src) => {
             src.push("vec4 encodeFloat( const in float v ) {");
             src.push("  const vec4 bitShift = vec4(256 * 256 * 256, 256 * 256, 256, 1.0);");
@@ -24,15 +18,7 @@ export const ShadowProgram = function(scene) {
         appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => {
             src.push(`outColor = encodeFloat(${gl_FragCoord}.z);`);
         },
-        setupInputs: (program) => {
-            const uShadowProjMatrix = program.getLocation("shadowProjMatrix");
-            const uShadowViewMatrix = program.getLocation("shadowViewMatrix");
-            return (frameCtx, layer) => {
-                gl.uniformMatrix4fv(uShadowProjMatrix, false, frameCtx.shadowProjMatrix); // Not tested
-                gl.uniformMatrix4fv(uShadowViewMatrix, false, frameCtx.shadowViewMatrix); // Not tested
-            };
-        },
 
-        shadowParameters: { projMatrix: "shadowProjMatrix", viewMatrix: "shadowViewMatrix" }
+        isShadowProgram: true
     };
 };
