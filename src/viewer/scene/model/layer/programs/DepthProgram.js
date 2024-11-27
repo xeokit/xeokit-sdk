c440336c76f5f0e764e759e665daf5f29eb8c314
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-27 16:23:17 +0100

    Replace getViewParams with usePickViewParams

--- src/viewer/scene/model/layer/programs/DepthProgram.js
+++ src/viewer/scene/model/layer/programs/DepthProgram.js
@@ -10,13 +10,6 @@ export const DepthProgram = function(logarithmicDepthBufferEnabled) {
             src.push("out vec4 outColor;");
         },
         // isn't it the same as vec4(vec3(1.0 - gl_FragCoord.z), 1.0) ?
-        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => src.push("outColor = vec4(vec3((1.0 - vHighPrecisionZW[0] / vHighPrecisionZW[1]) / 2.0), 1.0);"),
-
-        getViewParams: (frameCtx, camera) => ({
-            viewMatrix: camera.viewMatrix,
-            projMatrix: camera.projMatrix,
-            eye: camera.eye,
-            far: camera.project.far
-        })
+        appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr, viewMatrix) => src.push("outColor = vec4(vec3((1.0 - vHighPrecisionZW[0] / vHighPrecisionZW[1]) / 2.0), 1.0);")
     };
 };
