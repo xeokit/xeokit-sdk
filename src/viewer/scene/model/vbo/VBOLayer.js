d314254de3ef0012963cb8b3f63bcde0c500f935
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-11-28 22:48:32 +0100

    Abstract Layer::_getColSilhEdgePickFlags to use in VBO and DTX layers

--- src/viewer/scene/model/vbo/VBOLayer.js
+++ src/viewer/scene/model/vbo/VBOLayer.js
@@ -184,6 +184,8 @@ const tempVec3e = math.vec3();
 const tempVec3f = math.vec3();
 const tempVec3g = math.vec3();
 
+const tempUint8Array4 = new Uint8Array(4);
+
 // fills the whole dst array with src copies
 const fillArray = function(dst, src) {
     dst.set(src);
@@ -904,73 +906,19 @@ export class VBOLayer extends Layer {
      * flags are 4bits values encoded on a 32bit base. color flag on the first 4 bits, silhouette flag on the next 4 bits and so on for edge, pick and clippable.
      */
     _setFlags(portionId, flags, transparent, deferred = false) {
-
         if (!this._finalized) {
             throw "Not finalized";
         }
 
-        const visible = !!(flags & ENTITY_FLAGS.VISIBLE);
-        const xrayed = !!(flags & ENTITY_FLAGS.XRAYED);
-        const highlighted = !!(flags & ENTITY_FLAGS.HIGHLIGHTED);
-        const selected = !!(flags & ENTITY_FLAGS.SELECTED);
-        const edges = !!(this._hasEdges && flags & ENTITY_FLAGS.EDGES);
-        const pickable = !!(flags & ENTITY_FLAGS.PICKABLE);
-        const culled = !!(flags & ENTITY_FLAGS.CULLED);
-
-        let colorFlag;
-        if (!visible || culled || xrayed
-            || (highlighted && !this.model.scene.highlightMaterial.glowThrough)
-            || (selected && !this.model.scene.selectedMaterial.glowThrough)) {
-            colorFlag = RENDER_PASSES.NOT_RENDERED;
-        } else {
-            if (transparent) {
-                colorFlag = RENDER_PASSES.COLOR_TRANSPARENT;
-            } else {
-                colorFlag = RENDER_PASSES.COLOR_OPAQUE;
-            }
-        }
-
-        let silhouetteFlag;
-        if (!visible || culled) {
-            silhouetteFlag = RENDER_PASSES.NOT_RENDERED;
-        } else if (selected) {
-            silhouetteFlag = RENDER_PASSES.SILHOUETTE_SELECTED;
-        } else if (highlighted) {
-            silhouetteFlag = RENDER_PASSES.SILHOUETTE_HIGHLIGHTED;
-        } else if (xrayed) {
-            silhouetteFlag = RENDER_PASSES.SILHOUETTE_XRAYED;
-        } else {
-            silhouetteFlag = RENDER_PASSES.NOT_RENDERED;
-        }
-
-        let edgeFlag = 0;
-        if ((!this._hasEdges) || (!visible) || culled) {
-            edgeFlag = RENDER_PASSES.NOT_RENDERED;
-        } else if (selected) {
-            edgeFlag = RENDER_PASSES.EDGES_SELECTED;
-        } else if (highlighted) {
-            edgeFlag = RENDER_PASSES.EDGES_HIGHLIGHTED;
-        } else if (xrayed) {
-            edgeFlag = RENDER_PASSES.EDGES_XRAYED;
-        } else if (edges) {
-            if (transparent) {
-                edgeFlag = RENDER_PASSES.EDGES_COLOR_TRANSPARENT;
-            } else {
-                edgeFlag = RENDER_PASSES.EDGES_COLOR_OPAQUE;
-            }
-        } else {
-            edgeFlag = RENDER_PASSES.NOT_RENDERED;
-        }
-
-        const pickFlag = (visible && !culled && pickable) ? RENDER_PASSES.PICK : RENDER_PASSES.NOT_RENDERED;
+        this._getColSilhEdgePickFlags(flags, transparent, tempUint8Array4);
 
         const clippableFlag = !!(flags & ENTITY_FLAGS.CLIPPABLE) ? 1 : 0;
 
         let vertFlag = 0;
-        vertFlag |= colorFlag;
-        vertFlag |= silhouetteFlag << 4;
-        vertFlag |= edgeFlag << 8;
-        vertFlag |= pickFlag << 12;
+        vertFlag |= tempUint8Array4[0];
+        vertFlag |= tempUint8Array4[1] << 4;
+        vertFlag |= tempUint8Array4[2] << 8;
+        vertFlag |= tempUint8Array4[3] << 12;
         vertFlag |= clippableFlag << 16;
 
         tempFloat32[0] = vertFlag;
