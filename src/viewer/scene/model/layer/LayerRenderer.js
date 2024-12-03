0b51ab504413658d6d4d12b524ee414863870a45
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2024-12-03 17:04:52 +0100

    Migrate setupInputs to use getUniformSetter

--- src/viewer/scene/model/layer/LayerRenderer.js
+++ src/viewer/scene/model/layer/LayerRenderer.js
@@ -2,6 +2,7 @@ import {createClippingSetup, isPerspectiveMatrix} from "./Layer.js";
 import {createRTCViewMat, math} from "../../math/index.js";
 import {Program} from "../../webgl/Program.js";
 
+const tempVec2 = math.vec2();
 const tempVec3 = math.vec3();
 const tempMat4 = math.mat4();
 
@@ -40,7 +41,7 @@ export class LayerRenderer {
         this.getValid = () => hash === getHash();
 
         const gl = scene.canvas.gl;
-        const clipping = createClippingSetup(gl, sectionPlanesState);
+        const clipping = createClippingSetup(sectionPlanesState);
 
         const lazyShaderVariable = function(name) {
             const variable = {
@@ -306,20 +307,85 @@ export class LayerRenderer {
 
         const drawCall = renderingAttributes.makeDrawCall(program);
 
-        const uRenderPass = (! isShadowProgram) && program.getLocation("renderPass");
-        const uLogDepthBufFC = getLogDepth && program.getLocation("logDepthBufFC");
-        const setClippingState = clipping && clipping.setupInputs(program);
+        const getUniformSetter = (function() {
+            const activeUniforms = { };
+            const handle = program.handle;
+            const numUniforms = gl.getProgramParameter(handle, gl.ACTIVE_UNIFORMS);
+            for (let i = 0; i < numUniforms; ++i) {
+                const u = gl.getActiveUniform(handle, i);
+                let uName = u.name;
+                if (uName[uName.length - 1] === "\u0000") {
+                    uName = uName.substr(0, uName.length - 1);
+                }
+                const location = gl.getUniformLocation(handle, uName);
+                const type = (function() {
+                    for (let k in gl) {
+                        if (u.type === gl[k])
+                            return k;
+                    }
+                    return null;
+                })();
+
+                if ((u.type === gl.SAMPLER_2D)
+                    ||
+                    (u.type === gl.SAMPLER_CUBE)
+                    ||
+                    (u.type === gl.SAMPLER_2D_SHADOW)
+                    ||
+                    ((gl instanceof window.WebGL2RenderingContext)
+                     &&
+                     ((u.type === gl.UNSIGNED_INT_SAMPLER_2D)
+                      ||
+                      (u.type === gl.INT_SAMPLER_2D)))) {
+                    activeUniforms[uName] = function(texture, unit) {
+                        const bound = texture.bind(unit);
+                        if (bound) {
+                            gl.uniform1i(location, unit);
+                        }
+                        return bound;
+                    };
+                } else {
+                    activeUniforms[uName] = (function() {
+                        if (u.size === 1) {
+                            switch (u.type) {
+                            case gl.BOOL:       return value => gl.uniform1i(location, value);
+                            case gl.INT:        return value => gl.uniform1i(location, value);
+                            case gl.FLOAT:      return value => gl.uniform1f(location, value);
+                            case gl.FLOAT_VEC2: return value => gl.uniform2fv(location, value);
+                            case gl.FLOAT_VEC3: return value => gl.uniform3fv(location, value);
+                            case gl.FLOAT_VEC4: return value => gl.uniform4fv(location, value);
+                            case gl.FLOAT_MAT3: return value => gl.uniformMatrix3fv(location, false, value);
+                            case gl.FLOAT_MAT4: return value => gl.uniformMatrix4fv(location, false, value);
+                            }
+                        }
+                        throw `Unhandled uniform ${uName}`;
+                    })();
+                }
+            }
+
+            return function(name) {
+                const u = activeUniforms[name];
+                if (! u) {
+                    throw `Missing uniform "${name}"`;
+                }
+                return u;
+            };
+        })();
+
+        const uRenderPass = (! isShadowProgram) && getUniformSetter("renderPass");
+        const uLogDepthBufFC = getLogDepth && getUniformSetter("logDepthBufFC");
+        const setClippingState = clipping && clipping.setupInputs(getUniformSetter);
         const uSlice = sliceColorOr.needed && {
-            thickness: program.getLocation("sliceThickness"),
-            color:     program.getLocation("sliceColor")
+            thickness: getUniformSetter("sliceThickness"),
+            color:     getUniformSetter("sliceColor")
         };
         const uPoint = setupPoints && {
-            pointSize:       program.getLocation("pointSize"),
-            nearPlaneHeight: pointsMaterial.perspectivePoints && program.getLocation("nearPlaneHeight")
+            pointSize:       getUniformSetter("pointSize"),
+            nearPlaneHeight: pointsMaterial.perspectivePoints && getUniformSetter("nearPlaneHeight")
         };
-        const uIntensityRange = filterIntensityRange && program.getLocation("intensityRange");
+        const uIntensityRange = filterIntensityRange && getUniformSetter("intensityRange");
 
-        const setInputsState = setupInputs && setupInputs(program);
+        const setInputsState = setupInputs && setupInputs(getUniformSetter);
 
         this.destroy = () => program.destroy();
         this.drawLayer = (frameCtx, layer, renderPass) => {
@@ -328,31 +394,31 @@ export class LayerRenderer {
                 program.bind();
             }
 
-            if (uRenderPass) {
-                gl.uniform1i(uRenderPass, renderPass);
-            }
+            uRenderPass && uRenderPass(renderPass);
 
             if (setClippingState) {
                 setClippingState(layer);
                 const crossSections = uSlice && scene.crossSections;
                 if (crossSections) {
-                    gl.uniform1f(uSlice.thickness, crossSections.sliceThickness);
-                    gl.uniform4fv(uSlice.color,    crossSections.sliceColor);
+                    uSlice.thickness(crossSections.sliceThickness);
+                    uSlice.color(crossSections.sliceColor);
                 }
             }
 
             if (uPoint) {
-                gl.uniform1f(uPoint.pointSize, pointsMaterial.pointSize);
+                uPoint.pointSize(pointsMaterial.pointSize);
                 if (uPoint.nearPlaneHeight) {
                     const nearPlaneHeight = (scene.camera.projection === "ortho") ?
                           1.0
                           : (gl.drawingBufferHeight / (2 * Math.tan(0.5 * scene.camera.perspective.fov * Math.PI / 180.0)));
-                    gl.uniform1f(uPoint.nearPlaneHeight, nearPlaneHeight);
+                    uPoint.nearPlaneHeight(nearPlaneHeight);
                 }
             }
 
             if (uIntensityRange) {
-                gl.uniform2f(uIntensityRange, pointsMaterial.minIntensity, pointsMaterial.maxIntensity);
+                tempVec2[0] = pointsMaterial.minIntensity;
+                tempVec2[1] = pointsMaterial.maxIntensity;
+                uIntensityRange(tempVec2);
             }
 
             setInputsState && setInputsState(frameCtx, layer._state.textureSet);
@@ -367,9 +433,7 @@ export class LayerRenderer {
             const eye        = (usePickParams && frameCtx.pickOrigin) || scene.camera.eye;
             const far        = (usePickParams && frameCtx.pickProjMatrix) ? frameCtx.pickZFar : camera.project.far;
 
-            if (uLogDepthBufFC) {
-                gl.uniform1f(uLogDepthBufFC, 2.0 / (Math.log(far + 1.0) / Math.LN2));
-            }
+            uLogDepthBufFC && uLogDepthBufFC(2.0 / (Math.log(far + 1.0) / Math.LN2));
 
             let rtcViewMatrix;
             const rtcOrigin = tempVec3;
