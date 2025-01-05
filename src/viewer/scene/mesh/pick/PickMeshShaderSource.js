d9bf973061ba62652318c0064cdb5797eacc2478
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-01-05 20:15:07 +0100

    Define setupInputs for PickMeshShaderSource

--- src/viewer/scene/mesh/pick/PickMeshShaderSource.js
+++ src/viewer/scene/mesh/pick/PickMeshShaderSource.js
@@ -1,3 +1,6 @@
+import {math} from "../../math/math.js";
+const tmpVec4 = math.vec4();
+
 export const PickMeshShaderSource = function(mesh) {
     return {
         programName: "PickMesh",
@@ -8,6 +11,21 @@ export const PickMeshShaderSource = function(mesh) {
             src.push("uniform vec4 pickColor;");
             src.push("out vec4 outColor;");
         },
-        appendFragmentOutputs: (src) => src.push("outColor = pickColor;")
+        appendFragmentOutputs: (src) => src.push("outColor = pickColor;"),
+        setupInputs: (getInputSetter) => {
+            const pickClipPos = getInputSetter("pickClipPos");
+            const pickColor = getInputSetter("pickColor");
+            return (frameCtx, meshState) => {
+                pickClipPos(frameCtx.pickClipPos);
+
+                var pickID = meshState.pickID; // Mesh-indexed color
+                tmpVec4[0] = pickID       & 0xFF;
+                tmpVec4[1] = pickID >>  8 & 0xFF;
+                tmpVec4[2] = pickID >> 16 & 0xFF;
+                tmpVec4[3] = pickID >> 24 & 0xFF;
+                math.mulVec4Scalar(tmpVec4, 1 / 255, tmpVec4);
+                pickColor(tmpVec4);
+            };
+        }
     };
 };
