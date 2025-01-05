06b2bdac5bd1cf37b69c1d16237cdf67d9375dce
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-01-05 17:39:51 +0100

    Define pickColor attribute as lazyShaderAttribute

--- src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
+++ src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
@@ -4,11 +4,10 @@ export const PickTriangleShaderSource = function(mesh) {
         dontBillboardAnything: true,
         transformClipPos: clipPos => `vec4((${clipPos}.xy / ${clipPos}.w - pickClipPos) * ${clipPos}.w, ${clipPos}.zw)`,
         appendVertexDefinitions: (src) => {
-            src.push("in vec4 pickColor;");
             src.push("uniform vec2 pickClipPos;");
             src.push("out vec4 vColor;");
         },
-        appendVertexOutputs: (src) => src.push("vColor = pickColor;"),
+        appendVertexOutputs: (src, color, pickColor) => src.push(`vColor = ${pickColor};`),
         appendFragmentDefinitions: (src) => {
             src.push("in vec4 vColor;");
             src.push("out vec4 outColor;");
