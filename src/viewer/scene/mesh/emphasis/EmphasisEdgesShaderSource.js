760af78425c7d02956b485fa7e6253a08b88867e
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-01-07 19:53:30 +0100

    Abstract createGammaOutputSetup from {Draw,Emphasis{Edges,Fill},Lambert}ShaderSource

--- src/viewer/scene/mesh/emphasis/EmphasisEdgesShaderSource.js
+++ src/viewer/scene/mesh/emphasis/EmphasisEdgesShaderSource.js
@@ -1,9 +1,11 @@
+import {createGammaOutputSetup} from "../MeshRenderer.js";
 import {math} from "../../math/math.js";
 const tmpVec4 = math.vec4();
 
 export function EmphasisEdgesShaderSource(mesh) {
     const scene = mesh.scene;
-    const gammaOutput = scene.gammaOutput;
+    const gammaOutputSetup = createGammaOutputSetup(scene);
+
     return {
         programName: "EmphasisEdges",
         appendVertexDefinitions: (src) => {
@@ -12,20 +14,12 @@ export function EmphasisEdgesShaderSource(mesh) {
         },
         appendVertexOutputs: (src) => src.push("vColor = edgeColor;"),
         appendFragmentDefinitions: (src) => {
-            if (gammaOutput) {
-                src.push("uniform float gammaFactor;");
-                src.push("vec4 linearToGamma( in vec4 value, in float gammaFactor ) {");
-                src.push("  return vec4( pow( value.xyz, vec3( 1.0 / gammaFactor ) ), value.w );");
-                src.push("}");
-            }
+            gammaOutputSetup && gammaOutputSetup.appendDefinitions(src);
             src.push("in vec4 vColor;");
             src.push("out vec4 outColor;");
         },
-        appendFragmentOutputs: (src) => src.push(`outColor = ${gammaOutput ? "linearToGamma(vColor, gammaFactor)" : "vColor"};`),
-        setupInputs: gammaOutput && ((getInputSetter) => {
-            const gammaFactor = getInputSetter("gammaFactor");
-            return () => gammaFactor(scene.gammaFactor);
-        }),
+        appendFragmentOutputs: (src) => src.push(`outColor = ${gammaOutputSetup ? gammaOutputSetup.getValueExpression("vColor") : "vColor"};`),
+        setupInputs: gammaOutputSetup && gammaOutputSetup.setupInputs,
         setupMaterialInputs: (getInputSetter) => {
             const edgeColor = getInputSetter("edgeColor");
             return (mtl) => {
