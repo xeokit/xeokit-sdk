e44d06b51488194fa109768c2c230927fcc1f35c
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-01-09 20:56:14 +0100

    Move light setup out from DrawRenderer and into {Draw,Lambert}ShaderSource

--- src/viewer/scene/mesh/draw/LambertShaderSource.js
+++ src/viewer/scene/mesh/draw/LambertShaderSource.js
@@ -1,13 +1,12 @@
-import {createGammaOutputSetup} from "../MeshRenderer.js";
+import {createGammaOutputSetup, createLightSetup} from "../MeshRenderer.js";
 import {math} from "../../math/math.js";
 const tmpVec4 = math.vec4();
 
 export const LambertShaderSource = function(mesh) {
     const scene = mesh.scene;
     const geometryState = mesh._geometry._state;
-    const lightsState = scene._lightsState;
     const primitive = geometryState.primitiveName;
-    const normals = (geometryState.autoVertexNormals || geometryState.normalsBuf) && (primitive === "triangles" || primitive === "triangle-strip" || primitive === "triangle-fan");
+    const lightSetup = (geometryState.autoVertexNormals || geometryState.normalsBuf) && (primitive === "triangles" || primitive === "triangle-strip" || primitive === "triangle-fan") && createLightSetup(scene._lightsState);
     const gammaOutputSetup = createGammaOutputSetup(scene);
 
     return {
@@ -16,66 +15,18 @@ export const LambertShaderSource = function(mesh) {
         setupPointSize: true,
         appendVertexDefinitions: (src) => {
             src.push("uniform vec4 colorize;");
-            src.push("uniform vec4 lightAmbient;");
             src.push("uniform vec4 materialColor;");
             src.push("uniform vec3 materialEmissive;");
-            if (normals) {
-                for (let i = 0, len = lightsState.lights.length; i < len; i++) {
-                    const light = lightsState.lights[i];
-                    if (light.type === "ambient") {
-                        continue;
-                    }
-                    src.push("uniform vec4 lightColor" + i + ";");
-                    if (light.type === "dir") {
-                        src.push("uniform vec3 lightDir" + i + ";");
-                    }
-                    if (light.type === "point") {
-                        src.push("uniform vec3 lightPos" + i + ";");
-                    }
-                    if (light.type === "spot") {
-                        src.push("uniform vec3 lightPos" + i + ";");
-                        src.push("uniform vec3 lightDir" + i + ";");
-                    }
-                }
-            }
+            lightSetup && lightSetup.appendDefinitions(src);
             src.push("out vec4 vColor;");
         },
         appendVertexOutputs: (src, color, pickColor, uv, worldNormal, viewNormal) => {
             src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
-            src.push("vec3 viewLightDir = vec3(0.0, 0.0, -1.0);");
-            src.push("float lambertian = 1.0;");
-            if (normals) {
-                for (let i = 0, len = lightsState.lights.length; i < len; i++) {
-                    const light = lightsState.lights[i];
-                    if (light.type === "ambient") {
-                        continue;
-                    }
-                    if (light.type === "dir") {
-                        if (light.space === "view") {
-                            src.push("viewLightDir = normalize(lightDir" + i + ");");
-                        } else {
-                            src.push("viewLightDir = normalize((viewMatrix2 * vec4(lightDir" + i + ", 0.0)).xyz);");
-                        }
-                    } else if (light.type === "point") {
-                        if (light.space === "view") {
-                            src.push("viewLightDir = -normalize(lightPos" + i + " - viewPosition.xyz);");
-                        } else {
-                            src.push("viewLightDir = -normalize((viewMatrix2 * vec4(lightPos" + i + ", 0.0)).xyz);");
-                        }
-                    } else if (light.type === "spot") {
-                        if (light.space === "view") {
-                            src.push("viewLightDir = normalize(lightDir" + i + ");");
-                        } else {
-                            src.push("viewLightDir = normalize((viewMatrix2 * vec4(lightDir" + i + ", 0.0)).xyz);");
-                        }
-                    } else {
-                        continue;
-                    }
-                    src.push(`lambertian = max(dot(-${viewNormal}, viewLightDir), 0.0);`);
-                    src.push("reflectedColor += lambertian * (lightColor" + i + ".rgb * lightColor" + i + ".a);");
-                }
-            }
-            src.push("vColor = vec4((lightAmbient.rgb * lightAmbient.a * materialColor.rgb) + materialEmissive.rgb + (reflectedColor * materialColor.rgb), materialColor.a) * colorize;"); // TODO: How to have ambient bright enough for canvas BG but not too bright for scene?
+            lightSetup && lightSetup.getDirectionalLights("viewMatrix2", "viewPosition").forEach(light => {
+                src.push(`reflectedColor += max(dot(-${viewNormal}, ${light.direction}), 0.0) * ${light.color};`);
+            });
+            const ambientComponent = lightSetup ? (lightSetup.getAmbientColor() + " + ") : "";
+            src.push(`vColor = colorize * vec4((${ambientComponent}reflectedColor) * materialColor.rgb + materialEmissive.rgb, materialColor.a);`); // TODO: How to have ambient bright enough for canvas BG but not too bright for scene?
         },
         appendFragmentDefinitions: (src) => {
             src.push("in vec4 vColor;");
@@ -100,6 +51,7 @@ export const LambertShaderSource = function(mesh) {
                 materialColor(tmpVec4);
                 materialEmissive(mtl.emissive);
             };
-        }
+        },
+        setupLightInputs: lightSetup && lightSetup.setupInputs
     };
 };
