8bf6c4b6915a914d3dbae15e2569291e40e37ada
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-01-10 22:01:47 +0100

    Make mesh renderers more similar

--- src/viewer/scene/mesh/pick/PickMeshRenderer.js
+++ src/viewer/scene/mesh/pick/PickMeshRenderer.js
@@ -17,48 +17,56 @@ const tempVec3a = math.vec3();
 /**
  * @private
  */
-const PickMeshRenderer = function (hash, mesh) {
-    this._hash = hash;
+const PickMeshRenderer = function(mesh) {
     this._programSetup = PickMeshShaderSource(mesh);
     this._scene = mesh.scene;
     this._useCount = 0;
     this._allocate(mesh);
 };
 
+PickMeshRenderer.getHash = (mesh, ...rest) => [
+    mesh.scene.canvas.canvas.id,
+    mesh.scene._sectionPlanesState.getHash(),
+    mesh._geometry._state.hash,
+    mesh._state.hash
+].join(";");
+
+const rendererClass = PickMeshRenderer;
+
 const renderers = {};
 
-PickMeshRenderer.get = function (mesh) {
-    const hash = [
-        mesh.scene.canvas.canvas.id,
-        mesh.scene._sectionPlanesState.getHash(),
-        mesh._geometry._state.hash,
-        mesh._state.hash
-    ].join(";");
-    let renderer = renderers[hash];
-    if (!renderer) {
-        renderer = new PickMeshRenderer(hash, mesh);
+rendererClass.getInstance = function(matKey, mesh, ...rest) {
+    if (! (matKey in renderers)) {
+        renderers[matKey] = { };
+    }
+    const hash = rendererClass.getHash(mesh, ...rest);
+    if (! (hash in renderers[matKey])) {
+        const renderer = new rendererClass(mesh, ...rest);
         if (renderer.errors) {
             console.log(renderer.errors.join("\n"));
             return null;
         }
-        renderers[hash] = renderer;
+        renderer._hash = hash;
+        renderer._delete = () => { delete renderers[matKey][hash]; };
+        renderers[matKey][hash] = renderer;
         stats.memory.programs++;
     }
+    const renderer = renderers[matKey][hash];
     renderer._useCount++;
     return renderer;
 };
 
-PickMeshRenderer.prototype.put = function () {
+rendererClass.prototype.put = function () {
     if (--this._useCount === 0) {
         if (this._program) {
             this._program.destroy();
         }
-        delete renderers[this._hash];
+        this._delete();
         stats.memory.programs--;
     }
 };
 
-PickMeshRenderer.prototype.webglContextRestored = function () {
+rendererClass.prototype.webglContextRestored = function () {
     this._program = null;
 };
 
