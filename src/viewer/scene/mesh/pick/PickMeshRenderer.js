d0e379b37a593ff2e8393b43fddbdcd7a963ee12
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-01-11 20:37:55 +0100

    Inline _allocate and _bindProgram definitions

--- src/viewer/scene/mesh/pick/PickMeshRenderer.js
+++ src/viewer/scene/mesh/pick/PickMeshRenderer.js
@@ -16,10 +16,41 @@ const tempVec3a = math.vec3();
 /**
  * @private
  */
-const PickMeshRenderer = function(mesh) {
-    this._programSetup = PickMeshShaderSource(mesh);
-    this._scene = mesh.scene;
-    this._allocate(mesh);
+export const PickMeshRenderer = function(mesh) {
+    const scene = mesh.scene;
+    const gl = scene.canvas.gl;
+    const programSetup = PickMeshShaderSource(mesh);
+    const program = new Program(gl, MeshRenderer(programSetup, mesh));
+    if (program.errors) {
+        this.errors = program.errors;
+    } else {
+        this._program = program;
+        this._scene = scene;
+        const getInputSetter = makeInputSetters(gl, program.handle, true);
+        this._setInputsState = programSetup.setupInputs && programSetup.setupInputs(getInputSetter);
+        this._setMaterialInputsState = programSetup.setupMaterialInputs && programSetup.setupMaterialInputs(getInputSetter);
+
+        this._uPositionsDecodeMatrix = program.getLocation("positionsDecodeMatrix");
+        this._uModelMatrix = program.getLocation("modelMatrix");
+        this._uViewMatrix = program.getLocation("viewMatrix");
+        this._uProjMatrix = program.getLocation("projMatrix");
+        this._uSectionPlanes = [];
+        for (let i = 0, len = scene._sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
+            this._uSectionPlanes.push({
+                active: program.getLocation("sectionPlaneActive" + i),
+                pos: program.getLocation("sectionPlanePos" + i),
+                dir: program.getLocation("sectionPlaneDir" + i)
+            });
+        }
+        this._aPosition = program.getAttribute("position");
+        this._uClippable = program.getLocation("clippable");
+        this._uOffset = program.getLocation("offset");
+        if (scene.logarithmicDepthBufferEnabled ) {
+            this._uLogDepthBufFC = program.getLocation("logDepthBufFC");
+        }
+        this._lastMaterialId = null;
+        this._lastGeometryId = null;
+    }
 };
 
 PickMeshRenderer.getHash = (mesh, ...rest) => [
@@ -40,7 +71,15 @@ PickMeshRenderer.prototype.drawMesh = function (frameCtx, mesh) {
 
     if (frameCtx.lastProgramId !== this._program.id) {
         frameCtx.lastProgramId = this._program.id;
-        this._bindProgram(frameCtx);
+        const project = scene.camera.project;
+        this._program.bind();
+        frameCtx.useProgram++;
+        if (scene.logarithmicDepthBufferEnabled ) {
+            const logDepthBufFC = 2.0 / (Math.log(project.far + 1.0) / Math.LN2);
+            gl.uniform1f(this._uLogDepthBufFC, logDepthBufFC);
+        }
+        this._lastMaterialId = null;
+        this._lastGeometryId = null;
     }
 
     gl.uniformMatrix4fv(this._uViewMatrix, false, origin ? frameCtx.getRTCPickViewMatrix(meshState.originHash, origin) : frameCtx.pickViewMatrix);
@@ -129,54 +168,3 @@ PickMeshRenderer.prototype.drawMesh = function (frameCtx, mesh) {
         gl.drawArrays(gl.TRIANGLES, 0, geometryState.positionsBuf.numItems);
     }
 };
-
-PickMeshRenderer.prototype._allocate = function (mesh) {
-    const scene = mesh.scene;
-    const gl = scene.canvas.gl;
-    this._program = new Program(gl, MeshRenderer(this._programSetup, mesh));
-    if (this._program.errors) {
-        this.errors = this._program.errors;
-        return;
-    }
-    const program = this._program;
-    const getInputSetter = makeInputSetters(gl, program.handle, true);
-    this._setInputsState = this._programSetup.setupInputs && this._programSetup.setupInputs(getInputSetter);
-    this._setMaterialInputsState = this._programSetup.setupMaterialInputs && this._programSetup.setupMaterialInputs(getInputSetter);
-
-    this._uPositionsDecodeMatrix = program.getLocation("positionsDecodeMatrix");
-    this._uModelMatrix = program.getLocation("modelMatrix");
-    this._uViewMatrix = program.getLocation("viewMatrix");
-    this._uProjMatrix = program.getLocation("projMatrix");
-    this._uSectionPlanes = [];
-    for (let i = 0, len = scene._sectionPlanesState.getNumAllocatedSectionPlanes(); i < len; i++) {
-        this._uSectionPlanes.push({
-            active: program.getLocation("sectionPlaneActive" + i),
-            pos: program.getLocation("sectionPlanePos" + i),
-            dir: program.getLocation("sectionPlaneDir" + i)
-        });
-    }
-    this._aPosition = program.getAttribute("position");
-    this._uClippable = program.getLocation("clippable");
-    this._uOffset = program.getLocation("offset");
-    if (scene.logarithmicDepthBufferEnabled ) {
-        this._uLogDepthBufFC = program.getLocation("logDepthBufFC");
-    }
-    this._lastMaterialId = null;
-    this._lastGeometryId = null;
-};
-
-PickMeshRenderer.prototype._bindProgram = function (frameCtx) {
-    const scene = this._scene;
-    const gl = scene.canvas.gl;
-    const project = scene.camera.project;
-    this._program.bind();
-    frameCtx.useProgram++;
-    if (scene.logarithmicDepthBufferEnabled ) {
-        const logDepthBufFC = 2.0 / (Math.log(project.far + 1.0) / Math.LN2);
-        gl.uniform1f(this._uLogDepthBufFC, logDepthBufFC);
-    }
-    this._lastMaterialId = null;
-    this._lastGeometryId = null;
-};
-
-export {PickMeshRenderer};
