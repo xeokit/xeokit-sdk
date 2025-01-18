66da73b96ec24b85372504644bfb7eb476565cd6
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-01-18 21:45:26 +0100

    Make mesh renderers more similar

--- src/viewer/scene/mesh/shadow/ShadowRenderer.js
+++ src/viewer/scene/mesh/shadow/ShadowRenderer.js
@@ -11,13 +11,18 @@ export const ShadowRenderer = {
     instantiate: (mesh) => {
         const scene = mesh.scene;
         const gl = scene.canvas.gl;
-        const meshRenderer = MeshRenderer(ShadowShaderSource(mesh), mesh);
+        const programSetup = ShadowShaderSource(mesh);
+        const meshRenderer = MeshRenderer(programSetup, mesh);
         const program = new Program(gl, { vertex: meshRenderer.vertex, fragment: meshRenderer.fragment });
         if (program.errors) {
             return { errors: program.errors };
         } else {
             const getInputSetter = makeInputSetters(gl, program.handle, true);
+            const setInputsState = programSetup.setupInputs && programSetup.setupInputs(getInputSetter);
+            const setMaterialInputsState = programSetup.setupMaterialInputs && programSetup.setupMaterialInputs(getInputSetter);
+            const setLightInputState = programSetup.setupLightInputs && programSetup.setupLightInputs(getInputSetter);
             const setGeometryInputsState = meshRenderer.setupGeometryInputs(getInputSetter);
+            const setGeneralMaterialInputsState = meshRenderer.setupGeneralMaterialInputs && meshRenderer.setupGeneralMaterialInputs(getInputSetter);
             const setMeshInputsState = meshRenderer.setupMeshInputs(getInputSetter);
             const setSectionPlanesInputsState = meshRenderer.setupSectionPlanesInputs(getInputSetter);
 
@@ -26,66 +31,118 @@ export const ShadowRenderer = {
 
             return {
                 destroy: () => program.destroy(),
-                drawMesh: (frame, mesh) => {
-                    const gl = scene.canvas.gl;
-                    const materialState = mesh._material._state;
+                drawMesh: (frameCtx, mesh) => {
+                    const material = mesh._material;
+                    const materialState = material._state;
                     const meshState = mesh._state;
-                    const geometryState = mesh._geometry._state;
+                    const geometry = mesh._geometry;
+                    const geometryState = geometry._state;
+                    const origin = mesh.origin;
+                    const camera = scene.camera;
+                    const project = camera.project;
+                    const actsAsBackground = programSetup.canActAsBackground && meshState.background;
 
-                    if (frame.lastProgramId !== program.id) {
-                        frame.lastProgramId = program.id;
-                        const sectionPlanesState = scene._sectionPlanesState;
-                        program.bind();
-                        frame.useProgram++;
-                        lastMaterialId = null;
-                        lastGeometryId = null;
+                    if (programSetup.skipIfTransparent && (materialState.alpha < 1.0)) {
+                        return;
                     }
 
+                    if (frameCtx.lastProgramId !== program.id) {
+                        frameCtx.lastProgramId = program.id;
+                        program.bind();
+                        frameCtx.useProgram++;
+                        lastMaterialId = null;
+                        lastGeometryId = null;
+                        setLightInputState && setLightInputState();
+                        if (actsAsBackground) {
+                            gl.depthFunc(gl.LEQUAL);
+                        }
+                    }
+
+                    setSectionPlanesInputsState && setSectionPlanesInputsState(mesh.origin, mesh.renderFlags, meshState.clippable, scene._sectionPlanesState);
+
                     if (materialState.id !== lastMaterialId) {
-                        const backfaces = materialState.backfaces;
-                        if (frame.backfaces !== backfaces) {
-                            if (backfaces) {
+                        if (frameCtx.backfaces !== materialState.backfaces) {
+                            if (materialState.backfaces) {
                                 gl.disable(gl.CULL_FACE);
                             } else {
                                 gl.enable(gl.CULL_FACE);
                             }
-                            frame.backfaces = backfaces;
+                            frameCtx.backfaces = materialState.backfaces;
                         }
-                        const frontface = materialState.frontface;
-                        if (frame.frontface !== frontface) {
-                            if (frontface) {
-                                gl.frontFace(gl.CCW);
-                            } else {
-                                gl.frontFace(gl.CW);
-                            }
-                            frame.frontface = frontface;
+
+                        if (programSetup.setsFrontFace && (frameCtx.frontface !== materialState.frontface)) {
+                            gl.frontFace(materialState.frontface ? gl.CCW : gl.CW);
+                            frameCtx.frontface = materialState.frontface;
                         }
-                        if (frame.lineWidth !== materialState.lineWidth) {
+
+                        if (programSetup.setsEdgeWidth && (frameCtx.lineWidth !== materialState.edgeWidth)) {
+                            gl.lineWidth(materialState.edgeWidth);
+                            frameCtx.lineWidth = materialState.edgeWidth;
+                        }
+
+                        if (programSetup.setsLineWidth && (frameCtx.lineWidth !== materialState.lineWidth)) {
                             gl.lineWidth(materialState.lineWidth);
-                            frame.lineWidth = materialState.lineWidth;
+                            frameCtx.lineWidth = materialState.lineWidth;
                         }
+
+                        setMaterialInputsState && setMaterialInputsState(material);
+                        setGeneralMaterialInputsState && setGeneralMaterialInputsState(material);
+
                         lastMaterialId = materialState.id;
                     }
 
-                    setMeshInputsState(mesh, frame.shadowViewMatrix, scene.camera.viewNormalMatrix, frame.shadowProjMatrix, scene.camera.project.far);
+                    setInputsState && setInputsState(frameCtx, meshState);
 
-                    setSectionPlanesInputsState && setSectionPlanesInputsState(mesh.origin, mesh.renderFlags, meshState.clippable, scene._sectionPlanesState);
+                    setMeshInputsState(mesh, frameCtx.shadowViewMatrix, camera.viewNormalMatrix, frameCtx.shadowProjMatrix, camera.project.far);
 
-                    if (geometryState.id !== lastGeometryId) {
-                        setGeometryInputsState(geometryState, () => frame.bindArray++);
+                    if (programSetup.trianglePick) {
+                        const positionsBuf = geometry._getPickTrianglePositions();
+                        if (geometryState.id !== lastGeometryId) {
+                            setGeometryInputsState(geometryState, () => frameCtx.bindArray++, { positionsBuf: positionsBuf, pickColorsBuf: geometry._getPickTriangleColors() });
+                            lastGeometryId = geometryState.id;
+                        }
+
+                        gl.drawArrays(geometryState.primitive, 0, positionsBuf.numItems / 3);
+                    } else if (programSetup.setsEdgeWidth) {
+                        const indicesBuf = ((geometryState.primitive === gl.TRIANGLES)
+                                            ? geometry._getEdgeIndices()
+                                            : ((geometryState.primitive === gl.LINES) && geometryState.indicesBuf));
+
+                        if (indicesBuf) {
+                            if (geometryState.id !== lastGeometryId) {
+                                setGeometryInputsState(geometryState, () => frameCtx.bindArray++);
+
+                                indicesBuf.bind();
+                                frameCtx.bindArray++;
+                                lastGeometryId = geometryState.id;
+                            }
+
+                            gl.drawElements(gl.LINES, indicesBuf.numItems, indicesBuf.itemType, 0);
+
+                            frameCtx.drawElements++;
+                        }
+                    } else {
+                        if (geometryState.id !== lastGeometryId) {
+                            setGeometryInputsState(geometryState, () => frameCtx.bindArray++);
+
+                            if (geometryState.indicesBuf) {
+                                geometryState.indicesBuf.bind();
+                                frameCtx.bindArray++;
+                            }
+                            lastGeometryId = geometryState.id;
+                        }
 
                         if (geometryState.indicesBuf) {
-                            geometryState.indicesBuf.bind();
-                            frame.bindArray++;
+                            gl.drawElements(geometryState.primitive, geometryState.indicesBuf.numItems, geometryState.indicesBuf.itemType, 0);
+                            frameCtx.drawElements++;
+                        } else if (geometryState.positionsBuf) {
+                            gl.drawArrays(gl.TRIANGLES, 0, geometryState.positionsBuf.numItems);
+                            frameCtx.drawArrays++;
                         }
-                        lastGeometryId = geometryState.id;
                     }
-                    if (geometryState.indicesBuf) {
-                        gl.drawElements(geometryState.primitive, geometryState.indicesBuf.numItems, geometryState.indicesBuf.itemType, 0);
-                        frame.drawElements++;
-                    } else if (geometryState.positionsBuf) {
-                        gl.drawArrays(gl.TRIANGLES, 0, geometryState.positionsBuf.numItems);
-                        frame.drawArrays++;
+
+                    if (actsAsBackground) {
+                        gl.depthFunc(gl.LESS);
                     }
                 }
             };
