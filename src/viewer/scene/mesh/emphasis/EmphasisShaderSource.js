34498b0f5b6c6be9e7b5a4984edf1f477df82acf
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-04 20:53:15 +0100

    Combine EmphasisEdgesShaderSource and EmphasisFillShaderSource into EmphasisShaderSource

---
+++ src/viewer/scene/mesh/emphasis/EmphasisShaderSource.js
@@ -0,0 +1,61 @@
+import {createLightSetup} from "../MeshRenderer.js";
+import {math} from "../../math/math.js";
+const tmpVec4 = math.vec4();
+
+export const EmphasisShaderSource = function(mesh, isFill) {
+    const lightSetup = isFill && createLightSetup(mesh.scene._lightsState);
+    return {
+        getHash: () => [
+            mesh._state.hash,
+            mesh.scene.gammaOutput ? "go" : "", // Gamma input not needed
+            isFill && mesh.scene._lightsState.getHash()
+        ],
+        programName: isFill ? "EmphasisFill" : "EmphasisEdges",
+        dontSetFrontFace: true,
+        discardPoints: isFill,
+        drawEdges: ! isFill,
+        useGammaOutput: true,
+        appendVertexDefinitions: (src) => {
+            lightSetup && lightSetup.appendDefinitions(src);
+            src.push("uniform vec4 uColor;");
+            src.push("out vec4 vColor;");
+        },
+        appendVertexOutputs: (src, color, pickColor, uv, worldNormal, viewNormal) => {
+            if (lightSetup) {
+                src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
+                const geometry = mesh._geometry;
+                const geometryState = geometry._state;
+                if ((geometryState.autoVertexNormals || geometryState.normalsBuf) && [ "triangles", "triangle-strip", "triangle-fan" ].includes(geometry.primitive)) {
+                    lightSetup.directionalLights.forEach(light => {
+                        src.push(`reflectedColor += max(dot(${viewNormal}, ${light.getDirection("viewMatrix2", "viewPosition")}), 0.0) * ${light.getColor()};`);
+                    });
+                }
+                // TODO: A blending mode for emphasis materials, to select add/multiply/mix
+                //src.push("vColor = vec4((mix(reflectedColor, uColor.rgb, 0.7)), uColor.a);");
+                src.push(`vColor = vec4((${lightSetup.getAmbientColor()} + reflectedColor) * uColor.rgb, uColor.a);`);
+                //src.push("vColor = vec4(reflectedColor + uColor.rgb, uColor.a);");
+            } else {
+                src.push("vColor = uColor;");
+            }
+        },
+        appendFragmentDefinitions: (src) => {
+            src.push("in vec4 vColor;");
+            src.push("out vec4 outColor;");
+        },
+        appendFragmentOutputs: (src, getGammaOutputExpression) => src.push(`outColor = ${getGammaOutputExpression ? getGammaOutputExpression("vColor") : "vColor"};`),
+        setupMaterialInputs: (getInputSetter) => {
+            const uColor = getInputSetter("uColor");
+            return (mtl) => {
+                if (isFill) {
+                    tmpVec4.set(mtl.fillColor);
+                    tmpVec4[3] = mtl.fillAlpha;
+                } else {
+                    tmpVec4.set(mtl.edgeColor);
+                    tmpVec4[3] = mtl.edgeAlpha;
+                }
+                uColor(tmpVec4);
+            };
+        },
+        setupLightInputs: lightSetup && lightSetup.setupInputs
+    };
+};
