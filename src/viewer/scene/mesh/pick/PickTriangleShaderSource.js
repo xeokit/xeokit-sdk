ec7cf0008433c6a8ce8a5d7b65bb5c4786c3f993
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-07 20:47:46 +0100

    Move color and pickColor from appendVertexOutputs to appendFragmentOutputs definitions

--- src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
+++ src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
@@ -6,16 +6,9 @@ export const PickTriangleShaderSource = function(mesh) {
         trianglePick: true,
         dontBillboardAnything: true,
         transformClipPos: clipPos => `vec4((${clipPos}.xy / ${clipPos}.w - pickClipPos) * ${clipPos}.w, ${clipPos}.zw)`,
-        appendVertexDefinitions: (src) => {
-            src.push("uniform vec2 pickClipPos;");
-            src.push("out vec4 vColor;");
-        },
-        appendVertexOutputs: (src, color, pickColor) => src.push(`vColor = ${pickColor};`),
-        appendFragmentDefinitions: (src) => {
-            src.push("in vec4 vColor;");
-            src.push("out vec4 outColor;");
-        },
-        appendFragmentOutputs: (src) => src.push("outColor = vColor;"),
+        appendVertexDefinitions: (src) => src.push("uniform vec2 pickClipPos;"),
+        appendFragmentDefinitions: (src) => src.push("out vec4 outColor;"),
+        appendFragmentOutputs: (src, getGammaOutputExpression, gl_FragCoord, viewMatrix, attributes) => src.push(`outColor = ${attributes.pickColor};`),
         setupPickClipPosInputs: (getInputSetter) => {
             const pickClipPos = getInputSetter("pickClipPos");
             return (pos) => pickClipPos(pos);
