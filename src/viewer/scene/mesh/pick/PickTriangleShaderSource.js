6df86d3229344626a64c8503fb90d07fcce42551
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-07 21:28:30 +0100

    Move pickClipPos handling inside MeshRenderer

--- src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
+++ src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
@@ -2,16 +2,10 @@ export const PickTriangleShaderSource = function(mesh) {
     return {
         getHash: () => [ mesh._state.hash ],
         programName: "PickTriangle",
-        usePickView: true,
+        isPick: true,
         trianglePick: true,
         dontBillboardAnything: true,
-        transformClipPos: clipPos => `vec4((${clipPos}.xy / ${clipPos}.w - pickClipPos) * ${clipPos}.w, ${clipPos}.zw)`,
-        appendVertexDefinitions: (src) => src.push("uniform vec2 pickClipPos;"),
         appendFragmentDefinitions: (src) => src.push("out vec4 outColor;"),
-        appendFragmentOutputs: (src, getGammaOutputExpression, gl_FragCoord, viewMatrix, attributes) => src.push(`outColor = ${attributes.pickColor};`),
-        setupPickClipPosInputs: (getInputSetter) => {
-            const pickClipPos = getInputSetter("pickClipPos");
-            return (pos) => pickClipPos(pos);
-        }
+        appendFragmentOutputs: (src, getGammaOutputExpression, gl_FragCoord, viewMatrix, attributes) => src.push(`outColor = ${attributes.pickColor};`)
     };
 };
