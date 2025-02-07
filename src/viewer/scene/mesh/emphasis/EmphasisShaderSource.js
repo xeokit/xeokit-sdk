eea98a1de1ee68eb6bc429bece58d4bb65760f79
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-07 20:55:39 +0100

    Move uv from appendVertexOutputs to appendFragmentOutputs definitions

--- src/viewer/scene/mesh/emphasis/EmphasisShaderSource.js
+++ src/viewer/scene/mesh/emphasis/EmphasisShaderSource.js
@@ -20,7 +20,7 @@ export const EmphasisShaderSource = function(mesh, isFill) {
             src.push("uniform vec4 uColor;");
             src.push("out vec4 vColor;");
         },
-        appendVertexOutputs: (src, uv, world, view) => {
+        appendVertexOutputs: (src, world, view) => {
             if (lightSetup) {
                 src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
                 const geometry = mesh._geometry;
