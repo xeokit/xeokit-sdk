18b5294f341f17cde75fd95fcb4a5f2149bcd8d1
awilczura <anna.wilczura@creoox.com>
2025-02-09 20:05:05 +0100

    Update test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,11 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1280, height: 1024 },
+      },
+
     },
 
     // {
