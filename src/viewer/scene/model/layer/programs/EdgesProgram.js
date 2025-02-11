1dba7d851d69a3bfb3065dd8b0d6d53bbd4d8471
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-11 20:40:10 +0100

    Migrate EdgesProgram to lazyShaderUniform

--- src/viewer/scene/model/layer/programs/EdgesProgram.js
+++ src/viewer/scene/model/layer/programs/EdgesProgram.js
@@ -1,31 +1,34 @@
+import {lazyShaderUniform} from "../LayerRenderer.js";
+
 export const EdgesProgram = function(geometryParameters, logarithmicDepthBufferEnabled, colorUniform) {
+    const edgeColorUniform = colorUniform && lazyShaderUniform("edgeColor", "vec4");
     return {
         programName: colorUniform ? "Edges" : "EdgesColor",
         getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
         renderPassFlag: 2,  // EDGES_COLOR_OPAQUE | EDGES_COLOR_TRANSPARENT | EDGES_HIGHLIGHTED | EDGES_XRAYED | EDGES_SELECTED
         appendVertexDefinitions: (src) => {
-            if (! colorUniform) {
+            if (! edgeColorUniform) {
                 src.push("out vec4 vColor;");
             }
         },
         appendVertexOutputs: (src) => {
-            if (! colorUniform) {
+            if (! edgeColorUniform) {
                 const color = geometryParameters.attributes.color;
                 src.push(`vColor = vec4(${color}.rgb * 0.5, ${color}.a);`);
             }
         },
         appendFragmentDefinitions: (src) => {
-            if (colorUniform) {
-                src.push("uniform vec4 edgeColor;");
+            if (edgeColorUniform) {
+                edgeColorUniform.appendDefinitions(src);
             } else {
                 src.push("in vec4 vColor;");
             }
             src.push("out vec4 outColor;");
         },
-        appendFragmentOutputs: (src) => src.push("outColor = " + (colorUniform ? "edgeColor" : "vColor") + ";"),
-        setupInputs: colorUniform && ((getUniformSetter) => {
-            const edgeColor = getUniformSetter("edgeColor");
-            return (frameCtx, textureSet) => edgeColor(frameCtx.programColor);
+        appendFragmentOutputs: (src) => src.push("outColor = " + (edgeColorUniform || "vColor") + ";"),
+        setupInputs: edgeColorUniform && ((getUniformSetter) => {
+            const setEdgeColor = edgeColorUniform.setupInputs(getUniformSetter);
+            return (frameCtx, textureSet) => setEdgeColor(frameCtx.programColor);
         })
     };
 };
