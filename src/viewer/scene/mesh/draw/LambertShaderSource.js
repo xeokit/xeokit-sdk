83a6000f1cf5b8068c82ac09315329488c3d6048
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-12 21:14:57 +0100

    Migrate mesh renderers to programVariablesState

--- src/viewer/scene/mesh/draw/LambertShaderSource.js
+++ src/viewer/scene/mesh/draw/LambertShaderSource.js
@@ -1,12 +1,14 @@
-import {createLightSetup, lazyShaderUniform} from "../MeshRenderer.js";
+import {createLightSetup} from "../MeshRenderer.js";
 import {math} from "../../math/math.js";
 const tmpVec4 = math.vec4();
 
-export const LambertShaderSource = function(meshDrawHash, geometry, material, scene) {
-    const lightSetup = createLightSetup(scene._lightsState);
-    const colorize         = lazyShaderUniform("colorize",         "vec4");
-    const materialColor    = lazyShaderUniform("materialColor",    "vec4");
-    const materialEmissive = lazyShaderUniform("materialEmissive", "vec3");
+export const LambertShaderSource = function(meshDrawHash, programVariables, geometry, material, scene) {
+    const lightSetup       = createLightSetup(programVariables, scene._lightsState, false);
+    const colorize         = programVariables.createUniform("vec4", "colorize");
+    const materialColor    = programVariables.createUniform("vec4", "materialColor");
+    const materialEmissive = programVariables.createUniform("vec3", "materialEmissive");
+    const vColor           = programVariables.createVarying("vec4", "vColor");
+    const outColor         = programVariables.createOutput("vec4", "outColor");
 
     return {
         getHash: () => [
@@ -21,26 +23,15 @@ export const LambertShaderSource = function(meshDrawHash, geometry, material, sc
         setupPointSize: true,
         setsLineWidth: true,
         useGammaOutput: true,
-        appendVertexDefinitions: (src) => {
-            colorize.appendDefinitions(src);
-            materialColor.appendDefinitions(src);
-            materialEmissive.appendDefinitions(src);
-            lightSetup.appendDefinitions(src);
-            src.push("out vec4 vColor;");
-        },
         appendVertexOutputs: (src) => {
             src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
             const attributes = geometry.attributes;
             attributes.normal && lightSetup.directionalLights.forEach(light => {
                 src.push(`reflectedColor += max(dot(${attributes.normal.view}, ${light.getDirection(geometry.viewMatrix, attributes.position.view)}), 0.0) * ${light.getColor()};`);
             });
-            src.push(`vColor = ${colorize} * vec4((${lightSetup.getAmbientColor()} + reflectedColor) * ${materialColor}.rgb + ${materialEmissive}, ${materialColor}.a);`); // TODO: How to have ambient bright enough for canvas BG but not too bright for scene?
+            src.push(`${vColor} = ${colorize} * vec4((${lightSetup.getAmbientColor()} + reflectedColor) * ${materialColor}.rgb + ${materialEmissive}, ${materialColor}.a);`); // TODO: How to have ambient bright enough for canvas BG but not too bright for scene?
         },
-        appendFragmentDefinitions: (src) => {
-            src.push("in vec4 vColor;");
-            src.push("out vec4 outColor;");
-        },
-        appendFragmentOutputs: (src, getGammaOutputExpression) => src.push(`outColor = ${getGammaOutputExpression ? getGammaOutputExpression("vColor") : "vColor"};`),
+        appendFragmentOutputs: (src, getGammaOutputExpression) => src.push(`${outColor} = ${getGammaOutputExpression ? getGammaOutputExpression(vColor) : vColor};`),
         setupMeshInputs: (getInputSetter) => {
             const setColorize = colorize.setupInputs(getInputSetter);
             return (mesh) => setColorize(mesh.colorize);
