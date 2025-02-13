4694e77f91f3f76ad23e561509299afcebfbf992
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-13 20:41:52 +0100

    Replace setupProgramInputs by appendFragmentOutputs return

--- src/viewer/scene/mesh/pick/PickMeshShaderSource.js
+++ src/viewer/scene/mesh/pick/PickMeshShaderSource.js
@@ -9,19 +9,21 @@ export const PickMeshShaderSource = function(meshHash, programVariables) {
         programName: "PickMesh",
         isPick: true,
         dontBillboardAnything: true,
-        appendFragmentOutputs: (src) => src.push(`${outColor} = ${pickColor};`),
-        setupProgramInputs: () => {
-            const setPickColor = pickColor.setupInputs();
-            return {
-                setMeshStateValues: (mesh) => {
-                    var pickID = mesh._state.pickID; // Mesh-indexed color
-                    tmpVec4[0] = pickID       & 0xFF;
-                    tmpVec4[1] = pickID >>  8 & 0xFF;
-                    tmpVec4[2] = pickID >> 16 & 0xFF;
-                    tmpVec4[3] = pickID >> 24 & 0xFF;
-                    math.mulVec4Scalar(tmpVec4, 1 / 255, tmpVec4);
-                    setPickColor(tmpVec4);
-                }
+        appendFragmentOutputs: (src) => {
+            src.push(`${outColor} = ${pickColor};`);
+            return () => {
+                const setPickColor = pickColor.setupInputs();
+                return {
+                    setMeshStateValues: (mesh) => {
+                        var pickID = mesh._state.pickID; // Mesh-indexed color
+                        tmpVec4[0] = pickID       & 0xFF;
+                        tmpVec4[1] = pickID >>  8 & 0xFF;
+                        tmpVec4[2] = pickID >> 16 & 0xFF;
+                        tmpVec4[3] = pickID >> 24 & 0xFF;
+                        math.mulVec4Scalar(tmpVec4, 1 / 255, tmpVec4);
+                        setPickColor(tmpVec4);
+                    }
+                };
             };
         }
     };
