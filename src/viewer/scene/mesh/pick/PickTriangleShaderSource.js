e2d4afe3d85ad618f42bfe758b51e4dd8f1ddda3
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-13 20:35:32 +0100

    Move mesh varyings assignments from appendVertexOutputs to their own definitions

--- src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
+++ src/viewer/scene/mesh/pick/PickTriangleShaderSource.js
@@ -1,5 +1,5 @@
 export const PickTriangleShaderSource = function(meshHash, programVariables, geometry) {
-    const vColor = programVariables.createVarying("vec4", "vColor");
+    const vColor = programVariables.createVarying("vec4", "vColor", () => geometry.attributes.pickColor);
     const outColor = programVariables.createOutput("vec4", "outColor");
     return {
         getHash: () => [ meshHash ],
@@ -7,7 +7,6 @@ export const PickTriangleShaderSource = function(meshHash, programVariables, geo
         isPick: true,
         trianglePick: true,
         dontBillboardAnything: true,
-        appendVertexOutputs: (src) => src.push(`${vColor} = ${geometry.attributes.pickColor};`),
         appendFragmentOutputs: (src) => src.push(`${outColor} = ${vColor};`)
     };
 };
