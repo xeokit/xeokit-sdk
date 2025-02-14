937b7aeed7b85d1fbb0640be891624274b3dbf05
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-14 20:56:13 +0100

    Migrate mesh attribute and uniform assignments from setupInputs to their own definitions

--- src/viewer/scene/mesh/pick/PickMeshShaderSource.js
+++ src/viewer/scene/mesh/pick/PickMeshShaderSource.js
@@ -2,29 +2,21 @@ import {math} from "../../math/math.js";
 const tmpVec4 = math.vec4();
 
 export const PickMeshShaderSource = function(meshHash, programVariables) {
-    const pickColor = programVariables.createUniform("vec4", "pickColor");
+    const pickColor = programVariables.createUniform("vec4", "pickColor", (set, state) => {
+        var pickID = state.mesh._state.pickID; // Mesh-indexed color
+        tmpVec4[0] = pickID       & 0xFF;
+        tmpVec4[1] = pickID >>  8 & 0xFF;
+        tmpVec4[2] = pickID >> 16 & 0xFF;
+        tmpVec4[3] = pickID >> 24 & 0xFF;
+        math.mulVec4Scalar(tmpVec4, 1 / 255, tmpVec4);
+        set(tmpVec4);
+    });
     const outColor = programVariables.createOutput("vec4", "outColor");
     return {
         getHash: () => [ meshHash ],
         programName: "PickMesh",
         isPick: true,
         dontBillboardAnything: true,
-        appendFragmentOutputs: (src) => {
-            src.push(`${outColor} = ${pickColor};`);
-            return () => {
-                const setPickColor = pickColor.setupInputs();
-                return {
-                    setMeshStateValues: (mesh) => {
-                        var pickID = mesh._state.pickID; // Mesh-indexed color
-                        tmpVec4[0] = pickID       & 0xFF;
-                        tmpVec4[1] = pickID >>  8 & 0xFF;
-                        tmpVec4[2] = pickID >> 16 & 0xFF;
-                        tmpVec4[3] = pickID >> 24 & 0xFF;
-                        math.mulVec4Scalar(tmpVec4, 1 / 255, tmpVec4);
-                        setPickColor(tmpVec4);
-                    }
-                };
-            };
-        }
+        appendFragmentOutputs: (src) => src.push(`${outColor} = ${pickColor};`)
     };
 };
