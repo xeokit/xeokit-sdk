cf824349441aca9db839a81703ff33dd32b71a95
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-14 22:01:46 +0100

    Move encodeFloat function definition inside createFragmentDefinition

--- src/viewer/scene/mesh/shadow/ShadowShaderSource.js
+++ src/viewer/scene/mesh/shadow/ShadowShaderSource.js
@@ -1,4 +1,15 @@
 export const ShadowShaderSource = function(meshHash, programVariables) {
+    const encodeFloat = programVariables.createFragmentDefinition(
+        "encodeFloat",
+        (name, src) => {
+            src.push(`vec4 ${name}( const in float depth ) {`);
+            src.push("  const vec4 bitShift = vec4(256 * 256 * 256, 256 * 256, 256, 1.0);");
+            src.push("  const vec4 bitMask = vec4(0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0);");
+            src.push("  vec4 comp = fract(depth * bitShift);");
+            src.push("  comp -= comp.xxyz * bitMask;");
+            src.push("  return comp;");
+            src.push("}");
+        });
     const outColor = programVariables.createOutput("vec4", "outColor");
     return {
         getHash: () => [ meshHash ],
@@ -7,15 +18,6 @@ export const ShadowShaderSource = function(meshHash, programVariables) {
         useShadowView: true,
         dontBillboardAnything: true,
         dontGetLogDepth: true,
-        appendFragmentDefinitions: (src) => {
-            src.push("vec4 encodeFloat( const in float depth ) {");
-            src.push("  const vec4 bitShift = vec4(256 * 256 * 256, 256 * 256, 256, 1.0);");
-            src.push("  const vec4 bitMask = vec4(0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0);");
-            src.push("  vec4 comp = fract(depth * bitShift);");
-            src.push("  comp -= comp.xxyz * bitMask;");
-            src.push("  return comp;");
-            src.push("}");
-        },
-        appendFragmentOutputs: (src, getGammaOutputExpression, gl_FragCoord) => src.push(`${outColor} = encodeFloat(${gl_FragCoord}.z);`)
+        appendFragmentOutputs: (src, getGammaOutputExpression, gl_FragCoord) => src.push(`${outColor} = ${encodeFloat}(${gl_FragCoord}.z);`)
     };
 };
