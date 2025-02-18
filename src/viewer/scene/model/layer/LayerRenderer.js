3f74d114c120624b19808d9f737ea83b76efefd2
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-18 23:49:16 +0100

    Migrate DTX indices textures to setupTexture

--- src/viewer/scene/model/layer/LayerRenderer.js
+++ src/viewer/scene/model/layer/LayerRenderer.js
@@ -177,7 +177,6 @@ export class LayerRenderer {
 
         const buildVertexShader = () => {
             const src = [];
-            renderingAttributes.appendVertexDefinitions(src);
             programVariablesState.appendVertexDefinitions(src);
             src.push("void main(void) {");
             vertexData.forEach(line => src.push(line));
@@ -251,9 +250,7 @@ export class LayerRenderer {
             return;
         }
 
-        const getInputSetter = makeInputSetters(gl, program.handle);
-        const inputSetters = programVariablesState.setupInputs(getInputSetter);
-        const drawCall = renderingAttributes.makeDrawCall(getInputSetter);
+        const inputSetters = programVariablesState.setupInputs(makeInputSetters(gl, program.handle));
 
         this.destroy = () => program.destroy();
         this.drawLayer = (frameCtx, layer, renderPass) => {
@@ -285,7 +282,7 @@ export class LayerRenderer {
 
             const layerDrawState = layer.layerDrawState;
 
-            inputSetters.setUniforms({
+            const state = {
                 legacyFrameCtx: frameCtx,
                 layerDrawState: layerDrawState,
                 mesh: {
@@ -303,9 +300,9 @@ export class LayerRenderer {
                     viewMatrix:       rtcViewMatrix,
                     viewNormalMatrix: camera.viewNormalMatrix
                 }
-            });
+            };
 
-            drawCall(layerDrawState, inputSetters);
+            renderingAttributes.drawCall(layerDrawState, inputSetters, state);
 
             if (incrementDrawState) {
                 frameCtx.drawElements++;
