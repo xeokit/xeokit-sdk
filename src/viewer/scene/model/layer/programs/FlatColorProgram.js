81a324a65efa905adbf416523ac963f151a4c1c7
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-18 17:30:52 +0100

    Migrate Layer Programs to programVariables

--- src/viewer/scene/model/layer/programs/FlatColorProgram.js
+++ src/viewer/scene/model/layer/programs/FlatColorProgram.js
@@ -1,41 +1,23 @@
-export const FlatColorProgram = function(geometryParameters, logarithmicDepthBufferEnabled, lightSetup, sao) {
+export const FlatColorProgram = function(programVariables, geometry, logarithmicDepthBufferEnabled, lightSetup, sao) {
+    const attributes = geometry.attributes;
+    const vViewPosition = programVariables.createVarying("vec3", "vViewPosition", () => `${attributes.position.view}.xyz`);
+    const vColor = programVariables.createVarying("vec4", "vColor", () => attributes.color);
+    const outColor = programVariables.createOutput("vec4", "outColor");
     return {
         programName: "FlatColor",
         getHash: () => [lightSetup.getHash(), sao ? "sao" : "nosao"],
         getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
         renderPassFlag: 0,      // COLOR_OPAQUE | COLOR_TRANSPARENT
-        appendVertexDefinitions: (src) => {
-            src.push("out vec4 vViewPosition;");
-            src.push("out vec4 vColor;");
-        },
-        appendVertexOutputs: (src) => {
-            src.push(`vViewPosition = ${geometryParameters.attributes.position.view};`);
-            src.push(`vColor = ${geometryParameters.attributes.color};`);
-        },
-        appendFragmentDefinitions: (src) => {
-            lightSetup.appendDefinitions(src);
-            sao && sao.appendDefinitions(src);
-            src.push("in vec4 vViewPosition;");
-            src.push("in vec4 vColor;");
-            src.push("out vec4 outColor;");
-        },
         appendFragmentOutputs: (src, vWorldPosition, gl_FragCoord, sliceColorOr) => {
-            src.push("vec3 viewNormal = normalize(cross(dFdx(vViewPosition.xyz), dFdy(vViewPosition.xyz)));");
-            src.push("vec3 reflectedColor = vec3(0.0, 0.0, 0.0);");
-            lightSetup.directionalLights.forEach(light => {
-                src.push(`reflectedColor += max(dot(-viewNormal, ${light.getDirection(geometryParameters.viewMatrix, "vViewPosition")}), 0.0) * ${light.getColor()};`);
-            });
+            src.push(`vec3 viewNormal = normalize(cross(dFdx(${vViewPosition}), dFdy(${vViewPosition})));`);
+            const lightComponents = [
+                lightSetup.getAmbientColor()
+            ].concat(
+                lightSetup.directionalLights.map(
+                    light => `max(dot(viewNormal, ${light.getDirection(geometry.viewMatrix, vViewPosition)}), 0.0) * ${light.getColor()}`));
 
-            src.push(`vec4 fragColor = vec4(${lightSetup.getAmbientColor()} + reflectedColor, 1) * ${sliceColorOr("vColor")};`);
-            src.push("outColor = " + (sao ? ("vec4(fragColor.rgb * " + sao.getAmbient(gl_FragCoord) + ", fragColor.a)") : "fragColor") + ";");
-        },
-        setupInputs: (getUniformSetter) => {
-            const setLightsRenderState = lightSetup.setupInputs(getUniformSetter);
-            const setSAOState = sao && sao.setupInputs(getUniformSetter);
-            return (frameCtx, textureSet) => {
-                setLightsRenderState(frameCtx);
-                setSAOState && setSAOState(frameCtx);
-            };
+            src.push(`vec4 fragColor = vec4(${lightComponents.join(" + ")}, 1) * ${sliceColorOr(vColor)};`);
+            src.push(`${outColor} = ${sao ? `vec4(fragColor.rgb * ${sao.getAmbient(gl_FragCoord)}, fragColor.a)` : "fragColor"};`);
         }
     };
 };
