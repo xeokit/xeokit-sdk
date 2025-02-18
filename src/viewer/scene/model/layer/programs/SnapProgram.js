2beaf0f88fa36263ed6642e80f26b2eb08154b13
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-18 17:30:52 +0100

    Migrate Layer Programs to programVariables

--- src/viewer/scene/model/layer/programs/SnapProgram.js
+++ src/viewer/scene/model/layer/programs/SnapProgram.js
@@ -1,11 +1,16 @@
 import {math} from "../../../math/math.js";
-import {lazyShaderUniform} from "../LayerRenderer.js";
 
-export const SnapProgram = function(geometryParameters, isSnapInit, isPoints) {
-    const snapVectorA      = lazyShaderUniform("snapVectorA",     "vec2");
-    const snapInvVectorAB  = lazyShaderUniform("snapInvVectorAB", "vec2");
-    const layerNumber      = lazyShaderUniform("layerNumber",      "int");
-    const coordinateScaler = lazyShaderUniform("coordinateScaler", "vec3");
+export const SnapProgram = function(programVariables, geometry, isSnapInit, isPoints) {
+    const snapVectorA      = programVariables.createUniform("vec2", "snapVectorA",      (set, state) => set(state.legacyFrameCtx.snapVectorA));
+    const snapInvVectorAB  = programVariables.createUniform("vec2", "snapInvVectorAB",  (set, state) => set(state.legacyFrameCtx.snapInvVectorAB));
+    const layerNumber      = programVariables.createUniform("int",  "layerNumber",      (set, state) => set(state.legacyFrameCtx.snapPickLayerNumber));
+    const coordinateScaler = programVariables.createUniform("vec3", "coordinateScaler", (set, state) => set(state.legacyFrameCtx.snapPickCoordinateScale));
+
+    const vPickColor = programVariables.createVarying("vec4", "vPickColor", () => geometry.attributes.pickColor, "flat");
+
+    const outCoords    = programVariables.createOutput("highp ivec4", "outCoords", 0);
+    const outNormal    = programVariables.createOutput("highp ivec4", "outNormal", 1);
+    const outPickColor = programVariables.createOutput("lowp uvec4", "outPickColor", 2);
 
     return {
         programName: isSnapInit ? "SnapInit" : "Snap",
@@ -13,48 +18,15 @@ export const SnapProgram = function(geometryParameters, isSnapInit, isPoints) {
         getLogDepth: true && (vFragDepth => (isSnapInit ? `${vFragDepth} + length(vec2(dFdx(${vFragDepth}), dFdy(${vFragDepth})))` : vFragDepth)),
         renderPassFlag: 3,  // PICK
         usePickParams: true,
-        appendVertexDefinitions: (src) => {
-            snapVectorA.appendDefinitions(src);
-            snapInvVectorAB.appendDefinitions(src);
-            if (isSnapInit) {
-                src.push("flat out vec4 vPickColor;");
-            }
-        },
         transformClipPos: clipPos => `vec4((${clipPos}.xy / ${clipPos}.w - ${snapVectorA}) * ${snapInvVectorAB} * ${clipPos}.w, ${clipPos}.zw)`,
-        appendVertexOutputs: isSnapInit && ((src) => src.push(`vPickColor = ${geometryParameters.attributes.pickColor};`)),
-        appendFragmentDefinitions: (src) => {
-            layerNumber.appendDefinitions(src);
-            coordinateScaler.appendDefinitions(src);
-            if (isSnapInit) {
-                src.push("flat in vec4 vPickColor;");
-                src.push("layout(location = 0) out highp ivec4 outCoords;");
-                src.push("layout(location = 1) out highp ivec4 outNormal;");
-                src.push("layout(location = 2) out lowp uvec4 outPickColor;");
-            } else {
-                src.push("out highp ivec4 outCoords;");
-            }
-        },
         vertexCullX: (!isSnapInit) && "2.0",
+        dontCullOnAlphaZero: !isSnapInit,
         appendFragmentOutputs: (src, vWorldPosition) => {
-            src.push(`outCoords = ivec4(${vWorldPosition} * ${coordinateScaler}.xyz, ${isSnapInit ? "-" : ""}${layerNumber});`);
+            src.push(`${outCoords} = ivec4(${vWorldPosition} * ${coordinateScaler}.xyz, ${isSnapInit ? "-" : ""}${layerNumber});`);
             if (isSnapInit) {
-                src.push(`outNormal = ${isPoints ? "ivec4(1.0)" : `ivec4(normalize(cross(dFdx(${vWorldPosition}), dFdy(${vWorldPosition}))) * float(${math.MAX_INT}), 1.0)`};`);
-                src.push("outPickColor = uvec4(vPickColor);");
+                src.push(`${outNormal} = ${isPoints ? "ivec4(1.0)" : `ivec4(normalize(cross(dFdx(${vWorldPosition}), dFdy(${vWorldPosition}))) * float(${math.MAX_INT}), 1.0)`};`);
+                src.push(`${outPickColor} = uvec4(${vPickColor});`);
             }
-        },
-        setupInputs: (getUniformSetter) => {
-            const setSnapVectorA      = snapVectorA.setupInputs(getUniformSetter);
-            const setSnapInvVectorAB  = snapInvVectorAB.setupInputs(getUniformSetter);
-            const setLayerNumber      = layerNumber.setupInputs(getUniformSetter);
-            const setCoordinateScaler = coordinateScaler.setupInputs(getUniformSetter);
-            return (frameCtx, textureSet) => {
-                setSnapVectorA(frameCtx.snapVectorA);
-                setSnapInvVectorAB(frameCtx.snapInvVectorAB);
-                setLayerNumber(frameCtx.snapPickLayerNumber);
-                setCoordinateScaler(frameCtx.snapPickCoordinateScale);
-            };
-        },
-
-        dontCullOnAlphaZero: !isSnapInit
+        }
     };
 };
