19487b8c253d212c8f8b207f702bcb45cfd8cc8b
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-18 17:30:52 +0100

    Migrate Layer Programs to programVariables

--- src/viewer/scene/model/layer/programs/EdgesProgram.js
+++ src/viewer/scene/model/layer/programs/EdgesProgram.js
@@ -1,34 +1,12 @@
-import {lazyShaderUniform} from "../LayerRenderer.js";
-
-export const EdgesProgram = function(geometryParameters, logarithmicDepthBufferEnabled, colorUniform) {
-    const edgeColorUniform = colorUniform && lazyShaderUniform("edgeColor", "vec4");
+export const EdgesProgram = function(programVariables, geometry, logarithmicDepthBufferEnabled, colorUniform) {
+    const edgeColorUniform = colorUniform && programVariables.createUniform("vec4", "edgeColor", (set, state) => set(state.legacyFrameCtx.programColor));
+    const color = geometry.attributes.color;
+    const vColor = (!colorUniform) && programVariables.createVarying("vec4", "vColor", () => `vec4(${color}.rgb * 0.5, ${color}.a)`);
+    const outColor = programVariables.createOutput("vec4", "outColor");
     return {
         programName: colorUniform ? "Edges" : "EdgesColor",
         getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
         renderPassFlag: 2,  // EDGES_COLOR_OPAQUE | EDGES_COLOR_TRANSPARENT | EDGES_HIGHLIGHTED | EDGES_XRAYED | EDGES_SELECTED
-        appendVertexDefinitions: (src) => {
-            if (! edgeColorUniform) {
-                src.push("out vec4 vColor;");
-            }
-        },
-        appendVertexOutputs: (src) => {
-            if (! edgeColorUniform) {
-                const color = geometryParameters.attributes.color;
-                src.push(`vColor = vec4(${color}.rgb * 0.5, ${color}.a);`);
-            }
-        },
-        appendFragmentDefinitions: (src) => {
-            if (edgeColorUniform) {
-                edgeColorUniform.appendDefinitions(src);
-            } else {
-                src.push("in vec4 vColor;");
-            }
-            src.push("out vec4 outColor;");
-        },
-        appendFragmentOutputs: (src) => src.push("outColor = " + (edgeColorUniform || "vColor") + ";"),
-        setupInputs: edgeColorUniform && ((getUniformSetter) => {
-            const setEdgeColor = edgeColorUniform.setupInputs(getUniformSetter);
-            return (frameCtx, textureSet) => setEdgeColor(frameCtx.programColor);
-        })
+        appendFragmentOutputs: (src) => src.push(`${outColor} = ${edgeColorUniform || vColor};`)
     };
 };
--- src/viewer/scene/model/layer/programs/PickDepthProgram.js
+++ src/viewer/scene/model/layer/programs/PickDepthProgram.js
@@ -1,52 +1,32 @@
-import {lazyShaderUniform} from "../LayerRenderer.js";
-
-export const PickDepthProgram = function(geometryParameters, logarithmicDepthBufferEnabled, clipTransformSetup, isPoints) {
-    const pickZNear = lazyShaderUniform("pickZNear", "float");
-    const pickZFar  = lazyShaderUniform("pickZFar",  "float");
-
-    return {
-        programName: "PickDepth",
-        getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
-        renderPassFlag: 3,  // PICK
-        usePickParams: true,
-        appendVertexDefinitions: (src) => {
-            src.push("out vec4 vViewPosition;");
-            clipTransformSetup.appendDefinitions(src);
-        },
-        transformClipPos: clipTransformSetup.transformClipPos,
-        appendVertexOutputs: (src) => {
-            src.push(`vViewPosition = ${geometryParameters.attributes.position.view};`);
-            if (isPoints) {
-                src.push("gl_PointSize += 10.0;");
-            }
-        },
-        appendFragmentDefinitions: (src) => {
-            pickZNear.appendDefinitions(src);
-            pickZFar.appendDefinitions(src);
-            src.push("in vec4 vViewPosition;");
-            src.push("vec4 packDepth(const in float depth) {");
+export const PickDepthProgram = function(programVariables, geometry, logarithmicDepthBufferEnabled, clipTransformSetup) {
+    const pickZFar  = programVariables.createUniform("float", "pickZFar",  (set, state) => set(state.legacyFrameCtx.pickZFar));
+    const pickZNear = programVariables.createUniform("float", "pickZNear", (set, state) => set(state.legacyFrameCtx.pickZNear));
+    const vViewPosition = programVariables.createVarying("vec3", "vViewPosition", () => `${geometry.attributes.position.view}.xyz`);
+    const outPackedDepth = programVariables.createOutput("vec4", "outPackedDepth");
+    const packDepth = programVariables.createFragmentDefinition(
+        "packDepth",
+        (name, src) => {
+            src.push(`vec4 ${name}(const in float depth) {`);
             src.push("  const vec4 bitShift = vec4(256.0*256.0*256.0, 256.0*256.0, 256.0, 1.0);");
             src.push("  const vec4 bitMask  = vec4(0.0, 1.0/256.0, 1.0/256.0, 1.0/256.0);");
             src.push("  vec4 res = fract(depth * bitShift);");
             src.push("  res -= res.xxyz * bitMask;");
             src.push("  return res;");
             src.push("}");
-            src.push("out vec4 outPackedDepth;");
-        },
+        });
+
+    return {
+        programName: "PickDepth",
+        getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
+        renderPassFlag: 3,  // PICK
+        incPointSizeBy10: true,
+        usePickParams: true,
+        transformClipPos: clipTransformSetup.transformClipPos,
         appendFragmentOutputs: (src) => {
-            src.push(`float zNormalizedDepth = abs((pickZNear + vViewPosition.z) / (${pickZFar} - ${pickZNear}));`);
-            src.push("outPackedDepth = packDepth(zNormalizedDepth);"); // Must be linear depth
+            src.push(`float zNormalizedDepth = abs((${pickZNear} + ${vViewPosition}.z) / (${pickZFar} - ${pickZNear}));`);
+            // isn't zNormalizedDepth the same as gl_FragDepth or gl_FragCoord.z?
+            src.push(`${outPackedDepth} = ${packDepth}(zNormalizedDepth);`); // Must be linear depth
             // try: src.push("    outPackedDepth = vec4(zNormalizedDepth, fract(zNormalizedDepth * vec3(256.0, 256.0*256.0, 256.0*256.0*256.0)));");
-        },
-        setupInputs: (getUniformSetter) => {
-            const setPickZNear = pickZNear.setupInputs(getUniformSetter);
-            const setPickZFar  = pickZFar.setupInputs(getUniformSetter);
-            const setClipTransformState = clipTransformSetup.setupInputs(getUniformSetter);
-            return (frameCtx, textureSet) => {
-                setPickZNear(frameCtx.pickZNear);
-                setPickZFar(frameCtx.pickZFar);
-                setClipTransformState(frameCtx);
-            };
         }
     };
 };
