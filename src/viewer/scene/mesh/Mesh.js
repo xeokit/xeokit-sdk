6bb3c1396d20b5cdc855a3c1183faa3d30373673
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-19 20:34:01 +0100

    Abstract fragment shader assembly inside buildProgram

--- src/viewer/scene/mesh/Mesh.js
+++ src/viewer/scene/mesh/Mesh.js
@@ -2281,13 +2281,6 @@ const instantiateMeshRenderer = (mesh, attributes, auxVariables, programSetup, p
         });
     programSetup.appendFragmentOutputs(programFragmentOutputs, scene.gammaOutput && ((color) => `${linearToGamma}(${color}, ${gammaFactor})`), "gl_FragCoord");
 
-    const fragmentShader = [
-        ...programVariablesState.getFragmentDefinitions(),
-        "void main(void) {",
-        ...programFragmentOutputs,
-        "}"
-    ];
-
     const getVertexData = function() {
         const viewNormalDefinition = viewNormal && viewNormal.needed && `vec3 ${viewNormal} = normalize((${billboardIfApplicable(viewNormalMatrix)} * vec4(${worldNormal}, 0.0)).xyz);`;
         const src = [ ];
@@ -2335,7 +2328,7 @@ const instantiateMeshRenderer = (mesh, attributes, auxVariables, programSetup, p
         gl,
         programSetup.programName,
         {
-            fragmentShader:   fragmentShader,
+            fragmentOutputs:  programFragmentOutputs,
             getPointSize:     programSetup.setupPointSize && isPoints && (() => pointSize),
             getVertexData:    getVertexData,
             projMatrix:       projMatrix,
