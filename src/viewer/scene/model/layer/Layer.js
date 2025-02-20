a30c24377a4cf116f920e65844d55b9c87a7c4f6
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-20 21:31:39 +0100

    Inline LayerRenderer inside getRenderers

--- src/viewer/scene/model/layer/Layer.js
+++ src/viewer/scene/model/layer/Layer.js
@@ -1,9 +1,7 @@
-import {math} from "../../math/index.js";
+import {createRTCViewMat, math} from "../../math/index.js";
 import {ENTITY_FLAGS} from "../ENTITY_FLAGS.js";
 import {createLightSetup, createProgramVariablesState, setupTexture} from '../../webgl/WebGLRenderer.js';
 import {LinearEncoding} from "../../constants/constants.js";
-
-import {LayerRenderer} from "./LayerRenderer.js";
 
 import { ColorProgram        } from "./programs/ColorProgram.js";
 import { ColorTextureProgram } from "./programs/ColorTextureProgram.js";
@@ -22,6 +20,8 @@ import { SnapProgram         } from "./programs/SnapProgram.js";
 const tempVec2 = math.vec2();
 const tempVec3 = math.vec3();
 const tempVec4 = math.vec4();
+const tempMat4 = math.mat4();
+const vec3zero = math.vec3([0,0,0]);
 
 const RENDER_PASSES = {
     // Skipped - suppress rendering
@@ -163,22 +163,178 @@ export const getRenderers = (function() {
         const cache = cachedRenderers[cacheKey][primKey];
         const sceneId = scene.id;
         if (! (sceneId in cache)) {
-
+            const gl = scene.canvas.gl;
             const wrapRenderer = function(createProgramSetup, subGeometry, isEager) {
                 const instantiate = function() {
                     const programVariablesState = createProgramVariablesState();
-                    const renderingAttributes = makeRenderingAttributes(programVariablesState.programVariables, subGeometry);
+                    const programVariables = programVariablesState.programVariables;
+                    const renderingAttributes = makeRenderingAttributes(programVariables, subGeometry);
                     return createProgramSetup(
-                        programVariablesState.programVariables,
+                        programVariables,
                         renderingAttributes.geometryParameters,
                         function(programSetup) {
-                            return new LayerRenderer(
-                                scene,
-                                primitive,
-                                programSetup,
-                                subGeometry,
-                                renderingAttributes,
-                                programVariablesState);
+                            const pointsMaterial     = scene.pointsMaterial;
+                            const setupPoints        = (primitive === "points") && (! subGeometry);
+
+                            const getHash = () => [ scene._sectionPlanesState.getHash() ].concat(setupPoints ? [ pointsMaterial.hash ] : [ ]).concat(programSetup.getHash ? programSetup.getHash() : [ ]).join(";");
+                            const hash = getHash();
+                            const getValid = () => hash === getHash();
+
+                            const geometryParameters     = renderingAttributes.geometryParameters;
+                            const gl                     = scene.canvas.gl;
+                            const incrementDrawState     = programSetup.incrementDrawState;
+                            const isShadowProgram        = programSetup.isShadowProgram;
+                            const usePickParams          = programSetup.usePickParams;
+                            const worldPositionAttribute = geometryParameters.attributes.position.world;
+
+                            const [ program, errors ] = programVariablesState.buildProgram(
+                                gl,
+                                primitive + " " + renderingAttributes.signature + " " + programSetup.programName,
+                                {
+                                    appendFragmentOutputs:          programSetup.appendFragmentOutputs,
+                                    clippableTest:                  (function() {
+                                        const vClippable = programVariables.createVarying("float", "vClippable", () => renderingAttributes.getClippable(), "flat");
+                                        return () => `${vClippable} > 0.0`;
+                                    })(),
+                                    clippingCaps:                   programSetup.clippingCaps,
+                                    discardPoints:                  setupPoints && pointsMaterial.roundPoints,
+                                    fragmentOutputsSetup:           [ ],
+                                    getLogDepth:                    programSetup.getLogDepth,
+                                    getPointSize:                   (function() {
+                                        const addends = [ ];
+                                        if (setupPoints) {
+                                            const pointSize = programVariables.createUniform("float", "pointSize", (set) => set(pointsMaterial.pointSize));
+                                            if (pointsMaterial.perspectivePoints) {
+                                                const nearPlaneHeight = programVariables.createUniform("float", "nearPlaneHeight", (set) => {
+                                                    set((scene.camera.projection === "ortho") ?
+                                                        1.0
+                                                        : (gl.drawingBufferHeight / (2 * Math.tan(0.5 * scene.camera.perspective.fov * Math.PI / 180.0))));
+                                                });
+                                                const minVal = `${Math.floor(pointsMaterial.minPerspectivePointSize)}.0`;
+                                                const maxVal = `${Math.floor(pointsMaterial.maxPerspectivePointSize)}.0`;
+                                                addends.push(`clamp((${nearPlaneHeight} * ${pointSize}) / gl_Position.w, ${minVal}, ${maxVal})`);
+                                            } else {
+                                                addends.push(pointSize);
+                                            }
+                                        } else if (subGeometry && subGeometry.vertices) {
+                                            addends.push("1.0");
+                                        }
+                                        programSetup.incPointSizeBy10 && (primitive === "points") && addends.push("10.0");
+                                        return (addends.length > 0) && (() => addends.join(" + "));
+                                    })(),
+                                    getVertexData:                  () => {
+                                        const colorA = geometryParameters.attributes.color;
+                                        const flag = renderingAttributes.getFlag(programSetup.renderPassFlag);
+                                        const renderPass = programVariables.createUniform("int", "renderPass", (set, state) => set(state.renderPass));
+                                        const flagTest = (isShadowProgram
+                                                          ? `(${flag} <= 0) || (${colorA}.a < 1.0)`
+                                                          : `${flag} != ${renderPass}`);
+
+                                        const afterFlagsColorLines = [
+                                            `if (${flagTest}) {`,
+                                            `   gl_Position = vec4(${programSetup.vertexCullX || 0.0}, 0.0, 0.0, 0.0);`, // Cull vertex
+                                            "   return;",
+                                            "}"
+                                        ].concat(
+                                            (!renderingAttributes.isVBO) && (!programSetup.dontCullOnAlphaZero)
+                                                ? [
+                                                    `if (${colorA}.a == 0.0) {`,
+                                                    "   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);", // Cull vertex
+                                                    "   return;",
+                                                    "}"
+                                                ]
+                                                : [ ]
+                                        ).concat(
+                                            (programSetup.filterIntensityRange && (primitive === "points") && pointsMaterial.filterIntensity)
+                                                ? (function () {
+                                                    const intensityRange  = programVariables.createUniform("vec2", "intensityRange", (set) => {
+                                                        tempVec2[0] = pointsMaterial.minIntensity;
+                                                        tempVec2[1] = pointsMaterial.maxIntensity;
+                                                        set(tempVec2);
+                                                    });
+                                                    return [
+                                                        `if ((${colorA}.a < ${intensityRange}[0]) || (${colorA}.a > ${intensityRange}[1])) {`,
+                                                        "   gl_Position = vec4(0.0, 0.0, 0.0, 0.0);", // Cull vertex
+                                                        "   return;",
+                                                        "}"
+                                                    ];
+                                                })()
+                                            : [ ]);
+
+                                        const vertexData = [ ];
+                                        renderingAttributes.ensureColorAndFlagAvailable && renderingAttributes.ensureColorAndFlagAvailable(vertexData);
+                                        afterFlagsColorLines.forEach(line => vertexData.push(line));
+                                        renderingAttributes.appendVertexData(vertexData);
+                                        vertexData.push(`vec4 viewPosition = ${geometryParameters.viewMatrix} * ${worldPositionAttribute};`);
+                                        return vertexData;
+                                    },
+                                    projMatrix:                     geometryParameters.projMatrix,
+                                    scene:                          scene,
+                                    testPerspectiveForGl_FragDepth: ((primitive !== "points") && (primitive !== "lines")) || subGeometry,
+                                    transformClipPos:               programSetup.transformClipPos,
+                                    worldPositionAttribute:         worldPositionAttribute
+                                });
+
+                            if (errors) {
+                                console.error(errors);
+                                return {
+                                    getValid:  getValid,
+                                    destroy:   () => { },
+                                    drawLayer: (frameCtx, layer, renderPass) => { }
+                                };
+                            } else {
+                                return {
+                                    getValid:  getValid,
+                                    destroy:   () => program.destroy(),
+                                    drawLayer: (frameCtx, layer, renderPass) => {
+                                        if (frameCtx.lastProgramId !== program.id) {
+                                            frameCtx.lastProgramId = program.id;
+                                            program.bind();
+                                        }
+
+                                        const origin = layer.origin;
+                                        const model = layer.model;
+                                        const camera = scene.camera;
+                                        const rtcOrigin = tempVec3;
+                                        math.transformPoint3(model.matrix, origin, rtcOrigin);
+
+                                        const viewMatrix = (isShadowProgram && frameCtx.shadowViewMatrix) || (usePickParams && frameCtx.pickViewMatrix) || camera.viewMatrix;
+                                        const projMatrix = (isShadowProgram && frameCtx.shadowProjMatrix) || (usePickParams && frameCtx.pickProjMatrix) || camera.projMatrix;
+
+                                        if (frameCtx.snapPickOrigin) {
+                                            frameCtx.snapPickOrigin[0] = rtcOrigin[0];
+                                            frameCtx.snapPickOrigin[1] = rtcOrigin[1];
+                                            frameCtx.snapPickOrigin[2] = rtcOrigin[2];
+                                        }
+
+                                        const state = {
+                                            legacyFrameCtx: frameCtx,
+                                            layerDrawState: layer.layerDrawState,
+                                            mesh: {
+                                                layerIndex:  layer.layerIndex,
+                                                origin:      rtcOrigin,
+                                                renderFlags: { sectionPlanesActivePerLayer: model.renderFlags.sectionPlanesActivePerLayer },
+                                                worldMatrix: model.rotationMatrix
+                                            },
+                                            renderPass:     renderPass,
+                                            view: {
+                                                eye:              (usePickParams && frameCtx.pickOrigin) || camera.eye,
+                                                far:              (usePickParams && frameCtx.pickProjMatrix) ? frameCtx.pickZFar : camera.project.far,
+                                                pickClipPos:      frameCtx.pickClipPos,
+                                                projMatrix:       projMatrix,
+                                                viewMatrix:       math.compareVec3(rtcOrigin, vec3zero) ? viewMatrix : createRTCViewMat(viewMatrix, rtcOrigin, tempMat4),
+                                                viewNormalMatrix: camera.viewNormalMatrix
+                                            }
+                                        };
+
+                                        layer.drawCalls[subGeometry ? (subGeometry.vertices ? "drawVertices" : "drawEdges") : "drawSurface"](program.inputSetters, state);
+
+                                        if (incrementDrawState) {
+                                            frameCtx.drawElements++;
+                                        }
+                                    }
+                                };
+                            }
                         });
                 };
                 let renderer = isEager && instantiate();
@@ -202,8 +358,6 @@ export const getRenderers = (function() {
             // such as picking or emphasis, so that there is no delay when user first begins interacting with the viewer.
             const eager = (createProgramSetup, subGeometry) => wrapRenderer(createProgramSetup, subGeometry, true);
             const lazy  = (createProgramSetup, subGeometry) => wrapRenderer(createProgramSetup, subGeometry, false);
-
-            const gl = scene.canvas.gl;
 
             const makeColorProgram = (vars, geo, lights, sao) => ColorProgram(vars, geo, scene.logarithmicDepthBufferEnabled, lights, sao, primitive);
 
