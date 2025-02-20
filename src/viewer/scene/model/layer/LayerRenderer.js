60421efb1cb2b04514213b2e2cd3d47c3baebffe
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-02-20 18:31:08 +0100

    Inline LayerRenderer's buildProgram parameters

--- src/viewer/scene/model/layer/LayerRenderer.js
+++ src/viewer/scene/model/layer/LayerRenderer.js
@@ -18,116 +18,103 @@ export const lazyShaderVariable = function(name) {
 export class LayerRenderer {
 
     constructor(scene, primitive, cfg, subGeometry, renderingAttributes, programVariablesState) {
+        const pointsMaterial     = scene.pointsMaterial;
+        const setupPoints        = (primitive === "points") && (! subGeometry);
 
-        const isVBO = renderingAttributes.isVBO;
-        const pointsMaterial = scene.pointsMaterial;
-
-        const incrementDrawState        = cfg.incrementDrawState;
-        const renderPassFlag            = cfg.renderPassFlag;
-        const usePickParams             = cfg.usePickParams;
-        const cullOnAlphaZero           = (!isVBO) && (!cfg.dontCullOnAlphaZero);
-        const filterIntensityRange      = cfg.filterIntensityRange && (primitive === "points") && pointsMaterial.filterIntensity;
-        const isShadowProgram           = cfg.isShadowProgram;
-        const vertexCullX               = cfg.vertexCullX;
-        const incPointSizeBy10          = cfg.incPointSizeBy10;
-
-        const setupPoints = (primitive === "points") && (! subGeometry);
-
-        const sectionPlanesState = scene._sectionPlanesState;
-
-        const getHash = () => [ sectionPlanesState.getHash() ].concat(setupPoints ? [ pointsMaterial.hash ] : [ ]).concat(cfg.getHash ? cfg.getHash() : [ ]).join(";");
+        const getHash = () => [ scene._sectionPlanesState.getHash() ].concat(setupPoints ? [ pointsMaterial.hash ] : [ ]).concat(cfg.getHash ? cfg.getHash() : [ ]).join(";");
         const hash = getHash();
         this.getValid = () => hash === getHash();
 
-        const programVariables = programVariablesState.programVariables;
-        const gl = scene.canvas.gl;
-
-        const intensityRange  = programVariables.createUniform("vec2", "intensityRange", (set) => {
-            tempVec2[0] = pointsMaterial.minIntensity;
-            tempVec2[1] = pointsMaterial.maxIntensity;
-            set(tempVec2);
-        });
-        const nearPlaneHeight = programVariables.createUniform("float", "nearPlaneHeight", (set) => {
-            set((scene.camera.projection === "ortho") ?
-                1.0
-                : (gl.drawingBufferHeight / (2 * Math.tan(0.5 * scene.camera.perspective.fov * Math.PI / 180.0))));
-        });
-        const pointSize       = programVariables.createUniform("float", "pointSize",      (set) => set(pointsMaterial.pointSize));
-        const renderPass      = programVariables.createUniform("int",   "renderPass",     (set, state) => set(state.renderPass));
-
-        const geoParams = renderingAttributes.geometryParameters;
-        const worldPositionAttribute = geoParams.attributes.position.world;
-
-        const getVertexData = () => {
-            const colorA = geoParams.attributes.color;
-            const flagTest = (isShadowProgram
-                              ? `(${renderingAttributes.getFlag(renderPassFlag)} <= 0) || (${colorA}.a < 1.0)`
-                              : `${renderingAttributes.getFlag(renderPassFlag)} != ${renderPass}`);
-
-            const afterFlagsColorLines = [
-                `if (${flagTest}) {`,
-                `   gl_Position = vec4(${vertexCullX || 0.0}, 0.0, 0.0, 0.0);`, // Cull vertex
-                "   return;",
-                "}"
-            ].concat(
-                cullOnAlphaZero
-                    ? [
-                        `if (${colorA}.a == 0.0) {`,
-                        "   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);", // Cull vertex
-                        "   return;",
-                        "}"
-                    ]
-                    : [ ]
-            ).concat(
-                filterIntensityRange
-                    ? [
-                        `if ((${colorA}.a < ${intensityRange}[0]) || (${colorA}.a > ${intensityRange}[1])) {`,
-                        "   gl_Position = vec4(0.0, 0.0, 0.0, 0.0);", // Cull vertex
-                        "   return;",
-                        "}"
-                    ]
-                    : [ ]);
-
-            const vertexData = [ ];
-            renderingAttributes.ensureColorAndFlagAvailable && renderingAttributes.ensureColorAndFlagAvailable(vertexData);
-            afterFlagsColorLines.forEach(line => vertexData.push(line));
-            renderingAttributes.appendVertexData(vertexData);
-            vertexData.push(`vec4 viewPosition = ${geoParams.viewMatrix} * ${worldPositionAttribute};`);
-            return vertexData;
-        };
-
-        const getPointSize = (function() {
-            const addends = [ ];
-            if (setupPoints) {
-                if (pointsMaterial.perspectivePoints) {
-                    const minVal = `${Math.floor(pointsMaterial.minPerspectivePointSize)}.0`;
-                    const maxVal = `${Math.floor(pointsMaterial.maxPerspectivePointSize)}.0`;
-                    addends.push(`clamp((${nearPlaneHeight} * ${pointSize}) / gl_Position.w, ${minVal}, ${maxVal})`);
-                } else {
-                    addends.push(pointSize);
-                }
-            } else if (subGeometry && subGeometry.vertices) {
-                addends.push("1.0");
-            }
-            incPointSizeBy10 && (primitive === "points") && addends.push("10.0");
-            return (addends.length > 0) && (() => addends.join(" + "));
-        })();
-
-        const vClippable = programVariables.createVarying("float", "vClippable", () => renderingAttributes.getClippable(), "flat");
+        const geometryParameters     = renderingAttributes.geometryParameters;
+        const gl                     = scene.canvas.gl;
+        const incrementDrawState     = cfg.incrementDrawState;
+        const isShadowProgram        = cfg.isShadowProgram;
+        const programVariables       = programVariablesState.programVariables;
+        const usePickParams          = cfg.usePickParams;
+        const worldPositionAttribute = geometryParameters.attributes.position.world;
 
         const [ program, errors ] = programVariablesState.buildProgram(
             gl,
             primitive + " " + renderingAttributes.signature + " " + cfg.programName,
             {
                 appendFragmentOutputs:          cfg.appendFragmentOutputs,
-                clippableTest:                  () => `${vClippable} > 0.0`,
+                clippableTest:                  (function() {
+                    const vClippable = programVariables.createVarying("float", "vClippable", () => renderingAttributes.getClippable(), "flat");
+                    return () => `${vClippable} > 0.0`;
+                })(),
                 clippingCaps:                   cfg.clippingCaps,
                 discardPoints:                  setupPoints && pointsMaterial.roundPoints,
                 fragmentOutputsSetup:           [ ],
                 getLogDepth:                    cfg.getLogDepth,
-                getPointSize:                   getPointSize,
-                getVertexData:                  getVertexData,
-                projMatrix:                     geoParams.projMatrix,
+                getPointSize:                   (function() {
+                    const addends = [ ];
+                    if (setupPoints) {
+                        const pointSize = programVariables.createUniform("float", "pointSize", (set) => set(pointsMaterial.pointSize));
+                        if (pointsMaterial.perspectivePoints) {
+                            const nearPlaneHeight = programVariables.createUniform("float", "nearPlaneHeight", (set) => {
+                                set((scene.camera.projection === "ortho") ?
+                                    1.0
+                                    : (gl.drawingBufferHeight / (2 * Math.tan(0.5 * scene.camera.perspective.fov * Math.PI / 180.0))));
+                            });
+                            const minVal = `${Math.floor(pointsMaterial.minPerspectivePointSize)}.0`;
+                            const maxVal = `${Math.floor(pointsMaterial.maxPerspectivePointSize)}.0`;
+                            addends.push(`clamp((${nearPlaneHeight} * ${pointSize}) / gl_Position.w, ${minVal}, ${maxVal})`);
+                        } else {
+                            addends.push(pointSize);
+                        }
+                    } else if (subGeometry && subGeometry.vertices) {
+                        addends.push("1.0");
+                    }
+                    cfg.incPointSizeBy10 && (primitive === "points") && addends.push("10.0");
+                    return (addends.length > 0) && (() => addends.join(" + "));
+                })(),
+                getVertexData:                  () => {
+                    const colorA = geometryParameters.attributes.color;
+                    const flag = renderingAttributes.getFlag(cfg.renderPassFlag);
+                    const renderPass = programVariables.createUniform("int", "renderPass", (set, state) => set(state.renderPass));
+                    const flagTest = (isShadowProgram
+                                      ? `(${flag} <= 0) || (${colorA}.a < 1.0)`
+                                      : `${flag} != ${renderPass}`);
+
+                    const afterFlagsColorLines = [
+                        `if (${flagTest}) {`,
+                        `   gl_Position = vec4(${cfg.vertexCullX || 0.0}, 0.0, 0.0, 0.0);`, // Cull vertex
+                        "   return;",
+                        "}"
+                    ].concat(
+                        (!renderingAttributes.isVBO) && (!cfg.dontCullOnAlphaZero)
+                            ? [
+                                `if (${colorA}.a == 0.0) {`,
+                                "   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);", // Cull vertex
+                                "   return;",
+                                "}"
+                            ]
+                            : [ ]
+                    ).concat(
+                        (cfg.filterIntensityRange && (primitive === "points") && pointsMaterial.filterIntensity)
+                            ? (function () {
+                                const intensityRange  = programVariables.createUniform("vec2", "intensityRange", (set) => {
+                                    tempVec2[0] = pointsMaterial.minIntensity;
+                                    tempVec2[1] = pointsMaterial.maxIntensity;
+                                    set(tempVec2);
+                                });
+                                return [
+                                    `if ((${colorA}.a < ${intensityRange}[0]) || (${colorA}.a > ${intensityRange}[1])) {`,
+                                    "   gl_Position = vec4(0.0, 0.0, 0.0, 0.0);", // Cull vertex
+                                    "   return;",
+                                    "}"
+                                ];
+                            })()
+                        : [ ]);
+
+                    const vertexData = [ ];
+                    renderingAttributes.ensureColorAndFlagAvailable && renderingAttributes.ensureColorAndFlagAvailable(vertexData);
+                    afterFlagsColorLines.forEach(line => vertexData.push(line));
+                    renderingAttributes.appendVertexData(vertexData);
+                    vertexData.push(`vec4 viewPosition = ${geometryParameters.viewMatrix} * ${worldPositionAttribute};`);
+                    return vertexData;
+                },
+                projMatrix:                     geometryParameters.projMatrix,
                 scene:                          scene,
                 testPerspectiveForGl_FragDepth: ((primitive !== "points") && (primitive !== "lines")) || subGeometry,
                 transformClipPos:               cfg.transformClipPos,
