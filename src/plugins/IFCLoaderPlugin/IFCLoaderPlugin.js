e1d4192683669d39b67434b4c0d5c71d6f6ee032
jakub-lew <jakub.lewandowski@poczta.fm>
2025-02-24 13:47:55 +0100

    feature: ifc-loader plugin which uses cx-converter (wasm version)

---
+++ src/plugins/IFCLoaderPlugin/IFCLoaderPlugin.js
@@ -0,0 +1,38 @@
+import { Plugin } from "../../viewer/Plugin.js";
+import { GLTFLoaderPlugin } from "../GLTFLoaderPlugin/GLTFLoaderPlugin.js";
+import { ifc2gltf } from "@creooxag/cx-converter";
+
+async function fetchFile(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.text();
+    } catch (error) {
+        console.error('Error fetching file:', error);
+    }
+}
+
+class IFCLoaderPlugin extends Plugin {
+    constructor(viewer, cfg = {}) {
+        super("ifcLoader", viewer, cfg);
+    }
+    async load(params = {}) {
+        if (!params.src) {
+            this.error("load() param expected: src");
+        }
+        const data = await fetchFile(params.src);
+        const { gltf, metaData } = await ifc2gltf(data, "remote", params.progressCallback, params.progressTextCallback);
+        const gltfLoader = new GLTFLoaderPlugin(this.viewer);
+        const model = await gltfLoader.load({
+            id: "myModel",
+            gltf: gltf,
+            metaModelJSON: metaData            
+        });
+        model.on("loaded", () => {
+            this.viewer.cameraFlight.flyTo();            
+        });
+    }
+}
+export { IFCLoaderPlugin };
