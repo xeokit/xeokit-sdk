5ef640f1678dcae59dd39d889c21b1c87e81bead
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-05-14 18:09:41 +0200

    Make createPickClipTransformSetup use consistent for Pick*Program and SnapProgram

--- src/viewer/scene/model/layer/programs/PickMeshProgram.js
+++ src/viewer/scene/model/layer/programs/PickMeshProgram.js
@@ -6,7 +6,7 @@ export const PickMeshProgram = function(programVariables, geometry, logarithmicD
         getLogDepth: logarithmicDepthBufferEnabled && (vFragDepth => vFragDepth),
         renderPassFlag: 3,  // PICK
         incPointSizeBy10: true,
-        transformClipPos: clipTransformSetup.transformClipPos,
+        clipTransformSetup: clipTransformSetup,
         dontCullOnAlphaZero: true, // should be false?
         appendFragmentOutputs: (src) => src.push(`${outPickColor} = ${vPickColor};`)
     };
