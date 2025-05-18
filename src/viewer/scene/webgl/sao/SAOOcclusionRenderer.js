6cbc8130913d5c58ddac1d7c8d0e6d9b79c87aeb
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-05-18 11:52:11 +0200

    Simplify SAOOcclusionRenderer glsl

--- src/viewer/scene/webgl/sao/SAOOcclusionRenderer.js
+++ src/viewer/scene/webgl/sao/SAOOcclusionRenderer.js
@@ -124,138 +124,69 @@ export class SAOOcclusionRenderer {
             "getOutColor",
             (name, src) => {
                 src.push(`
-                #define NORMAL_TEXTURE 0
                 #define PI 3.14159265359
                 #define PI2 6.28318530718
                 #define EPSILON 1e-6
                 #define NUM_SAMPLES ${this._numSamples}
-                #define NUM_RINGS 4              
+                #define NUM_RINGS 4
 
-                float pow2( const in float x ) { return x*x; }
-                
-                highp float rand( const in vec2 uv ) {
+                highp float rand(const in vec2 uv) {
                     const highp float a = 12.9898, b = 78.233, c = 43758.5453;
-                    highp float dt = dot( uv.xy, vec2( a,b ) ), sn = mod( dt, PI );
-                    return fract(sin(sn) * c);
+                    return fract(sin(mod(dot(uv, vec2(a, b)), PI)) * c);
                 }
 
-                vec3 packNormalToRGB( const in vec3 normal ) {
-                    return normalize( normal ) * 0.5 + 0.5;
-                }
-
-                vec3 unpackRGBToNormal( const in vec3 rgb ) {
-                    return 2.0 * rgb.xyz - 1.0;
-                }
-
-                const float packUpscale = 256. / 255.;
-                const float unpackDownScale = 255. / 256.; 
-
-                const vec3 packFactors = vec3( 256. * 256. * 256., 256. * 256.,  256. );
-                const vec4 unPackFactors = unpackDownScale / vec4( packFactors, 1. );   
-
-                const float shiftRights = 1. / 256.;
-
-                vec4 packFloatToRGBA( const in float v ) {
-                    vec4 r = vec4( fract( v * packFactors ), v );
-                    r.yzw -= r.xyz * shiftRights; 
-                    return r * packUpscale;
-                }
-
-                float unpackRGBAToFloat( const in vec4 v ) {                   
-                    return dot( floor( v * 255.0 + 0.5 ) / 255.0, unPackFactors );
-                }
-                
-                float perspectiveDepthToViewZ( const in float invClipZ, const in float near, const in float far ) {
-                    return ( near * far ) / ( ( far - near ) * invClipZ - far );
-                }
-
-                float orthographicDepthToViewZ( const in float linearClipZ, const in float near, const in float far ) {
-                    return linearClipZ * ( near - far ) - near;
-                }
-                
-                float getDepth( const in vec2 screenPosition ) {
-                    return vec4(texture(${uDepthTexture}, screenPosition)).r;
-                }
-
-                float getViewZ( const in float depth ) {
-                     if (${uPerspective}) {
-                         return perspectiveDepthToViewZ( depth, ${uCameraNear}, ${uCameraFar} );
-                     } else {
-                        return orthographicDepthToViewZ( depth, ${uCameraNear}, ${uCameraFar} );
-                     }
-                }
-
-                vec3 getViewPos( const in vec2 screenPos, const in float depth, const in float viewZ ) {
-                       float clipW = ${uProjectMatrix}[2][3] * viewZ + ${uProjectMatrix}[3][3];
-                	vec4 clipPosition = vec4( ( vec3( screenPos, depth ) - 0.5 ) * 2.0, 1.0 );
-                	clipPosition *= clipW; 
-                       return ( ${uInverseProjectMatrix} * clipPosition ).xyz;
-                }
-
-                vec3 getViewNormal( const in vec3 viewPosition, const in vec2 screenPos ) {               
-                    return normalize( cross( dFdx( viewPosition ), dFdy( viewPosition ) ) );
-                }
-
-                float scaleDividedByCameraFar;
-                float minResolutionMultipliedByCameraFar;
-
-                float getOcclusion( const in vec3 centerViewPosition, const in vec3 centerViewNormal, const in vec3 sampleViewPosition ) {
-                	vec3 viewDelta = sampleViewPosition - centerViewPosition;
-                	float viewDistance = length( viewDelta );
-                	float scaledScreenDistance = scaleDividedByCameraFar * viewDistance;
-                       return max(0.0, (dot(centerViewNormal, viewDelta) - minResolutionMultipliedByCameraFar) / scaledScreenDistance - ${uBias}) / (1.0 + pow2( scaledScreenDistance ) );
-                }
-
-                const float ANGLE_STEP = PI2 * float( NUM_RINGS ) / float( NUM_SAMPLES );
-                const float INV_NUM_SAMPLES = 1.0 / float( NUM_SAMPLES );
-
-                float getAmbientOcclusion( const in vec3 centerViewPosition ) {
-            
-                       scaleDividedByCameraFar = ${uScale} / ${uCameraFar};
-                       minResolutionMultipliedByCameraFar = ${uMinResolution} * ${uCameraFar};
-                       vec3 centerViewNormal = getViewNormal( centerViewPosition, ${vUV} );
-
-                       float angle = rand( ${vUV} + ${uRandomSeed} ) * PI2;
-                       vec2 radius = vec2( ${uKernelRadius} * INV_NUM_SAMPLES ) / ${uViewport};
-                	vec2 radiusStep = radius;
-
-                	float occlusionSum = 0.0;
-                	float weightSum = 0.0;
-
-                	for( int i = 0; i < NUM_SAMPLES; i ++ ) {
-                               vec2 sampleUv = ${vUV} + vec2( cos( angle ), sin( angle ) ) * radius;
-                		radius += radiusStep;
-                		angle += ANGLE_STEP;
-
-                		float sampleDepth = getDepth( sampleUv );
-                		if( sampleDepth >= ( 1.0 - EPSILON ) ) {
-                			continue;
-                		}
-
-                		float sampleViewZ = getViewZ( sampleDepth );
-                		vec3 sampleViewPosition = getViewPos( sampleUv, sampleDepth, sampleViewZ );
-                		occlusionSum += getOcclusion( centerViewPosition, centerViewNormal, sampleViewPosition );
-                		weightSum += 1.0;
-                	}
-
-                	if( weightSum == 0.0 ) discard;
-
-                       return occlusionSum * ( ${uIntensity} / weightSum );
+                vec3 getViewPos(const in vec2 screenPos, const in float depth) {
+                        float near = ${uCameraNear};
+                        float far  = ${uCameraFar};
+                        float viewZ = (${uPerspective}
+                                       ? ((near * far) / ((far - near) * depth - far))
+                                       : (depth * (near - far) - near));
+                        float clipW = ${uProjectMatrix}[2][3] * viewZ + ${uProjectMatrix}[3][3];
+                        return (${uInverseProjectMatrix} * (clipW * vec4((vec3(screenPos, depth) - 0.5) * 2.0, 1.0))).xyz;
                 }
 
                 vec4 ${name}() {
-                        float centerDepth = getDepth( ${vUV} );
+                        float centerDepth = texture(${uDepthTexture}, ${vUV}).r;
 
-                	if( centerDepth >= ( 1.0 - EPSILON ) ) {
-                		discard;
-                	}
+                        if (centerDepth >= (1.0 - EPSILON)) {
+                                discard;
+                        }
 
-                	float centerViewZ = getViewZ( centerDepth );
-                        vec3 viewPosition = getViewPos( ${vUV}, centerDepth, centerViewZ );
+                        vec3 centerViewPosition = getViewPos(${vUV}, centerDepth);
+                        float scaleDividedByCameraFar = ${uScale} / ${uCameraFar};
+                        float minResolutionMultipliedByCameraFar = ${uMinResolution} * ${uCameraFar};
+                        vec3 centerViewNormal = normalize(cross(dFdx(centerViewPosition), dFdy(centerViewPosition)));
 
-                	float ambientOcclusion = getAmbientOcclusion( viewPosition );
-                
-                        return packFloatToRGBA(  1.0- ambientOcclusion );
+                        vec2 radiusStep = ${uKernelRadius} / ${uViewport} / float(NUM_SAMPLES);
+                        vec2 radius = radiusStep;
+                        const float angleStep = PI2 * float(NUM_RINGS) / float(NUM_SAMPLES);
+                        float angle = PI2 * rand(${vUV} + ${uRandomSeed});
+
+                        float occlusionSum = 0.0;
+                        float weightSum = 0.0;
+
+                        for (int i = 0; i < NUM_SAMPLES; i++) {
+                                vec2 sampleUv = ${vUV} + vec2(cos(angle), sin(angle)) * radius;
+                                radius += radiusStep;
+                                angle += angleStep;
+
+                                float sampleDepth = texture(${uDepthTexture}, sampleUv).r;
+                                if (sampleDepth >= (1.0 - EPSILON)) {
+                                        continue;
+                                }
+
+                                vec3 sampleViewPosition = getViewPos(sampleUv, sampleDepth);
+                                vec3 viewDelta = sampleViewPosition - centerViewPosition;
+                                float scaledScreenDistance = scaleDividedByCameraFar * length(viewDelta);
+                                occlusionSum += max(0.0, (dot(centerViewNormal, viewDelta) - minResolutionMultipliedByCameraFar) / scaledScreenDistance - ${uBias}) / (1.0 + scaledScreenDistance * scaledScreenDistance );
+                                weightSum += 1.0;
+                        }
+
+                        float v = 1.0 - occlusionSum * ${uIntensity} / weightSum;
+                        // packFloatToRGBA
+                        vec4 r = vec4(fract(v * vec3(256. * 256. * 256., 256. * 256., 256.)), v);
+                        r.yzw -= r.xyz / 256.;
+                        return r * 256. / 255.;
                 }`);
             });
 
