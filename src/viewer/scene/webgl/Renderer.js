8e48c79b4870bd85b4c89610c1c436bbcc0bc814
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-05-21 17:12:07 +0200

    Replace RenderBuffer::clear calls with gl.clear

--- src/viewer/scene/webgl/Renderer.js
+++ src/viewer/scene/webgl/Renderer.js
@@ -428,7 +428,6 @@ const Renderer = function (scene, options) {
         const occlusionRenderBuffer1 = renderBufferManager.getRenderBuffer("saoOcclusion");
         occlusionRenderBuffer1.setSize(size);
         occlusionRenderBuffer1.bind();
-        occlusionRenderBuffer1.clear();
 
         gl.viewport(0, 0, size[0], size[1]);
         gl.clearColor(0, 0, 0, 1);
@@ -447,7 +446,6 @@ const Renderer = function (scene, options) {
 
             const blurSAO = (src, dst, direction) => {
                 dst.bind();
-                dst.clear();
 
                 gl.viewport(0, 0, size[0], size[1]);
                 gl.clearColor(0, 0, 0, 1);
@@ -1492,7 +1490,6 @@ const Renderer = function (scene, options) {
             if (readPixelBuf) {
                 readPixelBuf.setSize([gl.drawingBufferWidth, gl.drawingBufferHeight]);
                 readPixelBuf.bind();
-                readPixelBuf.clear();
             }
 
             frameCtx.reset();
@@ -1541,7 +1538,7 @@ const Renderer = function (scene, options) {
     this.readPixels = function (pixels, colors, len, opaqueOnly) {
         const snapshotBuffer = renderBufferManager.getRenderBuffer("snapshot");
         snapshotBuffer.bind();
-        snapshotBuffer.clear();
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         this.render({force: true, opaqueOnly: opaqueOnly});
         let color;
         let i;
@@ -1573,7 +1570,7 @@ const Renderer = function (scene, options) {
                                ? [params.width, params.height]
                                : [gl.drawingBufferWidth, gl.drawingBufferHeight]);
         snapshotBuffer.bind();
-        snapshotBuffer.clear();
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         snapshotBound = true;
     };
 
@@ -1585,7 +1582,7 @@ const Renderer = function (scene, options) {
             return;
         }
         const snapshotBuffer = renderBufferManager.getRenderBuffer("snapshot");
-        snapshotBuffer.clear();
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         this.render({force: true, opaqueOnly: false});
         imageDirty = true;
     };
