3666e74fe1a99c69ea09ef9eb81f7f9a60e07e31
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-05-27 17:38:51 +0200

    Remove unused FrameContext::this.pickElements{Count,Offset}

--- src/viewer/scene/webgl/Renderer.js
+++ src/viewer/scene/webgl/Renderer.js
@@ -1091,8 +1091,6 @@ const Renderer = function (scene, options) {
                         resetFrameCtx(1);
                         frameCtx.viewParams.near = nearAndFar[0];
                         frameCtx.viewParams.far  = nearAndFar[1];
-                        frameCtx.pickElementsCount = pickable.pickElementsCount;
-                        frameCtx.pickElementsOffset = pickable.pickElementsOffset;
 
                         gl.viewport(0, 0, 1, 1);
 
