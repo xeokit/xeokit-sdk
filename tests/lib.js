d789302ae566322e89d841a4a39eccc2483eaa79
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-06-16 19:41:56 +0200

    Export testPage from tests/lib.js (so it's easy to replace for e.g. a local run)

---
+++ tests/lib.js
@@ -0,0 +1,26 @@
+import percySnapshot from "@percy/playwright";
+import { test, expect } from "@playwright/test";
+import "dotenv/config";
+
+async function testPage(pageName, callWithPage) {
+    return test(`should check if scene loaded for ${pageName}`, async ({ page }) => {
+        const loadedContent = page.locator("#percyLoaded");
+        await page.goto(`http://localhost:8080/test-scenes/${pageName}.html`);
+
+        await expect(loadedContent).toBeAttached();
+
+        if (callWithPage) {
+            await callWithPage(page);
+        }
+
+        await percySnapshot(page, pageName, { widths: [1280] });
+    });
+}
+
+async function wait() {
+    return new Promise((resolve) => setTimeout(() => resolve("Waited 3 seconds"), 3000));
+}
+
+test.beforeAll("wait", async () => await wait());
+
+export { test, testPage };
