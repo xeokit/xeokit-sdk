6fc29ae8f32f82aa497f750cbe9dad6be88223da
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-06-17 17:24:50 +0200

    Revert PR "Cleaner edges with non-logarithmic depth buffer" #1880 - TO BE reapplied later
    Revert "Cleaner edges with non-logarithmic depth buffer for DTX"
    This reverts commit 3f99add6524803cfc501a400801e216f937d4f13.
    
    Revert "Cleaner edges with non-logarithmic depth buffer"
    This reverts commit 050a433496b670179c6d6c09abcc278517ca39e1.

--- src/viewer/scene/model/vbo/batching/triangles/renderers/TrianglesFlatColorRenderer.js
+++ src/viewer/scene/model/vbo/batching/triangles/renderers/TrianglesFlatColorRenderer.js
@@ -233,11 +233,6 @@ export class TrianglesFlatColorRenderer extends TrianglesBatchingRenderer {
 
         if (scene.logarithmicDepthBufferEnabled) {
             src.push("    gl_FragDepth = isPerspective == 0.0 ? gl_FragCoord.z : log2( vFragDepth ) * logDepthBufFC * 0.5;");
-        } else {
-            src.push("    float dx = dFdx(gl_FragCoord.z);")
-            src.push("    float dy = dFdy(gl_FragCoord.z);")
-            src.push("    float diff = sqrt(dx*dx+dy*dy);");
-            src.push("    gl_FragDepth = gl_FragCoord.z + diff;");
         }
 
         src.push("}");
