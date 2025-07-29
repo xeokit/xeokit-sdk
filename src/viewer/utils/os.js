f188ab3002a1c14c620d80600eef502bf01ed63f
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-07-29 16:52:59 +0200

    XCD-332 Change addContextMenuListener to listen to touch events on iPads

--- src/viewer/utils/os.js
+++ src/viewer/utils/os.js
@@ -6,6 +6,15 @@ const os = {
 
         return isIphone && isSafari;
     },
+    isIphoneOrIpadSafari() {
+        const userAgent = window.navigator.userAgent;
+        const isIphoneOrIpad = /iPhone|iPad/i.test(userAgent) || (
+            // iPad detection: Check for touch support and macOS-like user agent
+            /Macintosh/i.test(userAgent) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2
+        );
+        const isSafari = /Safari/i.test(userAgent) && !/Chrome/i.test(userAgent);
+        return isIphoneOrIpad && isSafari;
+    },
     isTouchDevice() {
         return (
             'ontouchstart' in window || //works for most devices
