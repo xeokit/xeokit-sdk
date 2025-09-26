e3985bd48f74a3edab440e5ac4244a7a1fe4c237
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2025-09-26 17:14:22 +0200

    Add snap_pick_ViewCullPlugin test

--- tests/snapshots-snap-picking.spec.js
+++ tests/snapshots-snap-picking.spec.js
@@ -1,4 +1,4 @@
-import { test, testPage } from "./lib.js";
+import { expect, test, setupPage, testPage } from "./lib.js";
 
 test.describe("Add visual effect to picked entity on mouseover", async () => {
     testPage("snapToVertex_vbo_batching_triangles", async (page) => {
@@ -8,4 +8,14 @@ test.describe("Add visual effect to picked entity on mouseover", async () => {
         await page.mouse.move(...mousePosition);
         await page.waitForTimeout(500);
     });
+
+    const pageName = "snap_pick_ViewCullPlugin";
+    setupPage(
+        `getGeometryData_${pageName}`,
+        `${pageName}.html`,
+        async page => {
+            const data = await page.evaluate(() => JSON.stringify([...window.xeokitViewer.scene.pick({canvasPos: [300, 300], pickSurface: true, snapToVertex: true}).worldPos], null, 4));
+            // if vertexCullX is not "2.0" for (!isSnapInit) then fails on webkit/safari
+            expect(data).toMatchSnapshot();
+        });
 });
