50d8b67671d163b9d3520c5dfa15b769ef156511
Michal Dybizbanski <michal.dybizbanski@creoox.com>
2026-03-24 18:21:22 +0100

    Export addMousePressListener and addTouchPressListener ts declarations

--- types/plugins/lib/ui/index.d.ts
+++ types/plugins/lib/ui/index.d.ts
@@ -1,5 +1,6 @@
 import { Viewer } from "../../../viewer";
 import { PointerCircle } from "../../../extras/PointerCircle";
+import { CameraControl } from "../../../viewer/scene/CameraControl/CameraControl";
 
 type Vec2 = number[]
 type Vec3 = number[]
@@ -13,3 +14,7 @@ type OnCommit<T> = (canvasPos: Vec2, worldPos: T | null) => void;
 
 
 declare function touchPointSelector<T>(viewer: Viewer, pointerCircle: PointerCircle, ray2WorldPos: Ray2WorldPos<T>): (onCancel: OnCancel, onChange: OnChange<T>, onCommit: OnCommit<T>) => Cleanup;
+
+export declare addMousePressListener(element: HTMLElement, onChange: (canvasPos: Vec2 | null) => void);
+
+export declare addTouchPressListener(element: HTMLElement, cameraControl: CameraControl, pointerCircle: PointerCircle, onChange: (canvasPos: Vec2 | null) => void);
