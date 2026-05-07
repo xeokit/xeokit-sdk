e9acf91ecd220ca007797d3221e4f9f267c8c2e2
Michiel Lankamp <michiel.lankamp@madaster.com>
2026-05-07 12:36:15 +0200

    fix: correct type declarations for Vec2 and Vec3, and update function exports

--- types/plugins/lib/ui/index.d.ts
+++ types/plugins/lib/ui/index.d.ts
@@ -2,8 +2,8 @@ import { Viewer } from "../../../viewer";
 import { PointerCircle } from "../../../extras/PointerCircle";
 import { CameraControl } from "../../../viewer/scene/CameraControl/CameraControl";
 
-type Vec2 = number[]
-type Vec3 = number[]
+type Vec2 = number[];
+type Vec3 = number[];
 type Ray2WorldPos<T> = (origin: Vec3, direction: Vec3) => T | null;
 
 type Cleanup = () => void;
@@ -12,9 +12,8 @@ type OnCancel = () => void;
 type OnChange<T> = (canvasPos: Vec2, worldPos: T | null) => void;
 type OnCommit<T> = (canvasPos: Vec2, worldPos: T | null) => void;
 
+export declare function touchPointSelector<T>(viewer: Viewer, pointerCircle: PointerCircle, ray2WorldPos: Ray2WorldPos<T>): (onCancel: OnCancel, onChange: OnChange<T>, onCommit: OnCommit<T>) => Cleanup;
 
-declare function touchPointSelector<T>(viewer: Viewer, pointerCircle: PointerCircle, ray2WorldPos: Ray2WorldPos<T>): (onCancel: OnCancel, onChange: OnChange<T>, onCommit: OnCommit<T>) => Cleanup;
+export declare function addMousePressListener(element: HTMLElement, onChange: (canvasPos: Vec2 | null) => void): Cleanup;
 
-declare function addMousePressListener(element: HTMLElement, onChange: (canvasPos: Vec2 | null) => void): Cleanup;
-
-declare function addTouchPressListener(element: HTMLElement, cameraControl: CameraControl, pointerCircle: PointerCircle, onChange: (canvasPos: Vec2 | null) => void): Cleanup;
+export declare function addTouchPressListener(element: HTMLElement, cameraControl: CameraControl, pointerCircle: PointerCircle, onChange: (canvasPos: Vec2 | null) => void): Cleanup;
