20a3a6423a19dca97aa7db4342349c99be17d97b
mmilian <mateusz.milian@gmail.com>
2026-06-10 12:19:35 +0200

    chore: standarize js imports

--- src/plugins/AngleMeasurementsPlugin/index.js
+++ src/plugins/AngleMeasurementsPlugin/index.js
@@ -5,7 +5,7 @@ export * from "./AngleMeasurementsTouchControl.js";
 
 import {Component} from "../../viewer/scene/Component.js";
 import {math} from "../../viewer/scene/math/math.js";
-import {activateDraggableDots} from "../../../src/plugins/lib/ui/index.js";
+import {activateDraggableDots} from "../lib/ui/index.js";
 
 class AngleMeasurementEditControl extends Component {
 
